aba4e52db57f5ce6606bbc5b7d54c2f522a53e01
Toshiaki Hishinuma <hishinuma@ricos.co.jp>
2019-12-10 18:19:46 +0900

    spmv ci test pass

--- src/blas/sparse/spmv/crs_spmv.cpp
+++ src/blas/sparse/spmv/crs_spmv.cpp
@@ -5,7 +5,6 @@
 #include<stdlib.h>
 #include<omp.h>
 
-#include<cblas.h>
 #include "../../../../include/monolish_blas.hpp"
 
 #define BENCHMARK
@@ -21,42 +20,49 @@ namespace monolish{
 
 		}
 
-#if USE_GPU
-// 		int n = A.get_row();
-// 		int nnz = A.get_nnz();
-// 		double* xd = x.data();
-// 		double* yd = y.data();
-//
-// 		double* vald = A.val.data();
-// 		int* rowd = A.row_ptr.data();
-// 		int* cold = A.col_ind.data();
-//
-// #pragma acc loop create(yd[0:n])independent 
-// 		for(int i = 0 ; i < n; i++)
-// 			yd[i] = 0;
-//
-// #pragma acc loop copyin(xd[0:n], vald[0:nnz], rowd[0:n+1], cold[0:nnz])independent
-// 		for(int i = 0 ; i < n; i++)
-// 			for(int j = rowd[i] ; j < rowd[i+1]; j++)
-// 				yd[i] += vald[j] * xd[cold[j]];
-//
-#else
+#if USE_GPU // gpu
+		int n = A.get_row();
+		int nnz = A.get_nnz();
+		double* xd = x.data();
+		double* yd = y.data();
 
-#pragma omp parallel for 
+		double* vald = A.val.data();
+		int* rowd = A.row_ptr.data();
+		int* cold = A.col_ind.data();
+
+		#pragma acc data pcopyin(xd[0:n], vald[0:nnz], rowd[0:n+1], cold[0:nnz]) copyout(yd[0:n]) 
+		{
+			#pragma acc kernels
+			{
+				#pragma acc loop independent 
+				for(int i = 0 ; i < n; i++){
+					yd[i] = 0;
+				}
+
+				#pragma acc loop independent
+				for(int i = 0 ; i < n; i++){
+					for(int j = rowd[i] ; j < rowd[i+1]; j++){
+						yd[i] += vald[j] * xd[cold[j]];
+					}
+				}
+			}
+		}
+
+#else // cpu
+
+	#pragma omp parallel for 
 		for(int i = 0 ; i < A.get_row(); i++)
 			y.val[i] = 0;
 
-#pragma omp parallel for
+	#pragma omp parallel for
 		for(int i = 0 ; i < A.get_row(); i++)
 			for(int j = A.row_ptr[i] ; j < A.row_ptr[i+1]; j++)
 				y.val[i] += A.val[j] * x.val[A.col_ind[j]];
-	
-#endif
 
+#endif
 
 		logger.func_out();
 	}
-
 }
 
 
