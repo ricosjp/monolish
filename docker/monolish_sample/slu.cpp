e97a09bd3e72457c4652d033479c9e6e2bf4d444
Toshiaki Hishinuma <hishinuma@ricos.co.jp>
2020-01-17 14:19:37 +0900

    clean monolish image

--- docker/monolish_sample/slu.cpp
+++ docker/monolish_sample/slu.cpp
@@ -1,21 +1,13 @@
 #include<iostream>
-#include"./test_utils.hpp"
 #include"monolish_equation.hpp"
 #include"monolish_blas.hpp"
 
 int main(int argc, char** argv){
 
-	if(argc!=3){
-		std::cout << "error $1:matrix filename, $2:error check (1/0)" << std::endl;
-		return 1;
-	}
-
-	char* file = argv[1];
-	int check_ans = atoi(argv[2]);
 
 	monolish::equation::LU LU_solver;
 
-	monolish::matrix::COO<double> COO(file);
+	monolish::matrix::COO<double> COO("test.mtx");
 	monolish::matrix::CRS<double> A(COO);
 
 	// ans is 1
@@ -27,12 +19,6 @@ int main(int argc, char** argv){
 	monolish::vector<double> x(A.get_row(), 5.0);
 
 	LU_solver.solve(A, x, b);
-
-	if(check_ans == 1){
-		if(ans_check<double>(x.data(), ans.data(), x.size(), 1.0e-8) == false){
-			return 1;
-		};
-	}
 
 	x.print_all();
 
