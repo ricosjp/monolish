5d4c904c555c40dc26def3398a9b983c47cee1d4
Toshiaki Hishinuma <hishinuma@ricos.co.jp>
2020-02-10 15:11:09 +0900

    clean comments

--- include/common/monolish_vector.hpp
+++ include/common/monolish_vector.hpp
@@ -23,7 +23,7 @@ namespace monolish{
 	
 /**
  * @class monolish::vector<typename Float>
- * @brief it likes std::vector(?), it has flag that moniter changing value...
+ * @brief it likes std::vector, it has flag that moniter changing value...
 **/
 	template<typename Float>
 		class vector{
@@ -33,34 +33,71 @@ namespace monolish{
 				bool flag = 0; // 1 mean "changed", not impl..
 				std::vector<Float> val;
 
+/**
+ * @fn vector()
+ * @brief constructor
+**/
 				vector(){}
 
+/**
+ * @fn vector(std::string::size_type N)
+ * @brief initialize size N vector
+ * @param (N) vector size
+**/
 				vector(std::string::size_type N){
 					val.resize(N);
 				}
+
+/**
+ * @fn vector(std::string::size_type N, Float a)
+ * @brief initialize size N vector, value to fill the container
+ * @param (N) vector size
+ * @param (val) fill Float type value to all elements
+**/
 				vector(std::string::size_type N, Float a){
 					val.resize(N, a);
 				}
 
+/**
+ * @fn data()
+ * @brief returns a direct pointer to the vector
+ * @return A pointer to the first element
+**/
 				Float* data(){
 					return val.data();
 				}
 
+/**
+ * @fn size()
+ * @brief get vector size N
+ * @return vector size
+**/
 				std::string::size_type size(){
 					return val.size();
 				}
 
+/**
+ * @fn copy()
+ * @brief vector copy
+ * @return copied vector
+**/
 				vector copy(){
 					vector<Float> tmp(val.size());
 					std::copy(val.begin(), val.end(), tmp.val.begin());
 					return tmp;
 				}
 
+/**
+ * @fn print_all()
+ * @brief print all elements to standart I/O
+ * @return vector 
+**/
 				void print_all(){
 					for(int i = 0; i < val.size(); i++){
 						std::cout <<  val[i] << std::endl;
 					}
 				}
+
 
 // 				// need "ref operator[]" 
 // 				Float at(size_type n){
