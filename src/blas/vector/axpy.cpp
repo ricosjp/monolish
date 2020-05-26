58da835fa8b657018b0006d81c15989de23086dc
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-05-27 03:55:28 +0900

    axpy float/double send/recv

--- src/blas/vector/axpy.cpp
+++ src/blas/vector/axpy.cpp
@@ -23,18 +23,47 @@ namespace monolish{
 		double* yd = y.data();
 		size_t size = x.size();
 	
-#if USE_GPU
-		cublasHandle_t h;
-		check(cublasCreate(&h));
+		#if USE_GPU
+			cublasHandle_t h;
+			check(cublasCreate(&h));
 
-		#pragma acc host_data use_device(xd, yd)
-		{
-			check(cublasDaxpy(h, size, &alpha, xd, 1, yd, 1));
+			#pragma acc host_data use_device(xd, yd)
+			{
+				check(cublasDaxpy(h, size, &alpha, xd, 1, yd, 1));
+			}
+			cublasDestroy(h);
+		#else
+			cblas_daxpy(size, alpha, xd, 1, yd, 1);
+		#endif
+		logger.func_out();
+	}
+
+	// float ///////////////////
+	void blas::axpy(const float alpha, const vector<float> &x, vector<float> &y){
+		Logger& logger = Logger::get_instance();
+		logger.func_in(monolish_func);
+
+		//err
+		if( x.size() != y.size()){
+			throw std::runtime_error("error vector size is not same");
 		}
-		cublasDestroy(h);
-#else
-		cblas_daxpy(size, alpha, xd, 1, yd, 1);
-#endif
+
+		const float* xd = x.data();
+		float* yd = y.data();
+		size_t size = x.size();
+	
+		#if USE_GPU
+			cublasHandle_t h;
+			check(cublasCreate(&h));
+
+			#pragma acc host_data use_device(xd, yd)
+			{
+				check(cublasSaxpy(h, size, &alpha, xd, 1, yd, 1));
+			}
+			cublasDestroy(h);
+		#else
+			cblas_saxpy(size, alpha, xd, 1, yd, 1);
+		#endif
 		logger.func_out();
 	}
 }
