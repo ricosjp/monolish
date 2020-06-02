6bd061a4fa669a0d9e0aa15f956c3dbd33395255
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-06-02 16:42:44 +0900

    add none precon

--- include/monolish_equation.hpp
+++ include/monolish_equation.hpp
@@ -6,36 +6,64 @@
 #endif
 
 #include"common/monolish_common.hpp"
+#include<functional>
 
 namespace monolish{
 	namespace equation{
+
+		class none{
+			public:
+			template<typename T> void precon_create(matrix::CRS<T>& A);
+			template<typename T> void precon_apply(const vector<T>& r, vector<T>& z);
+			template<typename T> int solve(matrix::CRS<T> &A, vector<T> &x, vector<T> &b);
+		};
+
+		template<typename Float>
+		class precon{
+			private:
+			public:
+				vector<Float> M;
+				monolish::matrix::CRS<Float> A;
+				std::function<void(matrix::CRS<Float>&)> precon_create;
+				std::function<void(const vector<Float>& r, vector<Float>& z)> precon_apply;
+
+				std::function<void(void)> get_precon();
+
+				void set_precon_data(vector<Float>& m){M=m;};
+				vector<Float> get_precon_data() {return M;};
+
+				precon(){
+					equation::none p;
+					precon_create = std::bind(&none::precon_create<Float>, &p, std::placeholders::_1);
+					precon_apply = std::bind(&none::precon_apply<Float>, &p, std::placeholders::_1, std::placeholders::_2);
+				}
+		};
 
 		class solver{
 			private:
 
 			protected:
+
 				int lib = 0;
 				double tol = 1.0e-8;
 				size_t miniter = 0;
 				size_t maxiter = SIZE_MAX;
-				size_t precon_num=0;
 				size_t resid_method=0;
 				bool print_rhistory = false;
 				std::string rhistory_file;
 				std::ostream* rhistory_stream;
 				
-				/**
-				 * @brief create q = Ap
-				 **/
-				int precon_init(matrix::CRS<double> &A, vector<double> &p, vector<double> &q);
-
-				/**
-				 * @brief apply q = Ap
-				 **/
-				int precon_apply(matrix::CRS<double> &A, vector<double> &p, vector<double> &q);
-
 				double get_residual(vector<double>& x);
 				float get_residual(vector<float>& x);
+
+				// for mixed prec.
+				precon<float> f_precon;
+				precon<double> d_precon;
+
+				void precon_create(matrix::CRS<double> &A);
+				void precon_create(matrix::CRS<float> &A);
+				void precon_apply(const vector<float>& r, vector<float>& z);
+				void precon_apply(const vector<double>& r, vector<double>& z);
 
 			public:
 
@@ -54,6 +82,20 @@ namespace monolish{
 						 delete rhistory_stream;
 					 }
 				 }
+
+				/**
+				 * @brief set precondition create fucntion
+				 * @param[in] f function 
+				 **/
+				 void set_precon_create(std::function<void(matrix::CRS<double>&)> f);
+				 void set_precon_create(std::function<void(matrix::CRS<float>&)> f);
+
+				/**
+				 * @brief set precondition apply fucntion
+				 * @param[in] f function 
+				 **/
+				 void set_precon_apply(std::function<void(const vector<double>& z, vector<double>& r)> f);
+				 void set_precon_apply(std::function<void(const vector<float>& z, vector<float>& r)> f);
 
 				/**
 				 * @brief set library option
@@ -80,16 +122,10 @@ namespace monolish{
 				void set_miniter(size_t min){miniter = min;}
 
 				/**
-				 * @brief set precon number
-				 * @param[in] precondition number (0:none, 1:jacobi)
+				 * @brief set residual method (default=0)
+				 * @param[in] r residualt method number (0:nrm2)
 				 **/
-				void set_precon(size_t precondition){precon_num = precondition;}
-
-				/**
-				 * @brief set residual method(default=0)
-				 * @param[in] precondition number (0:nrm2)
-				 **/
-				void set_residual_method(size_t precondition){precon_num = precondition;}
+				void set_residual_method(size_t r){resid_method = r;}
 
 				/**
 				 * @brief print rhistory to standart out true/false. (default = false)
@@ -140,12 +176,6 @@ namespace monolish{
 				size_t get_miniter(){return miniter;}
 
 				/**
-				 * @brief get precondition number
-				 * @return  precondition number
-				 **/
-				size_t get_precon(){return precon_num;}
-
-				/**
 				 * @brief get residual method(default=0)
 				 * @return residual method number
 				 **/
@@ -158,6 +188,7 @@ namespace monolish{
 				bool get_print_rhistory(){return print_rhistory;}
 
 		};
+
 
 		/**
 		 * @brief CG solver class
@@ -178,6 +209,47 @@ namespace monolish{
 				 * @return error code (only 0 now)
 				 **/
 				template<typename T> int solve(matrix::CRS<T> &A, vector<T> &x, vector<T> &b);
+
+				template<typename T> void precon_create(matrix::CRS<T> A)
+				{throw std::runtime_error("error, CG cant be used as precon");}
+				template<typename T> void precon_apply(vector<T> z, vector<T> r)
+				{throw std::runtime_error("error, CG cant be used as precon");}
+		};
+
+		//jacobi////////////////////////////////
+		class Jacobi : public solver{
+			private:
+				int monolish_Jacobi(matrix::CRS<double> &A, vector<double> &x, vector<double> &b);
+				int monolish_PJacobi(matrix::CRS<double> &A, vector<double> &x, vector<double> &b);
+			public:
+// 				std::function<void(matrix::CRS<double>&)> precon_create;
+// 				std::function<void(const vector<double>& r, vector<double>& z)> precon_apply;
+
+				using solver::solver;
+
+				/**
+				 * @brief solve Ax = b by jacobi method(lib=0: monolish)
+				 * @param[in] A CRS format Matrix
+				 * @param[in] x solution vector
+				 * @param[in] b right hand vector
+				 * @return error code (only 0 now)
+				 **/
+				template<typename T> int solve(matrix::CRS<T> &A, vector<T> &x, vector<T> &b);
+
+				void precon_create(matrix::CRS<double> &A);
+				void precon_create(matrix::CRS<float> &A);
+				void precon_apply(const vector<float>& r, vector<float>& z);
+				void precon_apply(const vector<double>& r, vector<double>& z);
+
+// 				Jacobi(){
+// 					auto create = [&precon_create](matrix::CRS<double> &A){
+// 						precon_create(A);
+// 					};
+// 					f_precon.precon_create= std::bind(&create, ); 
+// 					precon_create = std::bind(&none::precon_create<Float>, &p, std::placeholders::_1);
+// 					//precon_apply = std::bind(&Jacobi::precon_apply, *this, std::placeholders::_1, std::placeholders::_2);
+// 				}
+
 		};
 
 
@@ -199,6 +271,11 @@ namespace monolish{
 				int get_sigularity(){ return singularity; }
 
 				int solve(matrix::CRS<double> &A, vector<double> &x, vector<double> &b);
+
+				template<typename T> void precon_create(matrix::CRS<T> A)
+				{throw std::runtime_error("error, LU cant be used as precon");}
+				template<typename T> void precon_apply(vector<T> z, vector<T> r)
+				{throw std::runtime_error("error, LU cant be used as precon");}
 		};
 
 		/**
@@ -227,6 +304,11 @@ namespace monolish{
 				int get_sigularity(){ return singularity; }
 				
 				template<typename T> int solve(matrix::CRS<T> &A, vector<T> &x, vector<T> &b);
+
+				template<typename T> void precon_create(matrix::CRS<T> A)
+				{throw std::runtime_error("error, QR cant be used as precon");}
+				template<typename T> void precon_apply(vector<T> z, vector<T> r)
+				{throw std::runtime_error("error, QR cant be used as precon");}
 		};
 
 		/**
@@ -255,28 +337,11 @@ namespace monolish{
 				int get_sigularity(){ return singularity; }
 				
 				template<typename T> int solve(matrix::CRS<T> &A, vector<T> &x, vector<T> &b);
+
+				template<typename T> void precon_create(matrix::CRS<T> A)
+				{throw std::runtime_error("error, Chol cant be used as precon");}
+				template<typename T> void precon_apply(vector<T> z, vector<T> r)
+				{throw std::runtime_error("error, Chol cant be used as precon");}
 		};
-
-		//jacobi////////////////////////////////
-		class Jacobi : public solver{
-			private:
-				int monolish_Jacobi(matrix::CRS<double> &A, vector<double> &x, vector<double> &b);
-				int monolish_PJacobi(matrix::CRS<double> &A, vector<double> &x, vector<double> &b);
-			public:
-				using solver::solver;
-
-				/**
-				 * @brief solve Ax = b by jacobi method(lib=0: monolish)
-				 * @param[in] A CRS format Matrix
-				 * @param[in] x solution vector
-				 * @param[in] b right hand vector
-				 * @return error code (only 0 now)
-				 **/
-				int solve(matrix::CRS<double> &A, vector<double> &x, vector<double> &b);
-
-				int Pinit(matrix::CRS<double> &A, vector<double> &x, vector<double> &b);
-				int Papply(matrix::CRS<double> &A, vector<double> &x, vector<double> &b);
-		};
-
 	}
 }
--- src/equation/precon.cpp
+++ src/equation/precon.cpp
@@ -9,6 +9,9 @@ namespace monolish{
 	////////////////////////////////////////
 	template <typename T>
 	void equation::none::precon_create(matrix::CRS<T>& A){
+		Logger& logger = Logger::get_instance();
+		logger.solver_in(monolish_func);
+		logger.solver_out();
 	}
 	template void equation::none::precon_create(matrix::CRS<double>& A);
 	template void equation::none::precon_create(matrix::CRS<float>& A);
@@ -17,7 +20,10 @@ namespace monolish{
 
 	template<typename T>
 	void equation::none::precon_apply(const vector<T>& r, vector<T>& z){
+		Logger& logger = Logger::get_instance();
+		logger.solver_in(monolish_func);
 		z = r;
+		logger.solver_out();
 	}
 	template void equation::none::precon_apply(const vector<double>& r, vector<double>& z);
 	template void equation::none::precon_apply(const vector<float>& r, vector<float>& z);
@@ -26,7 +32,10 @@ namespace monolish{
 
 	template<typename T>
 	int equation::none::solve(matrix::CRS<T> &A, vector<T> &x, vector<T> &b){
+		Logger& logger = Logger::get_instance();
+		logger.solver_in(monolish_func);
 		return MONOLISH_SOLVER_SUCCESS;
+		logger.solver_out();
 	}
 	template int equation::none::solve(matrix::CRS<float> &A, vector<float> &x, vector<float> &b);
 	template int equation::none::solve(matrix::CRS<double> &A, vector<double> &x, vector<double> &b);
@@ -48,6 +57,7 @@ namespace monolish{
 	void equation::solver::set_precon_apply(std::function<void(const vector<float>& r, vector<float>& z)> f){
 		f_precon.precon_apply = f;
 	}
+
 
 	//////////////////////////////////////////////////////
 	void equation::solver::precon_create(matrix::CRS<double>& A){
