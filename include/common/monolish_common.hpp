47b13f483f667aa362a26860839dcea4f2111055
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-09-10 05:25:57 +0900

    test matrix generator is added

--- include/common/monolish_common.hpp
+++ include/common/monolish_common.hpp
@@ -13,107 +13,114 @@
 #define MONOLISH_SOLVER_NOT_IMPL -4
 
 namespace monolish{
-	namespace util{
+    namespace util{
 
-		/**
-		 * @brief get nrm |b-Ax|
-		 */
-		template<typename T>
-			T get_residual_l2(matrix::CRS<T> &A, vector<T> &x, vector<T> &b);
+        /**
+         * @brief get nrm |b-Ax|
+         */
+        template<typename T>
+            T get_residual_l2(matrix::CRS<T> &A, vector<T> &x, vector<T> &b);
 
-		/**
-		 * @brief check error 
-		 * @param[in] err solver err code
-		 */
-		bool solver_check( const int err);
+        /**
+         * @brief check error 
+         * @param[in] err solver err code
+         */
+        bool solver_check( const int err);
 
-		/**
-		 * @brief      0 : none<br> 1 : all<br> 2 : solver<br>2 : solver, func <br>3 : solver, func, util
-		 * @param[in] Level Log level
-		 */
-		void set_log_level(size_t Level);
+        /**
+         * @brief      0 : none<br> 1 : all<br> 2 : solver<br>2 : solver, func <br>3 : solver, func, util
+         * @param[in] Level Log level
+         */
+        void set_log_level(size_t Level);
 
-		/**
-		 * @brief set output logfile name (defailt=standard I/O)
-		 * @param[in] filename log file name (if not set filename, output standard I/O)
-		 */
-		void set_log_filename(std::string filename);
+        /**
+         * @brief set output logfile name (defailt=standard I/O)
+         * @param[in] filename log file name (if not set filename, output standard I/O)
+         */
+        void set_log_filename(std::string filename);
 
-		/**
-		 * @brief create random vector
-		 * @return ramdom vector 
-		 **/
-		template<typename T>
-			void random_vector(vector<T>& vec, const T min, const T max){
-				// rand (0~1)
-				std::random_device random;
-				std::mt19937 mt(random());
-				std::uniform_real_distribution<> rand(min,max);
+        /**
+         * @brief create random vector
+         * @return ramdom vector 
+         **/
+        template<typename T>
+            void random_vector(vector<T>& vec, const T min, const T max){
+                // rand (0~1)
+                std::random_device random;
+                std::mt19937 mt(random());
+                std::uniform_real_distribution<> rand(min,max);
 
-				for(size_t i=0; i<vec.size(); i++){
-					vec[i] = rand(mt);
-				}
-			}
-//send///////////////////
+                for(size_t i=0; i<vec.size(); i++){
+                    vec[i] = rand(mt);
+                }
+            }
+        //send///////////////////
 
-		/**
-		 * @brief send data to GPU
-		 **/
-		template < typename T >
-			auto send( T& x )
-			{
-				x.send();
-			}
+        /**
+         * @brief send data to GPU
+         **/
+        template < typename T >
+            auto send( T& x )
+            {
+                x.send();
+            }
 
-		/**
-		 * @brief send datas to GPU
-		 **/
-		template < typename T, typename ... Types >
-			auto send( T& x, Types& ... args )
-			{
-				x.send();
-				send( args... );
-			}
+        /**
+         * @brief send datas to GPU
+         **/
+        template < typename T, typename ... Types >
+            auto send( T& x, Types& ... args )
+            {
+                x.send();
+                send( args... );
+            }
 
-//recv///////////////////
-		/**
-		 * @brief recv data from GPU
-		 **/
-		template < typename T >
-			auto recv( T& x )
-			{
-				x.recv();
-			}
+        //recv///////////////////
+        /**
+         * @brief recv data from GPU
+         **/
+        template < typename T >
+            auto recv( T& x )
+            {
+                x.recv();
+            }
 
-		/**
-		 * @brief recv datas to GPU
-		 **/
-		template < typename T, typename ... Types >
-			auto recv( T& x, Types& ... args )
-			{
-				x.recv();
-				recv( args... );
-			}
+        /**
+         * @brief recv datas to GPU
+         **/
+        template < typename T, typename ... Types >
+            auto recv( T& x, Types& ... args )
+            {
+                x.recv();
+                recv( args... );
+            }
 
-//device_free///////////////////
+        //device_free///////////////////
 
-		/**
-		 * @brief free data of GPU
-		 **/
-		template < typename T >
-			auto device_free( T& x )
-			{
-				x.device_free();
-			}
+        /**
+         * @brief free data of GPU
+         **/
+        template < typename T >
+            auto device_free( T& x )
+            {
+                x.device_free();
+            }
 
-		/**
-		 * @brief free datas of GPU
-		 **/
-		template < typename T, typename ... Types >
-			auto device_free( T& x, Types& ... args )
-			{
-				x.device_free();
-				device_free( args... );
-			}
-	}
+        /**
+         * @brief free datas of GPU
+         **/
+        template < typename T, typename ... Types >
+            auto device_free( T& x, Types& ... args )
+            {
+                x.device_free();
+                device_free( args... );
+            }
+        template<typename T>
+            matrix::COO<T> band_matrix(const int M, const int N, const int W, const T diag_val, const T val);
+        template<typename T>
+            matrix::COO<T> random_structure_matrix(const int M, const int N, const int nnzrow, const T val);
+        template<typename T>
+            matrix::COO<T> eye(const int M, const int N, const T val);
+    }
 }
+
