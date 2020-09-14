aae9ad4f100e281ac605e744b6c2533df74dc2dc
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-09-14 12:36:17 +0900

    test shell bug fix

--- test/matrix_blas/test.sh
+++ test/matrix_blas/test.sh
@@ -44,7 +44,7 @@ case $1 in
                 for M in ${MAT_TEST_SIZE[@]}; do
                     for N in ${MAT_TEST_SIZE[@]}; do
                         echo start $1 $2 $prec $format $M $N $3
-                        .$PROFILER ./$1_$2.out $prec $format $M $N $3 1 || exit 1
+                        $PROFILER ./$1_$2.out $prec $format $M $N $3 1 || exit 1
                     done
                 done
             done
