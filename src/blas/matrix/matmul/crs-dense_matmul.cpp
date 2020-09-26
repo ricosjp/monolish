6a46cfdac29dd3eebd0a3ecf1b675946f4d84fc1
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-09-26 12:52:54 +0900

    clang format

--- src/blas/matrix/matmul/crs-dense_matmul.cpp
+++ src/blas/matrix/matmul/crs-dense_matmul.cpp
@@ -13,7 +13,6 @@
 using reg = __m256;
 //#endif
 
-
 namespace monolish {
 
 // double ///////////////////
@@ -242,35 +241,35 @@ float ret[8];
       }
   }
 #else
-const int vecL=8;
-float ret[8];
-const reg zerov = SIMD_FUNC(set_ps)(0,0,0,0,0,0,0,0);
+  const int vecL = 8;
+  float ret[8];
+  const reg zerov = SIMD_FUNC(set_ps)(0, 0, 0, 0, 0, 0, 0, 0);
 
 #pragma omp parallel for
-for(int i = 0; i<(int)(M*N); i++){
+  for (int i = 0; i < (int)(M * N); i++) {
     Cd[i] = 0.0;
-}
+  }
 
-for (int i = 0; i < (int)M; i++) {
+  for (int i = 0; i < (int)M; i++) {
     int start = (int)rowd[i];
-    int end = (int)rowd[i+1];
+    int end = (int)rowd[i + 1];
     for (int k = start; k < end; k++) {
-        int Br = N*cold[k];
-        reg Av = SIMD_FUNC(broadcast_ss)(&vald[k]);
-        reg tv;
-        int j;
-        for (j = 0; j < (int)N - (vecL-1); j+=vecL) {
-            reg Bv = SIMD_FUNC(loadu_ps)((float*)&Bd[Br+j]);
-            reg Cv = SIMD_FUNC(loadu_ps)((float*)&Cd[i*N+j]);
-            tv = SIMD_FUNC(mul_ps)(Av, Bv);
-            Cv = SIMD_FUNC(add_ps)(Cv, tv);
-            SIMD_FUNC(storeu_ps)((float*)&Cd[i*N+j], Cv);
-        }
-        for ( ; j < (int)N; j++) {
-            Cd[i*N+j] += vald[k] * Bd[N * cold[k] + j];
-        }
+      int Br = N * cold[k];
+      reg Av = SIMD_FUNC(broadcast_ss)(&vald[k]);
+      reg tv;
+      int j;
+      for (j = 0; j < (int)N - (vecL - 1); j += vecL) {
+        reg Bv = SIMD_FUNC(loadu_ps)((float *)&Bd[Br + j]);
+        reg Cv = SIMD_FUNC(loadu_ps)((float *)&Cd[i * N + j]);
+        tv = SIMD_FUNC(mul_ps)(Av, Bv);
+        Cv = SIMD_FUNC(add_ps)(Cv, tv);
+        SIMD_FUNC(storeu_ps)((float *)&Cd[i * N + j], Cv);
+      }
+      for (; j < (int)N; j++) {
+        Cd[i * N + j] += vald[k] * Bd[N * cold[k] + j];
+      }
     }
-}
+  }
 #endif
 #endif
 /////////////////////////////////////////////////////
