75a8d977170401a14f46f274d1cb5ebcd6f1880d
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-09-28 07:35:16 +0900

    tanh gpu bug fix

--- src/blas/math/tanh.cpp
+++ src/blas/math/tanh.cpp
@@ -15,15 +15,6 @@
 namespace monolish {
 
 void tanh_core(size_t N, double *vec) {
-#if USE_GPU
-#pragma acc data present(vec [0:N])
-#pragma acc host_data use_device(vec)
-  {
-    for (size_t i = 0; i < N; i++) {
-      vec[i] = tanh(vec[i]);
-    }
-  }
-#else
 #if USE_MKL
   vdTanh(N, vec, vec);
 #else
@@ -32,19 +23,9 @@ void tanh_core(size_t N, double *vec) {
     vec[i] = std::tanh(vec[i]);
   }
 #endif
-#endif
 }
 
 void tanh_core(size_t N, float *vec) {
-#if USE_GPU
-#pragma acc data present(vec [0:N])
-#pragma acc host_data use_device(vec)
-  {
-    for (size_t i = 0; i < N; i++) {
-      vec[i] = tanhf(vec[i]);
-    }
-  }
-#else
 #if USE_MKL
   vsTanh(N, vec, vec);
 #else
@@ -52,7 +33,6 @@ void tanh_core(size_t N, float *vec) {
   for (size_t i = 0; i < N; i++) {
     vec[i] = std::tanh(vec[i]);
   }
-#endif
 #endif
 }
 
@@ -62,8 +42,10 @@ template <typename T> void vector<T>::tanh() {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
+  recv();
   T *vald = val.data();
   tanh_core(get_nnz(), vald);
+  send();
 
   logger.func_out();
 }
@@ -77,8 +59,10 @@ template <typename T> void matrix::Dense<T>::tanh() {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
+  recv();
   T *vald = val.data();
   tanh_core(get_nnz(), vald);
+  send();
 
   logger.func_out();
 }
@@ -92,8 +76,10 @@ template <typename T> void matrix::CRS<T>::tanh() {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
+  recv();
   T *vald = val.data();
   tanh_core(get_nnz(), vald);
+  send();
 
   logger.func_out();
 }
