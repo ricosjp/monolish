0bbc339db5dbfc743c650aa646f615d357347b7d
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-10-10 15:14:58 +0900

    cpp->hpp in logger, #281

--- src/utils/logger_utils.cpp
+++ src/utils/logger_utils.cpp
@@ -1,32 +1,190 @@
 #include "../../include/monolish_blas.hpp"
 
-void monolish::util::set_log_level(size_t Level) {
-  Logger &logger = Logger::get_instance();
-  logger.set_log_level(Level);
-}
+namespace monolish{
 
-void monolish::util::set_log_filename(std::string filename) {
-  Logger &logger = Logger::get_instance();
-  logger.set_log_filename(filename);
-}
+    // solver
 
-bool monolish::util::solver_check(const int err) {
-  switch (err) {
-  case MONOLISH_SOLVER_SUCCESS:
-    return 0;
-  case MONOLISH_SOLVER_MAXITER:
-    std::runtime_error("error, maxiter\n");
-    return false;
-  case MONOLISH_SOLVER_BREAKDOWN:
-    std::runtime_error("error, breakdown\n");
-    return false;
-  case MONOLISH_SOLVER_SIZE_ERROR:
-    std::runtime_error("error, size error\n");
-    return false;
-  case MONOLISH_SOLVER_NOT_IMPL:
-    std::runtime_error("error, this solver is not impl.\n");
-    return false;
-  default:
-    return 0;
-  }
+    void Logger::solver_in(std::string func_name) {
+        if (LogLevel >= 1) {
+            if (filename.empty()) {
+                pStream = &std::cout;
+            }
+
+            // init
+            calls.push_back(func_name);
+            times.push_back(std::chrono::system_clock::now());
+
+            // start
+            *pStream << "- {" << std::flush;
+
+            // func
+            *pStream << "type : solver, " << std::flush;
+            *pStream << "name : " << std::flush;
+            for (int i = 0; i < (int)calls.size(); i++)
+                *pStream << calls[i] << "/" << std::flush;
+            *pStream << ", " << std::flush;
+
+            // stat
+            *pStream << "stat : IN" << std::flush;
+
+            // end
+            *pStream << "}" << std::endl;
+        }
+    }
+
+    void Logger::solver_out() {
+        if (LogLevel >= 1) {
+            if (filename.empty()) {
+                pStream = &std::cout;
+            }
+
+            // start
+            *pStream << "- {" << std::flush;
+
+            // func
+            *pStream << "type : solver, " << std::flush;
+            *pStream << "name : " << std::flush;
+            for (int i = 0; i < (int)calls.size(); i++)
+                *pStream << calls[i] << "/" << std::flush;
+            *pStream << ", " << std::flush;
+
+            // time
+            auto end = std::chrono::system_clock::now();
+            double sec = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                    end - times[(int)times.size() - 1])
+                .count() /
+                1.0e+9;
+            *pStream << "stat : OUT, " << std::flush;
+            *pStream << "time : " << sec << std::flush;
+
+            // end
+            *pStream << "}" << std::endl;
+
+            calls.pop_back();
+            times.pop_back();
+        }
+    }
+
+    // func
+
+    void Logger::func_in(std::string func_name) {
+        if (LogLevel >= 2) {
+            if (filename.empty()) {
+                pStream = &std::cout;
+            }
+
+            calls.push_back(func_name);
+            times.push_back(std::chrono::system_clock::now());
+        }
+    }
+
+    void Logger::func_out() {
+        if (LogLevel >= 2) {
+            if (filename.empty()) {
+                pStream = &std::cout;
+            }
+
+            // start
+            *pStream << "- {" << std::flush;
+
+            // func
+            *pStream << "type : func, " << std::flush;
+            *pStream << "name : " << std::flush;
+            for (int i = 0; i < (int)calls.size(); i++)
+                *pStream << calls[i] << "/" << std::flush;
+            *pStream << ", " << std::flush;
+
+            // time
+            auto end = std::chrono::system_clock::now();
+            double sec = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                    end - times[(int)times.size() - 1])
+                .count() /
+                1.0e+9;
+            *pStream << "time : " << sec << std::flush;
+
+            // end
+            *pStream << "}" << std::endl;
+
+            calls.pop_back();
+            times.pop_back();
+        }
+    }
+
+    // util
+    void Logger::util_in(std::string func_name) {
+        if (LogLevel >= 3) {
+            if (filename.empty()) {
+                pStream = &std::cout;
+            }
+            calls.push_back(func_name);
+            times.push_back(std::chrono::system_clock::now());
+        }
+    }
+
+    void Logger::util_out() {
+        if (LogLevel >= 3) {
+            if (filename.empty()) {
+                pStream = &std::cout;
+            }
+
+            // start
+            *pStream << "- {" << std::flush;
+
+            // func
+            *pStream << "type : util, " << std::flush;
+            *pStream << "name : " << std::flush;
+            for (int i = 0; i < (int)calls.size(); i++)
+                *pStream << calls[i] << "/" << std::flush;
+            *pStream << ", " << std::flush;
+
+            // time
+            auto end = std::chrono::system_clock::now();
+            double sec = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                    end - times[(int)times.size() - 1])
+                .count() /
+                1.0e+9;
+            *pStream << "time : " << sec << std::flush;
+
+            // end
+            *pStream << "}" << std::endl;
+
+            calls.pop_back();
+            times.pop_back();
+        }
+    }
+
+    /// logger util ///
+
+    void util::set_log_level(size_t Level) {
+        Logger &logger = Logger::get_instance();
+        logger.set_log_level(Level);
+    }
+
+    void util::set_log_filename(std::string filename) {
+        Logger &logger = Logger::get_instance();
+        logger.set_log_filename(filename);
+    }
+
+    bool util::solver_check(const int err) {
+        switch (err) {
+            case MONOLISH_SOLVER_SUCCESS:
+                return 0;
+            case MONOLISH_SOLVER_MAXITER:
+                std::runtime_error("error, maxiter\n");
+                return false;
+            case MONOLISH_SOLVER_BREAKDOWN:
+                std::runtime_error("error, breakdown\n");
+                return false;
+            case MONOLISH_SOLVER_SIZE_ERROR:
+                std::runtime_error("error, size error\n");
+                return false;
+            case MONOLISH_SOLVER_NOT_IMPL:
+                std::runtime_error("error, this solver is not impl.\n");
+                return false;
+            default:
+                return 0;
+        }
+    }
+
+
 }
