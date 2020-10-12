0442f9d00f68a42a729741188a2b847359ca30a0
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-10-13 04:15:54 +0900

    closes #317

--- src/utils/vector_util.cpp
+++ src/utils/vector_util.cpp
@@ -4,33 +4,49 @@
 namespace monolish {
 
 /////vector constructor//////
-template <typename T> vector<T>::vector(const size_t N) { val.resize(N); }
+template <typename T> vector<T>::vector(const size_t N) { 
+  Logger &logger = Logger::get_instance();
+  logger.util_in(monolish_func);
+    val.resize(N); 
+  logger.util_out();
+}
 template vector<double>::vector(const size_t N);
 template vector<float>::vector(const size_t N);
 
 template <typename T> vector<T>::vector(const size_t N, const T value) {
+  Logger &logger = Logger::get_instance();
+  logger.util_in(monolish_func);
   val.resize(N, value);
+  logger.util_out();
 }
 template vector<double>::vector(const size_t N, const double value);
 template vector<float>::vector(const size_t N, const float value);
 
 template <typename T> vector<T>::vector(const std::vector<T> &vec) {
+  Logger &logger = Logger::get_instance();
+  logger.util_in(monolish_func);
   val.resize(vec.size());
   std::copy(vec.begin(), vec.end(), val.begin());
+  logger.util_out();
 }
 template vector<double>::vector(const std::vector<double> &vec);
 template vector<float>::vector(const std::vector<float> &vec);
 
 template <typename T> vector<T>::vector(const T *start, const T *end) {
+  Logger &logger = Logger::get_instance();
+  logger.util_in(monolish_func);
   size_t size = (end - start);
   val.resize(size);
   std::copy(start, end, val.begin());
+  logger.util_out();
 }
 template vector<double>::vector(const double *start, const double *end);
 template vector<float>::vector(const float *start, const float *end);
 
 template <typename T>
 vector<T>::vector(const size_t N, const T min, const T max) {
+  Logger &logger = Logger::get_instance();
+  logger.util_in(monolish_func);
   val.resize(N);
   std::random_device random;
   std::mt19937 mt(random());
@@ -40,6 +56,7 @@ vector<T>::vector(const size_t N, const T min, const T max) {
   for (size_t i = 0; i < val.size(); i++) {
     val[i] = rand(mt);
   }
+  logger.util_out();
 }
 template vector<double>::vector(const size_t N, const double min,
                                 const double max);
@@ -49,14 +66,19 @@ template vector<float>::vector(const size_t N, const float min,
 // vector utils//////////////////////
 
 template <typename T> void vector<T>::print_all() const {
+  Logger &logger = Logger::get_instance();
+  logger.util_in(monolish_func);
   for (const auto v : val) {
     std::cout << v << std::endl;
   }
+  logger.util_out();
 }
 template void vector<double>::print_all() const;
 template void vector<float>::print_all() const;
 
 template <typename T> void vector<T>::print_all(std::string filename) const {
+  Logger &logger = Logger::get_instance();
+  logger.util_in(monolish_func);
   std::ofstream ofs(filename);
   if (!ofs) {
     throw std::runtime_error("error file cant open");
@@ -64,6 +86,7 @@ template <typename T> void vector<T>::print_all(std::string filename) const {
   for (const auto v : val) {
     ofs << v << std::endl;
   }
+  logger.util_out();
 }
 template void vector<double>::print_all(std::string filename) const;
 template void vector<float>::print_all(std::string filename) const;
@@ -71,6 +94,8 @@ template void vector<float>::print_all(std::string filename) const;
 /// vector operator ///
 
 template <typename T> bool vector<T>::operator==(const vector<T> &vec) {
+  Logger &logger = Logger::get_instance();
+  logger.util_in(monolish_func);
   if (get_device_mem_stat()) {
     throw std::runtime_error("Error, GPU vector cant use operator==");
   }
@@ -80,12 +105,15 @@ template <typename T> bool vector<T>::operator==(const vector<T> &vec) {
     if (val[i] != vec.val[i])
       return false;
   }
+  logger.util_out();
   return true;
 }
 template bool vector<double>::operator==(const vector<double> &vec);
 template bool vector<float>::operator==(const vector<float> &vec);
 
 template <typename T> bool vector<T>::operator!=(const vector<T> &vec) {
+  Logger &logger = Logger::get_instance();
+  logger.util_in(monolish_func);
   if (get_device_mem_stat()) {
     throw std::runtime_error("Error, GPU vector cant use operator!=");
   }
@@ -95,6 +123,7 @@ template <typename T> bool vector<T>::operator!=(const vector<T> &vec) {
     if (val[i] != vec.val[i])
       return true;
   }
+  logger.util_out();
   return false;
 }
 template bool vector<double>::operator!=(const vector<double> &vec);
@@ -102,11 +131,14 @@ template bool vector<float>::operator!=(const vector<float> &vec);
 
 template <typename T>
 T util::get_residual_l2(matrix::CRS<T> &A, vector<T> &x, vector<T> &b) {
+  Logger &logger = Logger::get_instance();
+  logger.util_in(monolish_func);
   vector<T> tmp(x.size());
   tmp.send();
 
   blas::matvec(A, x, tmp); // tmp=Ax
   tmp = b - tmp;
+  logger.util_out();
   return blas::nrm2(tmp);
 }
 template double util::get_residual_l2(matrix::CRS<double> &A, vector<double> &x,
