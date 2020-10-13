5d489d532a546f999395fee67b6bc92e20dad6d9
sogi <shnchrOg@yandex.com>
2020-10-13 04:39:03 +0000

    fixed Aggregate.Aggregate

--- test/logger/logging/logger.py
+++ test/logger/logging/logger.py
@@ -106,20 +106,28 @@ class DropInformation:
 
 class Grouping:
     def grouping_1st_layer(self, target_dict_list):
+        # solve
         layer_1st = "solve/"
         solver_dict_list = list(filter(lambda any_dict:layer_1st in any_dict["name"], target_dict_list))
-        other_dict_list = list(filter(lambda any_dict:layer_1st not in any_dict["name"], target_dict_list))
-
         filter_list = list(map(lambda any_dict:(("stat" in any_dict) and any_dict["stat"] == "IN" and any_dict["name"] == layer_1st), solver_dict_list))
         split_index_list = [index for index, value in enumerate(filter_list) if value == True] + [len(filter_list)]
         solver_dict_block_list = [solver_dict_list[split_index_list[index]: split_index_list[index+1]] for index in range(len(split_index_list)-1)]
 
-        # filter_list = list(map(lambda any_dict:(("stat" in any_dict) and any_dict["name"] == layer_1st), other_dict_list))
-        # split_index_list = [index for index, value in enumerate(filter_list) if value == True] + [len(filter_list)]
-        # solver_dict_block_list = [other_dict_list[split_index_list[index]: split_index_list[index+1]] for index in range(len(split_index_list)-1)]
+        # other
+        other_dict_list = list(filter(lambda any_dict:layer_1st not in any_dict["name"], target_dict_list))
+        if other_dict_list:
+            layer_list = list(map(lambda any_dict:any_dict["name"].split("/")[0], other_dict_list))
+            layer_list = list(set(layer_list))
+            layer_list = list(map(lambda x:x+"/", layer_list))
+            for other_layer_1st in layer_list:
+                filter_list = list(map(lambda any_dict:any_dict["name"] == other_layer_1st, other_dict_list))
+                split_index_list = [index for index, value in enumerate(filter_list) if value == True] + [len(filter_list)]
+                other_dict_block_list = [other_dict_list[split_index_list[index]: split_index_list[index+1]] for index in range(len(split_index_list)-1)]
+        else:
+            other_dict_block_list = []
 
-        block_dict_lists = [other_dict_list] + solver_dict_block_list
-        title_list = ["other"] + [f"solver {str(layer)}" for layer in range(len(solver_dict_block_list))]
+        block_dict_lists = other_dict_block_list + solver_dict_block_list
+        title_list = [f"other {str(no)}" for no in range(len(other_dict_block_list))] + [f"solver {str(no)}" for no in range(len(solver_dict_block_list))]
         return title_list, block_dict_lists
 
 class Aggregate:
@@ -196,7 +204,7 @@ try:
 
         # drop dir info
         target_dict_list = drop_information.drop_dir_info(target_dict_list)
-        logger.log_success("drop dir infon")
+        logger.log_success("drop dir info")
 
         # 1st layer type
         grouping = Grouping()
