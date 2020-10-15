45c593815ea6a1af6e4769add28c23ebbc9b532a
muripoLife <life.is.too.high.difficulty@gmail.com>
2020-10-15 16:14:35 +0900

    refectoring module aggregate.culc_percent

--- test/logger/logging/libs/aggregate.py
+++ test/logger/logging/libs/aggregate.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def aggregated_by_floor(block_dict_2multiple_list):
     aggr_column_lists, aggr_ndarrays = [], []
-    block_dict_2multiple_list = filter(lambda x: x != [], block_dict_2multiple_list)
+    block_dict_2multiple_list = filter(lambda any_dict_list: any_dict_list != [], block_dict_2multiple_list)
     for index, block_dict_list in enumerate(block_dict_2multiple_list):
         block_dict_list = list(map(lambda block_dict: dict(list(block_dict.items())+[("stat", "")]) if ("stat" not in block_dict) else block_dict, block_dict_list))
         block_dict_list = list(map(lambda block_dict: dict(list(block_dict.items())+[("time", "")]) if ("time" not in block_dict) else block_dict, block_dict_list))
@@ -25,38 +25,10 @@ def aggregated_by_floor(block_dict_2multiple_list):
                 rst_narray = np.array([layer, col, count, total_time])
                 aggr_ndarray = np.append(aggr_ndarray, [rst_narray], axis=0)
 
-        # TODO
+        # culc_percent
         aggr_column_list = ["layer", "name", "count", "total_time [s]"]
-        for layer in range(1, max_layer):
-            any_aggr_ndarray = np.empty((0, 4))
-            # print(aggr_ndarray)
 
-            for any_ndarray in aggr_ndarray:
-
-                any_aggr_ndarray = np.append(any_aggr_ndarray, [any_ndarray], axis=0)
-
-                # if any_ndarray[0] == str(layer) or any_ndarray[0] == str(layer+1):
-                #     any_aggr_ndarray = np.append(any_aggr_ndarray, [any_ndarray], axis=0)
-                # else:
-                #     temp_ndarray = np.copy(any_ndarray)
-                #     temp_ndarray[3] = 0.0
-                #     any_aggr_ndarray = np.append(any_aggr_ndarray, [temp_ndarray], axis=0)
-            # print(any_aggr_ndarray)
-
-            denominator = (float)(any_aggr_ndarray[np.array(list(map(lambda x: int(x[0])==layer, any_aggr_ndarray)))][:, 3][0])
-            percent = np.array(any_aggr_ndarray[:, 3], dtype="float32") / denominator * 100.0
-            percent = np.round(percent, decimals=3)
-            percent = np.where(percent <= 100.0, percent, "")
-            any_aggr_ndarray = np.insert(any_aggr_ndarray, any_aggr_ndarray.shape[1], percent, axis=1)
-
-            aggr_column_list.append(f"breakdown_layer {str(layer)} [%]")
-            # print("----------------")
-            # print(any_aggr_ndarray)
-            # print("----------------")
-            # print(aggr_ndarray)
-            # print("----------------")
-
-        aggr_ndarray[:, 3] = np.round(np.array(aggr_ndarray[:, 3], dtype="float32"), decimals=3)
+        aggr_ndarray, aggr_column_list = culc_percent(aggr_ndarray, aggr_column_list, max_layer)
 
         # aggregate list
         aggr_column_lists.append(aggr_column_list)
@@ -66,3 +38,16 @@ def aggregated_by_floor(block_dict_2multiple_list):
         index = index + 1
 
     return aggr_column_lists, aggr_ndarrays, index
+
+def culc_percent(aggr_ndarray, aggr_column_list, max_layer):
+    aggr_column_list = ["layer", "name", "count", "total_time [s]"]
+    for layer in range(1, max_layer):
+        denominator = (float)(aggr_ndarray[np.array(list(map(lambda x: int(x[0])==layer, aggr_ndarray)))][:, 3][0])
+        percent = np.array(aggr_ndarray[:, 3], dtype="float32") / denominator * 100.0
+        percent = np.round(percent, decimals=3)
+        percent = np.where(percent <= 100.0, percent, "")
+        aggr_ndarray = np.insert(aggr_ndarray, aggr_ndarray.shape[1], percent, axis=1)
+        aggr_column_list.append(f"breakdown_layer {str(layer)} [%]")
+    aggr_ndarray[:, 3] = np.round(np.array(aggr_ndarray[:, 3], dtype="float32"), decimals=3)
+
+    return aggr_ndarray, aggr_column_list
