bcb8bc22580ac8438be316aef6af268baa05c6d6
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-10-22 03:45:01 +0900

    vector arithmetic test

---
+++ test/vector_blas/scalar-vector/sv_add.hpp
@@ -0,0 +1,45 @@
+#include "../../test_utils.hpp"
+
+template <typename T>
+void ans_svadd(monolish::vector<T> &mx, T value, monolish::vector<T> &ans) {
+  if (mx.size() != ans.size()) {
+    throw std::runtime_error("x.size != y.size");
+  }
+
+  for (size_t i = 0; i < mx.size(); i++) {
+    ans[i] += mx[i] + value;
+  }
+}
+
+template <typename T>
+bool test_send_svadd(const size_t size, double tol){
+
+  T value = 123.0;
+  monolish::vector<T> x(size, 0.1, 1.0);
+  monolish::vector<T> ans(size, 321.0);
+
+  monolish::vector<T> ans_tmp = ans.copy();
+  ans_svadd(x, value, ans_tmp);
+
+  monolish::util::send(x, ans);
+  ans += x + value;
+  ans.recv();
+
+  return ans_check<T>(__func__, ans.data(), ans_tmp.data(), x.size(), tol);
+}
+
+template <typename T>
+bool test_svadd(const size_t size, double tol){
+  std::cout << __func__ << "(" << get_type<T>() << ")" << std::flush;
+
+  T value = 123.0;
+  monolish::vector<T> x(size, 0.1, 1.0);
+  monolish::vector<T> ans(size, 321.0);
+
+  monolish::vector<T> ans_tmp = ans.copy();
+  ans_svadd(x, value, ans_tmp);
+
+  ans += x + value;
+
+  return ans_check<T>(__func__, ans.data(), ans_tmp.data(), x.size(), tol);
+}
---
+++ test/vector_blas/scalar-vector/sv_mul.hpp
@@ -0,0 +1,44 @@
+#include "../../test_utils.hpp"
+
+template <typename T>
+void ans_svmul(monolish::vector<T> &mx, T value, monolish::vector<T> &ans) {
+  if (mx.size() != ans.size()) {
+    throw std::runtime_error("x.size != y.size");
+  }
+
+  for (size_t i = 0; i < mx.size(); i++) {
+    ans[i] *= mx[i] * value;
+  }
+}
+
+template <typename T>
+bool test_send_svmul(const size_t size, double tol){
+
+  T value = 123.0;
+  monolish::vector<T> x(size, 0.1, 1.0);
+  monolish::vector<T> ans(size, 321.0);
+
+  monolish::vector<T> ans_tmp = ans.copy();
+  ans_svmul(x, value, ans_tmp);
+
+  monolish::util::send(x, ans);
+  ans *= x * value;
+  ans.recv();
+
+  return ans_check<T>(__func__, ans.data(), ans_tmp.data(), x.size(), tol);
+}
+
+template <typename T>
+bool test_svmul(const size_t size, double tol){
+
+  T value = 123.0;
+  monolish::vector<T> x(size, 0.1, 1.0);
+  monolish::vector<T> ans(size, 321.0);
+
+  monolish::vector<T> ans_tmp = ans.copy();
+  ans_svmul(x, value, ans_tmp);
+
+  ans *= x * value;
+
+  return ans_check<T>(__func__, ans.data(), ans_tmp.data(), x.size(), tol);
+}
