9fabf78b7cff2d2ca0f8683e77c0f094e3b303d1
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-10-23 08:40:46 +0900

    makefile update

--- test/matrix_blas/test.sh
+++ test/matrix_blas/test.sh
@@ -12,53 +12,59 @@ TEST_B=$(($RANDOM%100+100)) #100~200
 MAT_TEST_SIZE=($TEST_A $TEST_B)
 
 case $1 in
-    "mscal" ) 
-        for format in ${FORMAT[@]}; do
-            for prec in ${PREC[@]}; do
-                for M in ${MAT_TEST_SIZE[@]}; do
-                    for N in ${MAT_TEST_SIZE[@]}; do
-                        echo start $1 $2 $prec $format $M $N $3
-                        $PROFILER ./$1_$2.out $prec $format $M $N $3 1 || exit 1
-                    done
+    "matrix_common" ) 
+        echo start $1 $prec $format $M $N $3
+        $PROFILER ./$1_$2.out $prec $format $M $N $3 1 || exit 1
+        ;;
+
+    "matrix_blas" ) 
+        for M in ${MAT_TEST_SIZE[@]}; do
+            for N in ${MAT_TEST_SIZE[@]}; do
+                for K in ${MAT_TEST_SIZE[@]}; do
+                    $PROFILER ./$1_$2.out $M $N $K || exit 1
                 done
             done
         done
         ;;
-    "matvec" ) 
-        for format in ${FORMAT[@]}; do
-            for prec in ${PREC[@]}; do
-                for M in ${MAT_TEST_SIZE[@]}; do
-                    for N in ${MAT_TEST_SIZE[@]}; do
-                        echo start $1 $2 $prec $format $M $N $3
-                        $PROFILER ./$1_$2.out $prec $format $M $N $3 1 || exit 1
-                    done
+
+    "matrix_convert" ) 
+        for M in ${MAT_TEST_SIZE[@]}; do
+            for N in ${MAT_TEST_SIZE[@]}; do
+                echo start $1 $2 $prec $format $M $N $3
+                $PROFILER ./$1_$2.out $M $N || exit 1
+            done
+        done
+        ;;
+
+    "matrix_arithmetic" ) 
+        for M in ${MAT_TEST_SIZE[@]}; do
+            for N in ${MAT_TEST_SIZE[@]}; do
+                for K in ${MAT_TEST_SIZE[@]}; do
+                    $PROFILER ./$1_$2.out $M $N $K || exit 1
                 done
             done
         done
         ;;
-    "matadd" ) 
-        for format in ${FORMAT[@]}; do
-            for prec in ${PREC[@]}; do
-                for M in ${MAT_TEST_SIZE[@]}; do
-                    for N in ${MAT_TEST_SIZE[@]}; do
-                        echo start $1 $2 $prec $format $M $N $3
-                            $PROFILER ./$1_$2.out $prec $format $format $format $M $N $3 1 || exit 1
-                    done
-                done
+    "matrix_transpose" ) 
+        for M in ${MAT_TEST_SIZE[@]}; do
+            for N in ${MAT_TEST_SIZE[@]}; do
+                $PROFILER ./$1_$2.out $M $N $K || exit 1
             done
         done
         ;;
-    "matmul" ) 
-        for format in ${FORMAT[@]}; do
-            for prec in ${PREC[@]}; do
-                for M in ${MAT_TEST_SIZE[@]}; do
-                    for N in ${MAT_TEST_SIZE[@]}; do
-                        for K in ${MAT_TEST_SIZE[@]}; do
-                            echo start $1 $2 $prec $format $M $N $K $3
-                            $PROFILER ./$1_$2.out $prec $format Dense Dense $M $N $K $3 1 || exit 1
-                        done
-                    done
-                done
+
+    "matrix_math" ) 
+        for M in ${MAT_TEST_SIZE[@]}; do
+            for N in ${MAT_TEST_SIZE[@]}; do
+                $PROFILER ./$1_$2.out $M $N $K || exit 1
+            done
+        done
+        ;;
+
+    "matrix_ subvec_op" ) 
+        for M in ${MAT_TEST_SIZE[@]}; do
+            for N in ${MAT_TEST_SIZE[@]}; do
+                $PROFILER ./$1_$2.out $M $N $K || exit 1
             done
         done
         ;;
