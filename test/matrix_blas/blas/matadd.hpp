071060e2380d1880d6f8369cc5ce7077beeb9910
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-10-23 06:57:24 +0900

    matrix blas

---
+++ test/matrix_blas/blas/matadd.hpp
@@ -0,0 +1,86 @@
+#include "../../test_utils.hpp"
+
+template <typename T>
+void ans_matadd(const monolish::matrix::Dense<T> &A,
+             const monolish::matrix::Dense<T> &B,
+             monolish::matrix::Dense<T> &C) {
+
+  if (A.get_row() != B.get_row()) {
+    std::runtime_error("A.row != B.row");
+  }
+  if (A.get_col() != B.get_col()) {
+    std::runtime_error("A.col != B.col");
+  }
+
+  // MN=MN+MN
+  int M = A.get_row();
+  int N = A.get_col();
+
+  for (int i = 0; i < A.get_nnz(); i++) {
+    C.val[i] += A.val[i] * B.val[i];
+  }
+}
+
+template <typename MAT_A, typename MAT_B, typename MAT_C, typename T>
+bool test_send_matadd(const size_t M, const size_t N, double tol) {
+
+  size_t nnzrow = 81;
+  if ((nnzrow < M) && (nnzrow < N)) {
+    nnzrow = 81;
+  } else {
+    nnzrow = std::min({M, N}) - 1;
+  }
+
+  monolish::matrix::COO<T> seedA =
+      monolish::util::random_structure_matrix<T>(M, N, nnzrow, 1.0);
+
+  MAT_A A(seedA);
+  MAT_B B(seedA);
+  MAT_C C(seedA);
+
+  monolish::matrix::Dense<T> AA(seedA);
+  monolish::matrix::Dense<T> BB(seedA);
+  monolish::matrix::Dense<T> CC(seedA);
+
+  ans_matadd(AA, BB, CC);
+  monolish::matrix::COO<T> ansC(CC);
+
+  monolish::util::send(A, B, C);
+  monolish::blas::matadd(A, B, C);
+  C.recv();
+
+  monolish::matrix::COO<T> resultC(C);
+
+  return ans_check<T>(__func__, A.type(), resultC.val.data(), ansC.val.data(), ansC.get_nnz(), tol);
+}
+
+template <typename MAT_A, typename MAT_B, typename MAT_C, typename T>
+bool test_matadd(const size_t M, const size_t N, double tol) {
+
+  size_t nnzrow = 81;
+  if ((nnzrow < M) && (nnzrow < N)) {
+    nnzrow = 81;
+  } else {
+    nnzrow = std::min({M, N}) - 1;
+  }
+
+  monolish::matrix::COO<T> seedA =
+      monolish::util::random_structure_matrix<T>(M, N, nnzrow, 1.0);
+
+  MAT_A A(seedA);
+  MAT_B B(seedA);
+  MAT_C C(seedA);
+
+  monolish::matrix::Dense<T> AA(seedA);
+  monolish::matrix::Dense<T> BB(seedA);
+  monolish::matrix::Dense<T> CC(seedA);
+
+  ans_matadd(AA, BB, CC);
+  monolish::matrix::COO<T> ansC(CC);
+
+  monolish::blas::matadd(A, B, C);
+
+  monolish::matrix::COO<T> resultC(C);
+
+  return ans_check<T>(__func__, A.type(), resultC.val.data(), ansC.val.data(), ansC.get_nnz(), tol);
+}
---
+++ test/matrix_blas/blas/matvec.hpp
@@ -0,0 +1,87 @@
+#include "../../test_utils.hpp"
+
+template <typename T>
+void ans_matvec(monolish::matrix::Dense<T> &A, monolish::vector<T> &mx,
+             monolish::vector<T> &my) {
+
+  if (A.get_col() != mx.size()) {
+    std::runtime_error("A.col != x.size");
+  }
+  if (A.get_row() != my.size()) {
+    std::runtime_error("A.row != y.size");
+  }
+
+  T *x = mx.data();
+  T *y = my.data();
+  int M = A.get_row();
+  int N = A.get_col();
+
+  for (int i = 0; i < my.size(); i++)
+    y[i] = 0;
+
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) {
+      y[i] += A.val[N * i + j] * x[j];
+    }
+  }
+}
+
+template <typename MAT, typename T>
+bool test_send_matvec(const size_t M, const size_t N, double tol) {
+
+  size_t nnzrow = 81;
+  if (nnzrow < N) {
+    nnzrow = 81;
+  } else {
+    nnzrow = N - 1;
+  }
+
+  monolish::matrix::COO<T> seedA =
+      monolish::util::random_structure_matrix<T>(M, N, nnzrow, 1.0);
+
+  MAT A(seedA); // M*N matrix
+
+  monolish::vector<T> x(A.get_col(), 0.0, 1.0);
+  monolish::vector<T> y(A.get_row(), 0.0, 1.0);
+
+  monolish::vector<T> ansy(A.get_row());
+  ansy = y;
+
+  monolish::matrix::Dense<T> AA(seedA);
+  ans_matvec(AA, x, ansy);
+
+  monolish::util::send(A, x, y);
+  monolish::blas::matvec(A, x, y);
+  y.recv();
+
+  return ans_check<T>(__func__, A.type(), y.data(), ansy.data(), y.size(), tol);
+}
+
+template <typename MAT, typename T>
+bool test_matvec(const size_t M, const size_t N, double tol) {
+
+  size_t nnzrow = 81;
+  if (nnzrow < N) {
+    nnzrow = 81;
+  } else {
+    nnzrow = N - 1;
+  }
+
+  monolish::matrix::COO<T> seedA =
+      monolish::util::random_structure_matrix<T>(M, N, nnzrow, 1.0);
+
+  MAT A(seedA); // M*N matrix
+
+  monolish::vector<T> x(A.get_col(), 0.0, 1.0);
+  monolish::vector<T> y(A.get_row(), 0.0, 1.0);
+
+  monolish::vector<T> ansy(A.get_row());
+  ansy = y;
+
+  monolish::matrix::Dense<T> AA(seedA);
+  ans_matvec(AA, x, ansy);
+
+  monolish::blas::matvec(A, x, y);
+
+  return ans_check<T>(__func__, A.type(), y.data(), ansy.data(), y.size(), tol);
+}
