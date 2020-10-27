48ab8f6961f5ebcb56a0fb875eb9ddb9005b51a4
muripoLife <life.is.too.high.difficulty@gmail.com>
2020-10-27 21:02:59 +0900

    fixed pylint format.py

--- test/logger/logging/utils/formats.py
+++ test/logger/logging/utils/formats.py
@@ -1,3 +1,5 @@
+"""type convert."""
+
 import numpy as np
 import pandas as pd
 
@@ -5,12 +7,14 @@ def dict_list_to_ndarray(dict_list):
     """
         dict_list : [{}, {}, ...] -> array : [], ndarray : [[], [], [], ...]
     """
-    df = pd.DataFrame(dict_list)
-    column_array = np.array(df.columns)
-    values_ndarray = np.array([np.array(row) for index, row in df.iterrows()])
+    dataframe = pd.DataFrame(dict_list)
+    column_array = np.array(dataframe.columns)
+    values_ndarray = np.array([np.array(row) for index, row in dataframe.iterrows()])
     return column_array, values_ndarray
 
-
 def dict_list_to_dataframe(dict_list):
-    df = pd.DataFrame(dict_list)
-    return df
+    """
+        dict_list : [{}, {}, ...] -> DataFrame
+    """
+    dataframe = pd.DataFrame(dict_list)
+    return dataframe
