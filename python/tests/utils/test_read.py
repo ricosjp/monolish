e55e17894909672f5779abf0f4cbfd26835ca63b
muripoLife <life.is.too.high.difficulty@gmail.com>
2020-11-10 00:10:33 +0900

    add test_read

--- python/tests/utils/test_read.py
+++ python/tests/utils/test_read.py
@@ -4,20 +4,16 @@ import sys
 import json
 import yaml
 
-# from ...utils import read
-# from ....monolish_log_viewer.utils import read
 from monolish_log_viewer.utils import read
-
-# from unittest.mock import Mock
-# from .utils import read
-# from utils import read
-# from .monolish_log_viewer.utils import read
-# from monolish_log_viewer.utils import read
-# from .python.monolish_log_viewer.utils import read
-# from python.monolish_log_viewer.utils import read
 
 data_dir = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "/../test_data/base_data1.yml")
 
-with open(data_dir, "r") as file:
-    yaml_dict_list = read.reader(file, "xxx")
-    assert yaml_dict_list == []
+def test_read_unown_data():
+    with open(data_dir, "r") as file:
+        dict_list = read.reader(file, "xxx")
+        assert dict_list == []
+
+def test_read_normal_data():
+    with open(data_dir, "r") as file:
+        yaml_dict_list = read.reader(file, "yaml")
+        assert isinstance(yaml_dict_list, list) == True
