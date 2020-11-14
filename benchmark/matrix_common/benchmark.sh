16806a08b79a272c9335b86e52fc067c3a63e41a
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-11-14 18:13:30 +0900

    benchmark script bug fix ,closes #364

--- benchmark/matrix_common/benchmark.sh
+++ benchmark/matrix_common/benchmark.sh
@@ -12,24 +12,36 @@ case $1 in
     for format in ${FORMAT[@]}; do
       echo start $1 $format $2
       $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
     done
     ;;
   "mv_mul" ) 
     for format in ${FORMAT[@]}; do
       echo start $1 $format $2
       $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
     done
     ;;
   "transpose" ) 
     for format in ${FORMAT[@]}; do
       echo start $1 $format $2
       $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
     done
     ;;
   "m_tanh" ) 
     for format in ${FORMAT[@]}; do
       echo start $1 $format $2
-      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
     done
     ;;
 esac
