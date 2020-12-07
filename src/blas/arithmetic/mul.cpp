43a8b9e17f7fc8ae7eb237a35caa465bb7e6f273
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-12-07 11:36:02 +0900

    internal library, vector arithmetics

--- src/blas/arithmetic/mul.cpp
+++ src/blas/arithmetic/mul.cpp
@@ -2,148 +2,84 @@
 #include "../../internal/monolish_internal.hpp"
 
 namespace monolish {
-/////////////////////////////////////////////////
-// vec - scalar
-/////////////////////////////////////////////////
-template <typename T> vector<T> vector<T>::operator*(const T value) {
-  Logger &logger = Logger::get_instance();
-  logger.func_in(monolish_func);
+  /////////////////////////////////////////////////
+  // vec - scalar
+  /////////////////////////////////////////////////
+  template <typename T> vector<T> vector<T>::operator*(const T value) {
+    Logger &logger = Logger::get_instance();
+    logger.func_in(monolish_func);
 
-  vector<T> ans(val.size());
-
-  T *vald = val.data();
-  T *ansd = ans.data();
-  size_t size = val.size();
-
-  if (gpu_status == true) {
-#if MONOLISH_USE_GPU
-    ans.send();
-#pragma omp target teams distribute parallel for
-    for (size_t i = 0; i < size; i++) {
-      ansd[i] = vald[i] * value;
+    vector<T> ans(val.size());
+    if(gpu_status==true){
+      ans.send();
     }
-#else
-    throw std::runtime_error("error USE_GPU is false, but gpu_status == true");
-#endif
-  } else {
-#pragma omp parallel for
-    for (size_t i = 0; i < size; i++) {
-      ansd[i] = vald[i] * value;
-    }
+
+    internal::vmul(val.size(), val.data(), value, ans.data(), gpu_status);
+
+    logger.func_out();
+    return ans;
   }
 
-  logger.func_out();
-  return ans;
-}
+  template vector<double> vector<double>::operator*(const double value);
+  template vector<float> vector<float>::operator*(const float value);
 
-template vector<double> vector<double>::operator*(const double value);
-template vector<float> vector<float>::operator*(const float value);
+  template <typename T> void vector<T>::operator*=(const T value) {
+    Logger &logger = Logger::get_instance();
+    logger.func_in(monolish_func);
 
-template <typename T> void vector<T>::operator*=(const T value) {
-  Logger &logger = Logger::get_instance();
-  logger.func_in(monolish_func);
+    internal::vmul(val.size(), val.data(), value, val.data(), gpu_status);
 
-  vector<T> ans(val.size());
+    logger.func_out();
+  }
+  template void vector<double>::operator*=(const double value);
+  template void vector<float>::operator*=(const float value);
 
-  T *vald = val.data();
-  size_t size = val.size();
+  /////////////////////////////////////////////////
+  // vec - vec
+  /////////////////////////////////////////////////
 
-  if (gpu_status == true) {
-#if MONOLISH_USE_GPU
-#pragma omp target teams distribute parallel for
-    for (size_t i = 0; i < size; i++) {
-      vald[i] *= value;
+  template <typename T> vector<T> vector<T>::operator*(const vector<T> &vec) {
+    Logger &logger = Logger::get_instance();
+    logger.func_in(monolish_func);
+
+    // err
+    if (val.size() != vec.size()) {
+      throw std::runtime_error("error vector size is not same");
     }
-#else
-    throw std::runtime_error("error USE_GPU is false, but gpu_status == true");
-#endif
-  } else {
-#pragma omp parallel for
-    for (size_t i = 0; i < size; i++) {
-      vald[i] *= value;
+    if (gpu_status != vec.gpu_status) {
+      throw std::runtime_error("error gpu_status is not same");
     }
+
+    vector<T> ans(vec.size());
+    if(gpu_status==true){
+      ans.send();
+    }
+
+    internal::vmul(val.size(), val.data(), vec.data(), ans.data(), gpu_status);
+
+    logger.func_out();
+    return ans;
   }
 
-  logger.func_out();
-}
-template void vector<double>::operator*=(const double value);
-template void vector<float>::operator*=(const float value);
+  template vector<double> vector<double>::operator*(const vector<double> &vec);
+  template vector<float> vector<float>::operator*(const vector<float> &vec);
 
-/////////////////////////////////////////////////
-// vec - vec
-/////////////////////////////////////////////////
+  template <typename T> void vector<T>::operator*=(const vector<T> &vec) {
+    Logger &logger = Logger::get_instance();
+    logger.func_in(monolish_func);
 
-template <typename T> vector<T> vector<T>::operator*(const vector<T> &vec) {
-  Logger &logger = Logger::get_instance();
-  logger.func_in(monolish_func);
-
-  // err
-  if (val.size() != vec.size()) {
-    throw std::runtime_error("error vector size is not same");
-  }
-
-  vector<T> ans(vec.size());
-
-  const T *vecd = vec.data();
-  T *vald = val.data();
-  T *ansd = ans.data();
-  size_t size = vec.size();
-
-  if (gpu_status == true) {
-#if MONOLISH_USE_GPU
-    ans.send();
-#pragma omp target teams distribute parallel for
-    for (size_t i = 0; i < size; i++) {
-      ansd[i] = vald[i] * vecd[i];
+    // err
+    if (val.size() != vec.size()) {
+      throw std::runtime_error("error vector size is not same");
     }
-#else
-    throw std::runtime_error("error USE_GPU is false, but gpu_status == true");
-#endif
-  } else {
-#pragma omp parallel for
-    for (size_t i = 0; i < size; i++) {
-      ansd[i] = vald[i] * vecd[i];
+    if (gpu_status != vec.gpu_status) {
+      throw std::runtime_error("error gpu_status is not same");
     }
+
+    internal::vmul(val.size(), val.data(), vec.data(), val.data(), gpu_status);
+
+    logger.func_out();
   }
-
-  logger.func_out();
-  return ans;
-}
-
-template vector<double> vector<double>::operator*(const vector<double> &vec);
-template vector<float> vector<float>::operator*(const vector<float> &vec);
-
-template <typename T> void vector<T>::operator*=(const vector<T> &vec) {
-  Logger &logger = Logger::get_instance();
-  logger.func_in(monolish_func);
-
-  // err
-  if (val.size() != vec.size()) {
-    throw std::runtime_error("error vector size is not same");
-  }
-
-  const T *vecd = vec.data();
-  T *vald = val.data();
-  size_t size = vec.size();
-
-  if (gpu_status == true) {
-#if MONOLISH_USE_GPU
-#pragma omp target teams distribute parallel for
-    for (size_t i = 0; i < size; i++) {
-      vald[i] *= vecd[i];
-    }
-#else
-    throw std::runtime_error("error USE_GPU is false, but gpu_status == true");
-#endif
-  } else {
-#pragma omp parallel for
-    for (size_t i = 0; i < size; i++) {
-      vald[i] *= vecd[i];
-    }
-  }
-
-  logger.func_out();
-}
-template void vector<double>::operator*=(const vector<double> &vec);
-template void vector<float>::operator*=(const vector<float> &vec);
+  template void vector<double>::operator*=(const vector<double> &vec);
+  template void vector<float>::operator*=(const vector<float> &vec);
 } // namespace monolish
