5545ef66c3dcf786d2cb510177630bc87735f095
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-12-15 14:52:18 +0900

    matrix common -> matrix vml

--- benchmark/matrix_vml/benchmark.sh
+++ benchmark/matrix_vml/benchmark.sh
@@ -8,7 +8,7 @@ fi
 FORMAT=(Dense CRS)
 
 case $1 in
-  "convert" ) 
+  "mv_mul" ) 
     for format in ${FORMAT[@]}; do
       echo start $1 $format $2
       $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv
@@ -17,10 +17,10 @@ case $1 in
       fi
     done
     ;;
-  "transpose" ) 
+  "m_tanh" ) 
     for format in ${FORMAT[@]}; do
       echo start $1 $format $2
-      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
       if [ ${PIPESTATUS[0]} -ne 0 ]; then
         exit 1
       fi
