ddff6da9cccda5bec0506d7cf09cbeab20c6e7e3
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-12-19 01:38:47 +0900

    vsDiv bug fix

--- src/internal/arithmetic/d2f.sh
+++ src/internal/arithmetic/d2f.sh
@@ -3,6 +3,7 @@ cat double_arithmetic.cpp \
     | sed -e 's/vdAdd/vsAdd/g' \
     | sed -e 's/vdSub/vsSub/g' \
     | sed -e 's/vdMul/vsMul/g' \
+    | sed -e 's/vdDiv/vsDiv/g' \
     | sed -e 's/Dcopy/Scopy/g' \
     | sed -e 's/dcopy/scopy/g' \
     > float_arithmetic.cpp
