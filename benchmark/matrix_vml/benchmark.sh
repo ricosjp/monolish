335db284db054ac0fda1c7343a4b87bed3405120
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-12-20 23:19:22 +0900

    crs vml benchmark

--- benchmark/matrix_vml/benchmark.sh
+++ benchmark/matrix_vml/benchmark.sh
@@ -80,16 +80,124 @@ case $1 in
       fi
     done
     ;;
-  "mv_mul" ) 
+  "m_sqrt" ) 
     for format in ${FORMAT[@]}; do
       echo start $1 $format $2
-      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "mm_pow" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format $format $format | tee $1\_$format\_$format\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "sm_pow" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format $format | tee $1\_$format\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "m_sin" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "m_sinh" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "m_asin" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "m_asinh" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "m_tan" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
       if [ ${PIPESTATUS[0]} -ne 0 ]; then
         exit 1
       fi
     done
     ;;
   "m_tanh" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "m_atan" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "m_atanh" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "m_ceil" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "m_floor" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "m_sign" ) 
     for format in ${FORMAT[@]}; do
       echo start $1 $format $2
       $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
---
+++ benchmark/matrix_vml/vml_benchmark_gen.sh
@@ -0,0 +1,13 @@
+cat ./m_tanh.cpp | sed -e 's/tanh/sqrt/g' > m_sqrt.cpp
+cat ./m_tanh.cpp | sed -e 's/tanh/sin/g' > m_sin.cpp
+cat ./m_tanh.cpp | sed -e 's/tanh/sinh/g' > m_sinh.cpp
+cat ./m_tanh.cpp | sed -e 's/tanh/asin/g' > m_asin.cpp
+cat ./m_tanh.cpp | sed -e 's/tanh/asinh/g' > m_asinh.cpp
+cat ./m_tanh.cpp | sed -e 's/tanh/tan/g' > m_tan.cpp
+cat ./m_tanh.cpp | sed -e 's/tanh/atan/g' > m_atan.cpp
+cat ./m_tanh.cpp | sed -e 's/tanh/atanh/g' > m_atanh.cpp
+cat ./m_tanh.cpp | sed -e 's/tanh/ceil/g' > m_ceil.cpp
+cat ./m_tanh.cpp | sed -e 's/tanh/floor/g' > m_floor.cpp
+cat ./m_tanh.cpp | sed -e 's/tanh/sign/g' > m_sign.cpp
+cat ./mm_add.cpp | sed -e 's/add/pow/g' > vm_pow.cpp
+cat ./sm_add.cpp | sed -e 's/add/pow/g' > sm_pow.cpp
