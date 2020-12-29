5054f34e36216a90a68c4207d92e91405655848a
Ryo IGARASHI <rigarash@ricos.co.jp>
2020-12-29 11:02:26 +0900

    Use double2float for lapack, too.

--- include/double2float.sh
+++ include/double2float.sh
@@ -1,2 +1,4 @@
 sed 's/double precision/single precision/g' monolish_blas_double.hpp | \
     sed 's/double/float/g' > monolish_blas_float.hpp
+sed 's/double precision/single precision/g' monolish_lapack_double.hpp | \
+    sed 's/double/float/g' > monolish_lapack_float.hpp
