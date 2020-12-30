c641ece70ed0803b1349fc2f4bd63f150f9af2d4
Ryo IGARASHI <rigarash@ricos.co.jp>
2020-12-30 16:31:39 +0900

    Use lapacke interface on OSS.

--- src/lapack/syev/dense_syev.cpp
+++ src/lapack/syev/dense_syev.cpp
@@ -6,7 +6,8 @@
 #ifdef MONOLISH_USE_MKL
 #define dsyev_ dsyev
 #else
-void dsyev_(const char *, const char *, int *, double *, int *, double *, double *, int *, int *);
+//void dsyev_(const char *, const char *, int *, double *, int *, double *, double *, int *, int *);
+#include <lapacke.h>
 #endif
 
 namespace monolish {
@@ -14,16 +15,20 @@ namespace monolish {
 // double
 bool lapack::syev(const char *jobz, const char *uplo, matrix::Dense<double> &A,
                   vector<double> &W) {
-#if MONOLISH_USE_GPU
+#ifdef MONOLISH_USE_GPU
   throw std::logic_error("not yet implemented for GPU");
 #else // MONOLISH_USE_GPU
   int info = 0;
   int lwork = static_cast<int>((64+2)*A.get_row());
   std::vector<double> work(lwork);
   int size = static_cast<int>(A.get_row());
+  #ifdef MONOLISH_USE_MKL
   dsyev_(jobz, uplo, &size, A.val.data(), &size, W.data(), work.data(), &lwork, &info);
+  #else
+  info = LAPACKE_dsyev_work(LAPACK_COL_MAJOR, jobz[0], uplo[0], size, A.val.data(), size, W.data(), work.data(), lwork);
+  #endif
   return (info==0);
 #endif
 }
 
-} // namespace monolish    
+} // namespace monolish
