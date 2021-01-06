fa60cb4a852864c2271997a67089b1e87ecf2e89
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-01-07 03:37:56 +0900

    add: jacobi solver

--- src/equation/jacobi.cpp
+++ src/equation/jacobi.cpp
@@ -52,11 +52,56 @@ template <typename T>
 int equation::Jacobi<T>::monolish_Jacobi(matrix::CRS<T> &A, vector<T> &x,
                                          vector<T> &b) {
   Logger &logger = Logger::get_instance();
-  // nothing to do
-  (void)(&A);
-  (void)(&x);
-  (void)(&b);
   logger.solver_in(monolish_func);
+
+  if (A.get_row() != A.get_col()) {
+    throw std::runtime_error("error, A.row != A.col");
+  }
+  if (A.get_device_mem_stat() != x.get_device_mem_stat() &&
+      A.get_device_mem_stat() != b.get_device_mem_stat()) {
+    throw std::runtime_error("error, A.get_device_mem_stat != "
+                             "x.get_device_mem_stat != b.get_device_mem_stat");
+  }
+
+  vector<T> r(A.get_row(), 0.0);
+  vector<T> t(A.get_row(), 0.0);
+  vector<T> s(A.get_row(), 0.0);
+  vector<T> d(A.get_row(), 0.0);
+
+	auto bnrm2 = blas::nrm2(b);
+	bnrm2   = 1.0 / bnrm2;
+  T nrm2 = 0.0;
+
+  A.diag(d);
+  vml::reciprocal(d,d); // d[i] = 1/d[i]
+  //this->precond.apply_precond(r, z);
+
+  for (size_t iter = 0; iter < this->maxiter; iter++) {
+    /* x += D^{-1}(b - Ax) */
+    //lis_psolve(solver,x,s);
+    blas::matvec(A,s,t);
+    blas::axpyz(-1,t,b,r);
+    nrm2 = blas::nrm2(r);
+    vml::mul(r,d,r);
+    vml::add(x,r,x);
+
+    nrm2 = nrm2 * bnrm2;
+
+    if (this->print_rhistory == true) {
+      *this->rhistory_stream << iter + 1 << "\t" << std::scientific << nrm2
+        << std::endl;
+    }
+
+    if (nrm2 < this->tol && this->miniter <= iter + 1) {
+      logger.solver_out();
+      return MONOLISH_SOLVER_SUCCESS;
+    }
+
+    if (std::isnan(nrm2)) {
+      return MONOLISH_SOLVER_RESIDUAL_NAN;
+    }
+  }
+
   logger.solver_out();
   return MONOLISH_SOLVER_NOT_IMPL;
 }
