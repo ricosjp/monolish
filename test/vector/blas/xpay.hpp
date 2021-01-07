92c33db7745d87bda29b86ca5bfb7fe30d64f108
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-01-08 01:44:07 +0900

    add: xpay test, closes #474

---
+++ test/vector/blas/xpay.hpp
@@ -0,0 +1,43 @@
+#include "../../test_utils.hpp"
+#include "monolish_blas.hpp"
+
+template <typename T>
+void ans_xpay(double alpha, monolish::vector<T> &mx, monolish::vector<T> &my) {
+  if (mx.size() != my.size()) {
+    throw std::runtime_error("x.size != y.size");
+  }
+
+  for (size_t i = 0; i < mx.size(); i++) {
+    my[i] = alpha * mx[i] + my[i];
+  }
+}
+
+template <typename T> bool test_send_xpay(const size_t size, double tol) {
+
+  T alpha = 123.0;
+  monolish::vector<T> x(size, 0.0, 1.0);
+  monolish::vector<T> y(size, 0.0, 1.0);
+
+  monolish::vector<T> ansy = y;
+  ans_xpay(alpha, x, ansy);
+
+  monolish::util::send(x, y);
+  monolish::blas::xpay(alpha, x, y);
+  y.recv();
+
+  return ans_check<T>(__func__, y.data(), ansy.data(), y.size(), tol);
+}
+
+template <typename T> bool test_xpay(const size_t size, double tol) {
+
+  T alpha = 123.0;
+  monolish::vector<T> x(size, 0.0, 1.0);
+  monolish::vector<T> y(size, 0.0, 1.0);
+
+  monolish::vector<T> ansy = y;
+  ans_xpay(alpha, x, ansy);
+
+  monolish::blas::xpay(alpha, x, y);
+
+  return ans_check<T>(__func__, y.data(), ansy.data(), y.size(), tol);
+}
--- test/vector/vector_blas.cpp
+++ test/vector/vector_blas.cpp
@@ -1,5 +1,6 @@
 #include "blas/asum.hpp"
 #include "blas/axpy.hpp"
+#include "blas/xpay.hpp"
 #include "blas/axpyz.hpp"
 #include "blas/dot.hpp"
 #include "blas/nrm1.hpp"
@@ -46,6 +47,21 @@ int main(int argc, char **argv) {
     return 1;
   }
   if (test_send_axpy<float>(size, 1.0e-4) == false) {
+    return 1;
+  }
+
+  // xpay//
+  if (test_xpay<double>(size, 1.0e-8) == false) {
+    return 1;
+  }
+  if (test_xpay<float>(size, 1.0e-4) == false) {
+    return 1;
+  }
+
+  if (test_send_xpay<double>(size, 1.0e-8) == false) {
+    return 1;
+  }
+  if (test_send_xpay<float>(size, 1.0e-4) == false) {
     return 1;
   }
 
