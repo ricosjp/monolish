64605c6ee3041a8703ad378ce138ebddbd4bfe9f
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-01-19 23:39:18 +0900

    add: matrix vml max/min test

--- benchmark/matrix_vml/benchmark.sh
+++ benchmark/matrix_vml/benchmark.sh
@@ -215,4 +215,40 @@ case $1 in
       fi
     done
     ;;
+  "m_max" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "m_min" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format | tee $1\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "mm_max" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format $format $format | tee $1\_$format\_$format\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
+  "mm_min" ) 
+    for format in ${FORMAT[@]}; do
+      echo start $1 $format $2
+      $PROFILER ./$1_$2.out $format $format $format | tee $1\_$format\_$format\_$format\_$2.tsv 
+      if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        exit 1
+      fi
+    done
+    ;;
 esac
