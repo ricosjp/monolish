7a8dc5dbf26f9af9ab1fe38e0a38795ab0413e07
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-01-27 05:27:01 +0900

    fix: error check

--- src/blas/matrix/copy/double_copy.cpp
+++ src/blas/matrix/copy/double_copy.cpp
@@ -9,15 +9,8 @@ void blas::copy(const matrix::Dense<double> &A, matrix::Dense<double> &C) {
   logger.util_in(monolish_func);
 
   // err
-  if (A.get_row() != C.get_row()) {
-    throw std::runtime_error("error A.row != C.row");
-  }
-  if (A.get_col() != C.get_col()) {
-    throw std::runtime_error("error A.col != C.col");
-  }
-  if (A.get_device_mem_stat() != C.get_device_mem_stat()) {
-    throw std::runtime_error("error get_device_mem_stat() is not same");
-  }
+  assert(util::is_same_size(A, C));
+  assert(util::is_same_device_mem_stat(A, C));
 
   internal::vcopy(A.get_nnz(), A.val.data(), C.val.data(),
                   A.get_device_mem_stat());
@@ -31,18 +24,8 @@ void blas::copy(const matrix::CRS<double> &A, matrix::CRS<double> &C) {
   logger.util_in(monolish_func);
 
   // err
-  if (A.get_row() != C.get_row()) {
-    throw std::runtime_error("error A.row != C.row");
-  }
-  if (A.get_col() != C.get_col()) {
-    throw std::runtime_error("error A.col != C.col");
-  }
-  if (A.get_nnz() != C.get_nnz()) {
-    throw std::runtime_error("error A.nnz != C.nnz");
-  }
-  if (A.get_device_mem_stat() != C.get_device_mem_stat()) {
-    throw std::runtime_error("error get_device_mem_stat() is not same");
-  }
+  assert(util::is_same_size(A, C));
+  assert(util::is_same_device_mem_stat(A, C));
 
   internal::vcopy(A.get_nnz(), A.val.data(), C.val.data(),
                   A.get_device_mem_stat());
@@ -57,15 +40,8 @@ void blas::copy(const matrix::LinearOperator<double> &A,
   logger.util_in(monolish_func);
 
   // err
-  if (A.get_row() != C.get_row()) {
-    throw std::runtime_error("error A.row != C.row");
-  }
-  if (A.get_col() != C.get_col()) {
-    throw std::runtime_error("error A.col != C.col");
-  }
-  if (A.get_device_mem_stat() != C.get_device_mem_stat()) {
-    throw std::runtime_error("error get_device_mem_stat() is not same");
-  }
+  assert(util::is_same_size(A, C));
+  assert(util::is_same_device_mem_stat(A, C));
 
   if (A.get_matvec_init_flag()) {
     C.set_matvec(A.get_matvec());
