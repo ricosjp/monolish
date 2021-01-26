21a72546f838d9caa38187050a5edff1bfa7d66c
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-01-26 15:35:39 +0900

    fix: vml linear operator error

--- src/vml/double_linearoperator_linearoperator.cpp
+++ src/vml/double_linearoperator_linearoperator.cpp
@@ -15,25 +15,13 @@ void vml::add(const matrix::LinearOperator<double> &A,
   logger.func_in(monolish_func);
 
   // err
-  if (A.get_row() != B.get_row() && A.get_row() != C.get_row()) {
-    throw std::runtime_error("error A.row != B.row != C.row");
-  }
-  if (A.get_col() != B.get_col() && A.get_col() != C.get_col()) {
-    throw std::runtime_error("error A.col != B.col != C.col");
-  }
-  if (A.get_device_mem_stat() != B.get_device_mem_stat() ||
-      A.get_device_mem_stat() != C.get_device_mem_stat()) {
-    throw std::runtime_error("error matrix get_device_mem_stat() is not same");
-  }
+  assert(A.get_row() != B.get_row() || A.get_row() != C.get_row());
+  assert(A.get_col() != B.get_col() && A.get_col() != C.get_col());
+  assert(A.get_device_mem_stat() != B.get_device_mem_stat() ||
+      A.get_device_mem_stat() != C.get_device_mem_stat());
 
-  if (A.get_matvec_init_flag() != B.get_matvec_init_flag()) {
-    throw std::runtime_error("error A.matvec_init_flag != B.matvec_init_flag");
-  }
-
-  if (A.get_rmatvec_init_flag() != B.get_rmatvec_init_flag()) {
-    throw std::runtime_error(
-        "error A.rmatvec_init_flag != B.rmatvec_init_flag");
-  }
+  assert(A.get_matvec_init_flag() != B.get_matvec_init_flag());
+  assert(A.get_rmatvec_init_flag() != B.get_rmatvec_init_flag());
 
   if (A.get_matvec_init_flag()) {
     C.set_matvec([&](const vector<double> &VEC) {
@@ -77,25 +65,13 @@ void vml::sub(const matrix::LinearOperator<double> &A,
   logger.func_in(monolish_func);
 
   // err
-  if (A.get_row() != B.get_row() && A.get_row() != C.get_row()) {
-    throw std::runtime_error("error A.row != B.row != C.row");
-  }
-  if (A.get_col() != B.get_col() && A.get_col() != C.get_col()) {
-    throw std::runtime_error("error A.col != B.col != C.col");
-  }
-  if (A.get_device_mem_stat() != B.get_device_mem_stat() ||
-      A.get_device_mem_stat() != C.get_device_mem_stat()) {
-    throw std::runtime_error("error matrix get_device_mem_stat() is not same");
-  }
+  assert(A.get_row() != B.get_row() || A.get_row() != C.get_row());
+  assert(A.get_col() != B.get_col() && A.get_col() != C.get_col());
+  assert(A.get_device_mem_stat() != B.get_device_mem_stat() ||
+      A.get_device_mem_stat() != C.get_device_mem_stat());
 
-  if (A.get_matvec_init_flag() != B.get_matvec_init_flag()) {
-    throw std::runtime_error("error A.matvec_init_flag != B.matvec_init_flag");
-  }
-
-  if (A.get_rmatvec_init_flag() != B.get_rmatvec_init_flag()) {
-    throw std::runtime_error(
-        "error A.rmatvec_init_flag != B.rmatvec_init_flag");
-  }
+  assert(A.get_matvec_init_flag() != B.get_matvec_init_flag());
+  assert(A.get_rmatvec_init_flag() != B.get_rmatvec_init_flag());
 
   if (A.get_matvec_init_flag()) {
     C.set_matvec([&](const vector<double> &VEC) {
