cef5bb153d522cf3dda6a20f885ab8c1d5776ae2
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-01-29 06:14:38 +0900

    add: view1D prototype

--- include/common/monolish_view1D.hpp
+++ include/common/monolish_view1D.hpp
@@ -55,6 +55,12 @@ template <typename TYPE>
         size = last - first; 
       }
 
+      void resize(size_t ){
+        first = start;
+        last = end;
+        size = last - first; 
+        
+      }
       size_t get_size(){ return size;}
 
       size_t get_device_mem_stat(){ return target.get_device_mem_stat();}
