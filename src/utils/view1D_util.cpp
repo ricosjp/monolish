a87e9754656ae422d457f78a99c67a99fd31de5d
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-01-29 09:07:09 +0900

    add: view1D resize, data

--- src/utils/view1D_util.cpp
+++ src/utils/view1D_util.cpp
@@ -10,29 +10,56 @@ namespace monolish {
     return target.val.data()+first;
   }
 
-  template<> void view1D<monolish::vector<double>>::print_all(){
+  template<> void view1D<monolish::vector<double>>::print_all() const{
     for(size_t i=first; i<last; i++){
       std::cout << target[i] << std::endl;
     }
   }
 
-  template<> void view1D<monolish::matrix::Dense<double>>::print_all(){
+  template<> void view1D<monolish::matrix::Dense<double>>::print_all() const{
     for(size_t i=first; i<last; i++){
       std::cout << target.val[i] << std::endl;
     }
   }
 
-  template<> auto& view1D<monolish::vector<double>>::operator[](size_t i){
+  template<> auto& view1D<monolish::vector<double>>::operator[](const size_t i){
     if (get_device_mem_stat()) {
       throw std::runtime_error("Error, GPU vector cant use operator[]");
     }
     return target[first+i];
   }
 
-  template<> auto& view1D<monolish::matrix::Dense<double>>::operator[](size_t i){
+  template<> auto& view1D<monolish::matrix::Dense<double>>::operator[](const size_t i){
     if (get_device_mem_stat()) {
       throw std::runtime_error("Error, GPU vector cant use operator[]");
     }
     return target.val[first+i];
   }
+
+  template<> void view1D<monolish::vector<double>>::resize(size_t N){
+    assert(first+N <= target.size());
+    last = first + N;
+  }
+
+  template<> void view1D<monolish::matrix::Dense<double>>::resize(size_t N){
+    assert(first+N <= target.get_nnz());
+    last = first + N;
+  }
+
+  template<> void view1D<monolish::vector<double>>::set_first(size_t i){
+    first=i;
+  }
+  template<> void view1D<monolish::matrix::Dense<double>>::set_first(size_t i){
+    first=i;
+  }
+
+  template<> void view1D<monolish::vector<double>>::set_last(size_t i){
+    assert(first+i <= target.size());
+    last=i;
+  }
+  template<> void view1D<monolish::matrix::Dense<double>>::set_last(size_t i){
+    assert(first+i <= target.get_nnz());
+    last=i;
+  }
+
 }
