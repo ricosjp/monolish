379c1cd74cbc8b94be818e783daf5bb68610d5fd
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-02-01 00:01:32 +0900

    vecsub gen

--- src/blas/vector/addsub/Dvecadd2other.sh
+++ src/blas/vector/addsub/Dvecadd2other.sh
@@ -2,11 +2,11 @@ cat double_vecadd.cpp \
     | sed -e 's/double/float/g' \
     > float_vecadd.cpp
 
-cat double_vecsub.cpp \
+cat double_vecadd.cpp \
     | sed -e 's/vecadd/vecsub/g' \
     > double_vecsub.cpp
 
-cat double_vecsub.cpp \
+cat double_vecadd.cpp \
     | sed -e 's/double/float/g' \
     | sed -e 's/vecadd/vecsub/g' \
     > float_vecsub.cpp
---
+++ src/blas/vector/addsub/double_vecsub.cpp
@@ -0,0 +1,31 @@
+#include "../../../../include/monolish_blas.hpp"
+#include "../../../internal/monolish_internal.hpp"
+
+namespace monolish {
+// vecsub ///////////////////
+//
+namespace {
+template <typename F1, typename F2, typename F3>
+void vecsub_core(const F1 &a, const F2 &b, F3 &y) {
+  monolish::Logger &logger = monolish::Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  // err
+  assert(monolish::util::is_same_size(a, b, y));
+  assert(monolish::util::is_same_device_mem_stat(a, b, y));
+
+  monolish::internal::vadd(y.size(), a.data(), b.data(), y.data(),
+                           y.get_device_mem_stat());
+
+  logger.func_out();
+}
+} // namespace
+
+namespace blas {
+void vecsub(const vector<double> &a, const vector<double> &b,
+            vector<double> &y) {
+  vecsub_core(a, b, y);
+}
+} // namespace blas
+
+} // namespace monolish
