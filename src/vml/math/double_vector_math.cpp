0ce2f564d49020f2b8e687bb56cb8d1cc1298a0f
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-02-07 09:50:27 +0900

    fix: atanh, max bug

--- src/vml/math/double_vector_math.cpp
+++ src/vml/math/double_vector_math.cpp
@@ -37,6 +37,21 @@ void vml::pow(const vector<double> &a, const double alpha, vector<double> &y) {
 //////////////////////////////////////////
 // other
 //////////////////////////////////////////
+void vml::max(const vector<double> &a, const vector<double> &b,
+              vector<double> &y) {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  // err
+  assert(util::is_same_size(a, b, y));
+  assert(util::is_same_device_mem_stat(a, b, y));
+
+  internal::vmax(y.size(), a.data(), b.data(), y.data(),
+                 y.get_device_mem_stat());
+
+  logger.func_out();
+}
+
 double vml::max(const vector<double> &y) {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
