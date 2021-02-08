952555ca6b90119c584e272434889976e210027a
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-02-08 22:29:15 +0900

    delete: double/float blas header

--- include/monolish_blas.hpp
+++ include/monolish_blas.hpp
@@ -2,5 +2,3 @@
 #include "./blas/monolish_vector_blas.hpp"
 #include "./blas/monolish_matvec_blas.hpp"
 #include "./blas/monolish_matrix_blas.hpp"
-#include "./monolish_blas_double.hpp"
-#include "./monolish_blas_float.hpp"
