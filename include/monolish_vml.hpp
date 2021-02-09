125a1515682881da075c4441e823b0284ba0604b
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-02-09 09:19:44 +0900

    del: vml double float

--- include/monolish_vml.hpp
+++ include/monolish_vml.hpp
@@ -1,5 +1,5 @@
 #pragma once
 #include "./vml/monolish_vector_vml.hpp"
 #include "./vml/monolish_dense_vml.hpp"
-#include "./monolish_vml_double.hpp"
-#include "./monolish_vml_float.hpp"
+#include "./vml/monolish_crs_vml.hpp"
+#include "./vml/monolish_linearoperator_vml.hpp"
