65439c6f039120ddfc4f8d84bb7e5c60e0c05c94
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-02-14 17:47:37 +0900

    fix: gen vecaddsub

--- src/blas/vector/gen_vector_blas.sh
+++ src/blas/vector/gen_vector_blas.sh
@@ -1,13 +1,18 @@
 C=const
 
 echo "#pragma once
-#include \"../common/monolish_common.hpp\"
-#include <stdio.h>
-#include <vector>
-
-#if defined USE_MPI
-#include <mpi.h>
-#endif
+#include \"asum.hpp\"
+#include \"axpy.hpp\"
+#include \"axpyz.hpp\"
+#include \"copy.hpp\"
+#include \"dot.hpp\"
+#include \"nrm1.hpp\"
+#include \"nrm2.hpp\"
+#include \"scal.hpp\"
+#include \"sum.hpp\"
+#include \"vecadd.hpp\"
+#include \"vecsub.hpp\"
+#include \"xpay.hpp\"
 
 namespace monolish {
 namespace blas {
@@ -18,340 +23,181 @@ for prec in double float; do
   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
     for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
       for arg3 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-echo "void vecadd(const $arg1 &a, const $arg2 &b, $arg3 &y);"
+        if [ $prec = "double" ]
+        then
+          echo "void vecadd(const $arg1 &a, const $arg2 &b, $arg3 &y){vecadd_core(a, b, y);}"
+        else
+          echo "void vecadd(const $arg1 &a, const $arg2 &b, $arg3 &y){vecadd_core(a, b, y);}"
+        fi
       done
     done
   done
 done
+
+echo ""
 
 ## vecsub
 for prec in double float; do
   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
     for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
       for arg3 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-echo "void vecsub(const $arg1 &a, const $arg2 &b, $arg3 &y);"
+        if [ $prec = "double" ]
+        then
+          echo "void vecsub(const $arg1 &a, const $arg2 &b, $arg3 &y){vecsub_core(a, b, y);}"
+        else
+          echo "void vecsub(const $arg1 &a, const $arg2 &b, $arg3 &y){vecsub_core(a, b, y);}"
+        fi
       done
     done
   done
 done
-
-
-## copy
-echo "
-/**
- * @brief vector\<$prec\> copy (y=a)
- * @param a monolish vector\<$prec\> (size N)
- * @param y monolish vector\<$prec\> (size N)
- * @note
- * - # of computation: N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-      echo "void copy($C $arg1 &x, $arg2 &y);"
-    done
-  done
-done
-
-echo ""
-
-## asum
-echo "
-/**
- * @brief vector\<$prec\> asum (absolute sum)
- * @param x monolish vector\<$prec\> (size N)
- * @return The result of the asum
- * @note
- * - # of computation: N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    echo "void asum($C $arg1 &x, $prec &ans);"
-  done
-done
-
-echo ""
-
-## asum
-echo "
-/**
- * @brief vector\<$prec\> asum (absolute sum)
- * @param x monolish vector\<$prec\> (size N)
- * @param ans The result of the asum
- * @note
- * - # of computation: N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    echo "$prec asum($C $arg1 &x);"
-  done
-done
-
-## sum
-echo "
-/**
- * @brief vector\<$prec\> sum
- * @param x monolish vector\<$prec\> (size N)
- * @return The result of the sum
- * @note
- * - # of computation: N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    echo "void sum($C $arg1 &x, $prec &ans);"
-  done
-done
-
-echo ""
-
-## sum
-echo "
-/**
- * @brief vector\<$prec\> sum
- * @param x monolish vector\<$prec\> (size N)
- * @param ans The result of the sum
- * @note
- * - # of computation: N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    echo "$prec sum($C $arg1 &x);"
-  done
-done
-
-echo ""
-
-## axpy
-echo "
-/**
- * @brief axpy: y = ax + y
- * @param alpha scalar value
- * @param x monolish vector\<$prec\> (size N)
- * @param y monolish vector\<$prec\> (size N)
- * @note
- * - # of computation: N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-      echo "void axpy(const $prec alpha, const $arg1 &x, $arg2 &y);"
-    done
-  done
-done
-
-echo ""
-
-## axpyz
-echo "
-/**
- * @brief axpyz: z = ax + y
- * @param alpha scalar value
- * @param x monolish vector\<$prec\> (size N)
- * @param y monolish vector\<$prec\> (size N)
- * @param z monolish vector\<$prec\> (size N)
- * @note
- * - # of computation: 2N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-      for arg3 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-        echo "void axpyz(const $prec alpha, const $arg1 &x, const $arg2 &y, $arg3 &z);"
-      done
-    done
-  done
-done
-
-echo ""
-
-## dot
-echo "
-/**
- * @brief inner product (dot)
- * @param x monolish vector\<$prec\> (size N)
- * @param y monolish vector\<$prec\> (size N)
- * @return The result of the inner product product of x and y
- * @note
- * - # of computation: 2N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-      echo "void dot($C $arg1 &x, $C $arg2 &y, $prec &ans);"
-    done
-  done
-done
-
-echo ""
-
-## dot
-echo "
-/**
- * @brief inner product (dot)
- * @param x monolish vector\<$prec\> (size N)
- * @param y monolish vector\<$prec\> (size N)
- * @param ans The result of the inner product product of x and y
- * @note
- * - # of computation: 2N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-      echo "$prec dot($C $arg1 &x, $C $arg2 &y);"
-    done
-  done
-done
-
-echo ""
-
-## nrm1
-echo "
-/**
- * @brief nrm1: sum(abs(x[0:N]))
- * @param x monolish vector\<$prec\> (size N)
- * @return The result of the nrm1
- * @note
- * - # of computation: N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    echo "void nrm1($C $arg1 &x, $prec &ans);"
-  done
-done
-
-echo ""
-
-## nrm1
-echo "
-/**
- * @brief nrm1: sum(abs(x[0:N]))
- * @param x monolish vector\<$prec\> (size N)
- * @param ans The result of the nrm1
- * @note
- * - # of computation: N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    echo "$prec nrm1($C $arg1 &x);"
-  done
-done
-
-echo ""
-
-## nrm2
-echo "
-/**
- * @brief nrm2: ||x||_2
- * @param x monolish vector\<$prec\> (size N)
- * @return The result of the nrm2
- * @note
- * - # of computation: 2N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    echo "void nrm2($C $arg1 &x, $prec &ans);"
-  done
-done
-
-echo ""
-
-## nrm2
-echo "
-/**
- * @brief nrm2: ||x||_2
- * @param x monolish vector\<$prec\> (size N)
- * @param ans The result of the nrm2
- * @note
- * - # of computation: 2N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    echo "$prec nrm2($C $arg1 &x);"
-  done
-done
-
-echo ""
-
-## scal
-echo "
-/**
- * @brief scal: x = alpha * x
- * @param alpha scalar value
- * @param x monolish vector\<$prec\> (size N)
- * @note
- * - # of computation: N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-      echo "void scal(const $prec alpha, $arg1 &x);"
-  done
-done
-
-echo ""
-
-## xpay
-echo "
-/**
- * @brief xpay: y = x + ay
- * @param alpha scalar value
- * @param x monolish vector\<$prec\> (size N)
- * @param y monolish vector\<$prec\> (size N)
- * @note
- * - # of computation: 2N
- * - Multi-threading: true
- * - GPU acceleration: true
- *    - # of data transfer: 0
- */ "
-for prec in double float; do
-  for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-    for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
-      echo "void xpay(const $prec alpha, const $arg1 &x, $arg2 &y);"
-    done
-  done
-done
-
+# 
+# 
+# ## copy
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#       echo "void copy($C $arg1 &x, $arg2 &y);"
+#     done
+#   done
+# done
+# 
+# echo ""
+# 
+# ## asum
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     echo "void asum($C $arg1 &x, $prec &ans);"
+#   done
+# done
+# 
+# echo ""
+# 
+# ## asum
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     echo "$prec asum($C $arg1 &x);"
+#   done
+# done
+# 
+# ## sum
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     echo "void sum($C $arg1 &x, $prec &ans);"
+#   done
+# done
+# 
+# echo ""
+# 
+# ## sum
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     echo "$prec sum($C $arg1 &x);"
+#   done
+# done
+# 
+# echo ""
+# 
+# ## axpy
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#       echo "void axpy(const $prec alpha, const $arg1 &x, $arg2 &y);"
+#     done
+#   done
+# done
+# 
+# echo ""
+# 
+# ## axpyz
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#       for arg3 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#         echo "void axpyz(const $prec alpha, const $arg1 &x, const $arg2 &y, $arg3 &z);"
+#       done
+#     done
+#   done
+# done
+# 
+# echo ""
+# 
+# ## dot
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#       echo "void dot($C $arg1 &x, $C $arg2 &y, $prec &ans);"
+#     done
+#   done
+# done
+# 
+# echo ""
+# 
+# ## dot
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#       echo "$prec dot($C $arg1 &x, $C $arg2 &y);"
+#     done
+#   done
+# done
+# 
+# echo ""
+# 
+# ## nrm1
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     echo "void nrm1($C $arg1 &x, $prec &ans);"
+#   done
+# done
+# 
+# echo ""
+# 
+# ## nrm1
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     echo "$prec nrm1($C $arg1 &x);"
+#   done
+# done
+# 
+# echo ""
+# 
+# ## nrm2
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     echo "void nrm2($C $arg1 &x, $prec &ans);"
+#   done
+# done
+# 
+# echo ""
+# 
+# ## nrm2
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     echo "$prec nrm2($C $arg1 &x);"
+#   done
+# done
+# 
+# echo ""
+# 
+# ## scal
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#       echo "void scal(const $prec alpha, $arg1 &x);"
+#   done
+# done
+# 
+# echo ""
+# 
+# ## xpay
+# for prec in double float; do
+#   for arg1 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#     for arg2 in vector\<$prec\> view1D\<vector\<$prec\>,$prec\> view1D\<matrix::Dense\<$prec\>,$prec\>; do
+#       echo "void xpay(const $prec alpha, const $arg1 &x, $arg2 &y);"
+#     done
+#   done
+# done
+# 
 echo "
 } // namespace blas
 } // namespace monolish"
--- src/blas/vector/vecsub.hpp
+++ src/blas/vector/vecsub.hpp
@@ -1,9 +1,6 @@
 #include "../../../include/monolish_blas.hpp"
 #include "../../internal/monolish_internal.hpp"
 
-namespace monolish {
-// vecsub ///////////////////
-//
 namespace {
 template <typename F1, typename F2, typename F3>
 void vecsub_core(const F1 &a, const F2 &b, F3 &y) {
@@ -24,75 +21,3 @@ void vecsub_core(const F1 &a, const F2 &b, F3 &y) {
   logger.func_out();
 }
 } // namespace
-
-namespace blas {
-
-void vecsub(const vector<double> &a, const vector<double> &b,
-            vector<double> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const vector<double> &a, const vector<double> &b,
-            view1D<vector<double>, double> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const vector<double> &a, const view1D<vector<double>, double> &b,
-            vector<double> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const vector<double> &a, const view1D<vector<double>, double> &b,
-            view1D<vector<double>, double> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const view1D<vector<double>, double> &a, const vector<double> &b,
-            vector<double> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const view1D<vector<double>, double> &a, const vector<double> &b,
-            view1D<vector<double>, double> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const view1D<vector<double>, double> &a,
-            const view1D<vector<double>, double> &b, vector<double> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const view1D<vector<double>, double> &a,
-            const view1D<vector<double>, double> &b,
-            view1D<vector<double>, double> &y) {
-  vecsub_core(a, b, y);
-}
-
-void vecsub(const vector<float> &a, const vector<float> &b, vector<float> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const vector<float> &a, const vector<float> &b,
-            view1D<vector<float>, float> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const vector<float> &a, const view1D<vector<float>, float> &b,
-            vector<float> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const vector<float> &a, const view1D<vector<float>, float> &b,
-            view1D<vector<float>, float> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const view1D<vector<float>, float> &a, const vector<float> &b,
-            vector<float> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const view1D<vector<float>, float> &a, const vector<float> &b,
-            view1D<vector<float>, float> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const view1D<vector<float>, float> &a,
-            const view1D<vector<float>, float> &b, vector<float> &y) {
-  vecsub_core(a, b, y);
-}
-void vecsub(const view1D<vector<float>, float> &a,
-            const view1D<vector<float>, float> &b,
-            view1D<vector<float>, float> &y) {
-  vecsub_core(a, b, y);
-}
-
-} // namespace blas
-} // namespace monolish
