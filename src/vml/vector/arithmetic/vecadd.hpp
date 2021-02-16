0927b81a7db56b5132b23111a5f0cac68e00561e
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-02-16 23:24:04 +0900

    vml: gen vector bug fix

--- src/vml/vector/arithmetic/vecadd.hpp
+++ src/vml/vector/arithmetic/vecadd.hpp
@@ -1,9 +1,20 @@
-#include "../../../include/monolish_vml.hpp"
-#include "../../internal/monolish_internal.hpp"
-
 namespace monolish {
 
 namespace {
+template <typename F1, typename F2, typename F3>
+void svadd_core(const F1 &a, const F2 alpha, F3 &y) {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  // err
+  assert(util::is_same_size(a, y));
+  assert(util::is_same_device_mem_stat(a, y));
+
+  internal::vadd(y.size(), a.data(), alpha, y.data(), y.get_device_mem_stat());
+
+  logger.func_out();
+}
+
 template <typename F1, typename F2, typename F3>
 void vvadd_core(const F1 &a, const F2 &b, F3 &y) {
   Logger &logger = Logger::get_instance();
@@ -18,18 +29,6 @@ void vvadd_core(const F1 &a, const F2 &b, F3 &y) {
 
   logger.func_out();
 }
-
 } // namespace
-
-namespace vml {
-void add(const vector<double> &a, const vector<double> &b, vector<double> &y) {
-  vvadd_core(a, b, y);
-}
-
-void add(const vector<float> &a, const vector<float> &b, vector<float> &y) {
-  vvadd_core(a, b, y);
-}
-
-} // namespace vml
 
 } // namespace monolish
--- src/vml/vector/arithmetic/vecsub.hpp
+++ src/vml/vector/arithmetic/vecsub.hpp
@@ -1,9 +1,20 @@
-#include "../../../include/monolish_vml.hpp"
-#include "../../internal/monolish_internal.hpp"
-
 namespace monolish {
 
 namespace {
+template <typename F1, typename F2, typename F3>
+void svsub_core(const F1 &a, const F2 alpha, F3 &y) {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  // err
+  assert(util::is_same_size(a, y));
+  assert(util::is_same_device_mem_stat(a, y));
+
+  internal::vsub(y.size(), a.data(), alpha, y.data(), y.get_device_mem_stat());
+
+  logger.func_out();
+}
+
 template <typename F1, typename F2, typename F3>
 void vvsub_core(const F1 &a, const F2 &b, F3 &y) {
   Logger &logger = Logger::get_instance();
@@ -18,18 +29,6 @@ void vvsub_core(const F1 &a, const F2 &b, F3 &y) {
 
   logger.func_out();
 }
-
 } // namespace
-
-namespace vml {
-void sub(const vector<double> &a, const vector<double> &b, vector<double> &y) {
-  vvsub_core(a, b, y);
-}
-
-void sub(const vector<float> &a, const vector<float> &b, vector<float> &y) {
-  vvsub_core(a, b, y);
-}
-
-} // namespace vml
 
 } // namespace monolish
