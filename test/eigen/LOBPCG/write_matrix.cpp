261a32a2df986a80d9825b0c4e74d09a0e9ac077
Ryo IGARASHI <rigarash@ricos.co.jp>
2021-02-18 10:05:50 +0900

    Apply clang-format.

--- test/eigen/LOBPCG/write_matrix.cpp
+++ test/eigen/LOBPCG/write_matrix.cpp
@@ -1,5 +1,5 @@
-#include <monolish_blas.hpp>
 #include <iostream>
+#include <monolish_blas.hpp>
 #include <string>
 
 int main(int argc, char *argv[]) {
@@ -9,10 +9,14 @@ int main(int argc, char *argv[]) {
 
   int DIM = std::stoi(argv[1]);
 
-  monolish::matrix::COO<double> COO_A = monolish::util::toeplitz_plus_hankel_matrix<double>(DIM, 1.0, -1.0 / 3.0, -1.0 / 6.0);
+  monolish::matrix::COO<double> COO_A =
+      monolish::util::toeplitz_plus_hankel_matrix<double>(DIM, 1.0, -1.0 / 3.0,
+                                                          -1.0 / 6.0);
   std::string fileA("A.mtx");
   COO_A.print_all(fileA);
-  monolish::matrix::COO<double> COO_B = monolish::util::toeplitz_plus_hankel_matrix<double>(DIM, 11.0 / 20.0, 13.0 / 60.0, 1.0 / 120.0);
+  monolish::matrix::COO<double> COO_B =
+      monolish::util::toeplitz_plus_hankel_matrix<double>(
+          DIM, 11.0 / 20.0, 13.0 / 60.0, 1.0 / 120.0);
   std::string fileB("B.mtx");
   COO_B.print_all(fileB);
   return 0;
