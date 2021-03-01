af6b3c0ce274d6b100583ba806ef435ef93e97c2
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-03-01 18:14:14 +0900

    fix: math2op view1D offset bug

--- src/vml/vector/math_1_3op/vecmax.hpp
+++ src/vml/vector/math_1_3op/vecmax.hpp
@@ -11,7 +11,7 @@ void vvmax_core(const F1 &a, const F2 &b, F3 &y) {
   assert(util::is_same_size(a, b, y));
   assert(util::is_same_device_mem_stat(a, b, y));
 
-  internal::vmax(y.size(), a.data(), b.data(), y.data(),
+  internal::vmax(y.size(), a.data()+a.get_offset(), b.data()+b.get_offset(), y.data()+y.get_offset(),
                  y.get_device_mem_stat());
 
   logger.func_out();
@@ -21,7 +21,7 @@ template <typename F1, typename F2> F2 vmax_core(const F1 &y) {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
-  F2 ret = internal::vmax(y.size(), y.data(), y.get_device_mem_stat());
+  F2 ret = internal::vmax(y.size(), y.data()+y.get_offset(), y.get_device_mem_stat());
 
   logger.func_out();
   return ret;
