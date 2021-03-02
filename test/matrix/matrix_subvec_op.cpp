289d6ba1688cda67f9ba106bea9abf8dfb8b7953
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-03-03 03:16:22 +0900

    delete: row/col arithmetics test

--- test/matrix/matrix_subvec_op.cpp
+++ test/matrix/matrix_subvec_op.cpp
@@ -11,47 +11,7 @@ template <typename T> void ans_subvec_op(monolish::vector<T> &vec) {
 }
 
 template <typename T>
-bool test_subvec_row(const size_t M, const size_t N, double tol) {
-  monolish::matrix::Dense<T> A(M, N, 10.0); // M*N matrix
-
-  monolish::vector<T> vec(A.get_col(), 2.0);
-  monolish::vector<T> ansvec(A.get_col(), 2.0);
-
-  A.row(A.get_row() - 1, ansvec);
-  ans_subvec_op(ansvec);
-
-  A.row_add(A.get_row() - 1, 3.0);
-  A.row_mul(A.get_row() - 1, 2.0);
-  A.row_sub(A.get_row() - 1, 1.0);
-  A.row_div(A.get_row() - 1, 2.0);
-  A.row(A.get_row() - 1, vec);
-
-  return ans_check<T>(__func__, A.type(), ansvec.data(), vec.data(), vec.size(),
-                      tol);
-}
-
-template <typename T>
-bool test_subvec_col(const size_t M, const size_t N, double tol) {
-  monolish::matrix::Dense<T> A(M, N, 10.0); // M*N matrix
-
-  monolish::vector<T> vec(A.get_row(), 2.0);
-  monolish::vector<T> ansvec(A.get_row(), 2.0);
-
-  A.col(A.get_col() - 1, ansvec);
-  ans_subvec_op(ansvec);
-
-  A.col_add(A.get_col() - 1, 3.0);
-  A.col_mul(A.get_col() - 1, 2.0);
-  A.col_sub(A.get_col() - 1, 1.0);
-  A.col_div(A.get_col() - 1, 2.0);
-  A.col(A.get_col() - 1, vec);
-
-  return ans_check<T>(__func__, A.type(), ansvec.data(), vec.data(), vec.size(),
-                      tol);
-}
-
-template <typename T>
-bool test_subvec_diag(const size_t M, const size_t N, double tol) {
+bool test_diag(const size_t M, const size_t N, double tol) {
   monolish::matrix::Dense<T> A(M, N, 10.0); // M*N matrix
 
   monolish::vector<T> vec(A.get_row(), 2.0);
@@ -85,27 +45,10 @@ int main(int argc, char **argv) {
 
   std::cout << "M=" << M << ", N=" << N << std::endl;
 
-  // row //
-  if (test_subvec_row<double>(M, N, 1.0e-8) == false) {
+  if (test_diag<double>(M, M, 1.0e-8) == false) {
     return 1;
   }
-  if (test_subvec_row<float>(M, N, 1.0e-8) == false) {
-    return 1;
-  }
-
-  // col //
-  if (test_subvec_col<double>(M, N, 1.0e-8) == false) {
-    return 1;
-  }
-  if (test_subvec_col<float>(M, N, 1.0e-8) == false) {
-    return 1;
-  }
-
-  // diag (only square now) //
-  if (test_subvec_diag<double>(M, M, 1.0e-8) == false) {
-    return 1;
-  }
-  if (test_subvec_diag<float>(M, M, 1.0e-8) == false) {
+  if (test_diag<float>(M, M, 1.0e-8) == false) {
     return 1;
   }
 
