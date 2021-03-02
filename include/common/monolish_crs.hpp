833d01f52216a57d4e7e1660f6fea2af100a8bbd
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-03-02 14:25:13 +0900

    add: crs n-origin constructor and monolish vector constructor

--- include/common/monolish_crs.hpp
+++ include/common/monolish_crs.hpp
@@ -104,22 +104,23 @@ public:
       const int *colind, const Float *value);
 
   /**
-   * @brief Create CRS matrix from std::vector, also compute the hash
+   * @brief Create CRS matrix from array, also compute the hash
    * @param M # of row
    * @param N # of col
-   * @param rowptr n-origin row_ptr, which stores the starting points of the rows of the
+   * @param NNZ # of non-zero elements
+   * @param rowptr row_ptr, which stores the starting points of the rows of the
    *arrays value and col_ind (size M+1)
    * @param colind n-origin col_ind, which stores the column numbers of the non-zero
    *elements (size nnz)
-   * @param value n-origin value index, which stores the non-zero elements (size nnz)
-   * @param origin n-origin
+   * @param value value index, which stores the non-zero elements (size nnz)
    * @note
-   * - # of computation: (M+1)+2nnz + (M+1)+nnz (compute hash) + nnz (compute origin)
+   * - # of computation: (M+1)+2nnz + (M+1)+nnz (compute hash) + nnz(compute origin)
    * - Multi-threading: false
    * - GPU acceleration: false
    **/
-  CRS(const size_t M, const size_t N, const std::vector<int> rowptr,
-      const std::vector<int> colind, const std::vector<Float> value, const size_t origin);
+  CRS(const size_t M, const size_t N, const size_t NNZ, const int *rowptr,
+      const int *colind, const Float *value, const size_t origin);
+
 
   /**
    * @brief Create CRS matrix from std::vector, also compute the hash
@@ -135,8 +136,8 @@ public:
    * - Multi-threading: false
    * - GPU acceleration: false
    **/
-  CRS(const size_t M, const size_t N, const std::vector<int> rowptr,
-      const std::vector<int> colind, const std::vector<Float> value);
+  CRS(const size_t M, const size_t N, const std::vector<int> &rowptr,
+      const std::vector<int> &colind, const std::vector<Float> &value);
 
   /**
    * @brief Create CRS matrix from std::vector, also compute the hash
@@ -152,8 +153,8 @@ public:
    * - Multi-threading: false
    * - GPU acceleration: false
    **/
-  CRS(const size_t M, const size_t N, const std::vector<int> rowptr,
-      const std::vector<int> colind, const vector<Float> value);
+  CRS(const size_t M, const size_t N, const std::vector<int> &rowptr,
+      const std::vector<int> &colind, const vector<Float> &value);
 
   /**
    * @brief Convert CRS matrix from COO matrix, also compute the hash
--- src/utils/crs_constructor.cpp
+++ src/utils/crs_constructor.cpp
@@ -49,33 +49,6 @@ template CRS<float>::CRS(const size_t M, const size_t N, const size_t NNZ,
                          const float *value);
 
 template <typename T>
-CRS<T>::CRS(const size_t M, const size_t N, const std::vector<int> rowptr,
-            const std::vector<int> colind, const std::vector<T> value) {
-  Logger &logger = Logger::get_instance();
-  logger.util_in(monolish_func);
-  rowN = M;
-  colN = N;
-  nnz = value.size();
-  gpu_status = false;
-  row_ptr.resize(M + 1);
-  col_ind.resize(nnz);
-  val.resize(nnz);
-  std::copy(rowptr.data(), rowptr.data() + (M + 1), row_ptr.begin());
-  std::copy(colind.data(), colind.data() + nnz, col_ind.begin());
-  std::copy(value.data(), value.data() + nnz, val.begin());
-  compute_hash();
-  logger.util_out();
-}
-template CRS<double>::CRS(const size_t M, const size_t N,
-                          const std::vector<int> rowptr,
-                          const std::vector<int> colind,
-                          const std::vector<double> value);
-template CRS<float>::CRS(const size_t M, const size_t N,
-                         const std::vector<int> rowptr,
-                         const std::vector<int> colind,
-                         const std::vector<float> value);
-
-template <typename T>
 CRS<T>::CRS(const size_t M, const size_t N, const size_t NNZ, const int *rowptr,
             const int *colind, const T *value, const size_t origin) {
   Logger &logger = Logger::get_instance();
@@ -106,10 +79,9 @@ template CRS<float>::CRS(const size_t M, const size_t N, const size_t NNZ,
                          const int *rowptr, const int *colind,
                          const float *value, const size_t origin);
 
-
 template <typename T>
-CRS<T>::CRS(const size_t M, const size_t N, const std::vector<int> rowptr,
-            const std::vector<int> colind, const vector<T> value) {
+CRS<T>::CRS(const size_t M, const size_t N, const std::vector<int> &rowptr,
+            const std::vector<int> &colind, const std::vector<T> &value) {
   Logger &logger = Logger::get_instance();
   logger.util_in(monolish_func);
   rowN = M;
@@ -126,13 +98,41 @@ CRS<T>::CRS(const size_t M, const size_t N, const std::vector<int> rowptr,
   logger.util_out();
 }
 template CRS<double>::CRS(const size_t M, const size_t N,
-                          const std::vector<int> rowptr,
-                          const std::vector<int> colind,
-                          const vector<double> value);
+                          const std::vector<int> &rowptr,
+                          const std::vector<int> &colind,
+                          const std::vector<double> &value);
 template CRS<float>::CRS(const size_t M, const size_t N,
-                         const std::vector<int> rowptr,
-                         const std::vector<int> colind,
-                         const vector<float> value);
+                         const std::vector<int> &rowptr,
+                         const std::vector<int> &colind,
+                         const std::vector<float> &value);
+
+template <typename T>
+CRS<T>::CRS(const size_t M, const size_t N, const std::vector<int> &rowptr,
+            const std::vector<int> &colind, const vector<T> &value) {
+  Logger &logger = Logger::get_instance();
+  logger.util_in(monolish_func);
+  rowN = M;
+  colN = N;
+  nnz = value.size();
+  gpu_status = false;
+  row_ptr.resize(M + 1);
+  col_ind.resize(nnz);
+  val.resize(nnz);
+  std::copy(rowptr.data(), rowptr.data() + (M + 1), row_ptr.begin());
+  std::copy(colind.data(), colind.data() + nnz, col_ind.begin());
+  std::copy(value.data(), value.data() + nnz, val.begin());
+  compute_hash();
+  logger.util_out();
+}
+
+template CRS<double>::CRS(const size_t M, const size_t N,
+                          const std::vector<int> &rowptr,
+                          const std::vector<int> &colind,
+                          const vector<double> &value);
+template CRS<float>::CRS(const size_t M, const size_t N,
+                         const std::vector<int> &rowptr,
+                         const std::vector<int> &colind,
+                         const vector<float> &value);
 
 // copy constructor
 template <typename T> CRS<T>::CRS(const CRS<T> &mat) {
