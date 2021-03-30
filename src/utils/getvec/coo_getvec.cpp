66ac5823a6b30aa947562d28bd46a6c5e2ab8418
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-03-30 20:47:22 +0900

    add: view1D row/col/diag in COO

--- src/utils/getvec/coo_getvec.cpp
+++ src/utils/getvec/coo_getvec.cpp
@@ -9,18 +9,66 @@ template <typename T> void COO<T>::diag(vector<T> &vec) const {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
-  std::size_t s = get_row() > get_col() ? get_col() : get_row();
+  size_t s = get_row() > get_col() ? get_col() : get_row();
   assert(s == vec.size());
 
-  for (std::size_t nz = 0; nz < get_nnz(); ++nz) {
+  for (size_t i = 0; i < vec.size(); i++) {
+    vec[i] = 0;
+  }
+
+  for (size_t nz = 0; nz < get_nnz(); ++nz) {
+    if (get_row_ptr()[nz] == get_col_ind()[nz]) {
+      vec[get_row_ptr()[nz]] = get_val_ptr()[nz];
+    }
+  }
+
+  logger.func_out();
+}
+template void monolish::matrix::COO<double>::diag(vector<double> &vec) const;
+template void monolish::matrix::COO<float>::diag(vector<float> &vec) const;
+
+template <typename T> void COO<T>::diag(view1D<vector<T>, T> &vec) const {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  size_t s = get_row() > get_col() ? get_col() : get_row();
+  assert(s == vec.size());
+
+  for (size_t i = 0; i < vec.size(); i++) {
+    vec[i] = 0;
+  }
+
+  for (size_t nz = 0; nz < get_nnz(); ++nz) {
     if (get_row_ptr()[nz] == get_col_ind()[nz]) {
       vec[get_row_ptr()[nz]] = get_val_ptr()[nz];
     }
   }
   logger.func_out();
 }
-template void monolish::matrix::COO<double>::diag(vector<double> &vec) const;
-template void monolish::matrix::COO<float>::diag(vector<float> &vec) const;
+template void monolish::matrix::COO<double>::diag(view1D<vector<double>, double> &vec) const;
+template void monolish::matrix::COO<float>::diag(view1D<vector<float>, float> &vec) const;
+
+template <typename T> void COO<T>::diag(view1D<matrix::Dense<T>, T> &vec) const {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  size_t s = get_row() > get_col() ? get_col() : get_row();
+  assert(s == vec.size());
+
+  for (size_t i = 0; i < vec.size(); i++) {
+    vec[i] = 0;
+  }
+
+  for (size_t nz = 0; nz < get_nnz(); ++nz) {
+    if (get_row_ptr()[nz] == get_col_ind()[nz]) {
+      vec[get_row_ptr()[nz]] = get_val_ptr()[nz];
+    }
+  }
+
+  logger.func_out();
+}
+template void monolish::matrix::COO<double>::diag(view1D<matrix::Dense<double>, double> &vec) const;
+template void monolish::matrix::COO<float>::diag(view1D<matrix::Dense<float>, float> &vec) const;
 
 // row
 template <typename T> void COO<T>::row(const size_t r, vector<T> &vec) const {
@@ -28,8 +76,12 @@ template <typename T> void COO<T>::row(const size_t r, vector<T> &vec) const {
   logger.func_in(monolish_func);
 
   assert(get_col() == vec.size());
+  
+  for (size_t i = 0; i < vec.size(); i++) {
+    vec[i] = 0;
+  }
 
-  for (std::size_t nz = 0; nz < get_nnz(); ++nz) {
+  for (size_t nz = 0; nz < get_nnz(); ++nz) {
     if (get_row_ptr()[nz] == static_cast<int>(r)) {
       vec[get_col_ind()[nz]] = get_val_ptr()[nz];
     }
@@ -41,6 +93,50 @@ template void monolish::matrix::COO<double>::row(const size_t r,
 template void monolish::matrix::COO<float>::row(const size_t r,
                                                 vector<float> &vec) const;
 
+template <typename T> void COO<T>::row(const size_t r, view1D<vector<T>,T> &vec) const {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  assert(get_col() == vec.size());
+  
+  for (size_t i = 0; i < vec.size(); i++) {
+    vec[i] = 0;
+  }
+
+  for (size_t nz = 0; nz < get_nnz(); ++nz) {
+    if (get_row_ptr()[nz] == static_cast<int>(r)) {
+      vec[get_col_ind()[nz]] = get_val_ptr()[nz];
+    }
+  }
+  logger.func_out();
+}
+template void monolish::matrix::COO<double>::row(const size_t r,
+                                                 view1D<vector<double>,double> &vec) const;
+template void monolish::matrix::COO<float>::row(const size_t r,
+                                                view1D<vector<float>, float> &vec) const;
+
+template <typename T> void COO<T>::row(const size_t r, view1D<matrix::Dense<T>,T> &vec) const {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  assert(get_col() == vec.size());
+  
+  for (size_t i = 0; i < vec.size(); i++) {
+    vec[i] = 0;
+  }
+
+  for (size_t nz = 0; nz < get_nnz(); ++nz) {
+    if (get_row_ptr()[nz] == static_cast<int>(r)) {
+      vec[get_col_ind()[nz]] = get_val_ptr()[nz];
+    }
+  }
+  logger.func_out();
+}
+template void monolish::matrix::COO<double>::row(const size_t r,
+                                                 view1D<matrix::Dense<double>,double> &vec) const;
+template void monolish::matrix::COO<float>::row(const size_t r,
+                                                 view1D<matrix::Dense<float>,float> &vec) const;
+
 // col
 template <typename T> void COO<T>::col(const size_t c, vector<T> &vec) const {
   Logger &logger = Logger::get_instance();
@@ -48,7 +144,11 @@ template <typename T> void COO<T>::col(const size_t c, vector<T> &vec) const {
 
   assert(get_row() == vec.size());
 
-  for (std::size_t nz = 0; nz < get_nnz(); ++nz) {
+  for (size_t i = 0; i < vec.size(); i++) {
+    vec[i] = 0;
+  }
+
+  for (size_t nz = 0; nz < get_nnz(); ++nz) {
     if (get_col_ind()[nz] == static_cast<int>(c)) {
       vec[get_row_ptr()[nz]] = get_val_ptr()[nz];
     }
@@ -59,5 +159,49 @@ template void monolish::matrix::COO<double>::col(const size_t c,
                                                  vector<double> &vec) const;
 template void monolish::matrix::COO<float>::col(const size_t c,
                                                 vector<float> &vec) const;
+
+template <typename T> void COO<T>::col(const size_t c, view1D<vector<T>,T> &vec) const {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  assert(get_row() == vec.size());
+
+  for (size_t i = 0; i < vec.size(); i++) {
+    vec[i] = 0;
+  }
+
+  for (size_t nz = 0; nz < get_nnz(); ++nz) {
+    if (get_col_ind()[nz] == static_cast<int>(c)) {
+      vec[get_row_ptr()[nz]] = get_val_ptr()[nz];
+    }
+  }
+  logger.func_out();
+}
+template void monolish::matrix::COO<double>::col(const size_t c,
+                                                 view1D<vector<double>,double> &vec) const;
+template void monolish::matrix::COO<float>::col(const size_t c,
+                                                view1D<vector<float>,float> &vec) const;
+
+template <typename T> void COO<T>::col(const size_t c, view1D<matrix::Dense<T>,T> &vec) const {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  assert(get_row() == vec.size());
+
+  for (size_t i = 0; i < vec.size(); i++) {
+    vec[i] = 0;
+  }
+
+  for (size_t nz = 0; nz < get_nnz(); ++nz) {
+    if (get_col_ind()[nz] == static_cast<int>(c)) {
+      vec[get_row_ptr()[nz]] = get_val_ptr()[nz];
+    }
+  }
+  logger.func_out();
+}
+template void monolish::matrix::COO<double>::col(const size_t c,
+                                                 view1D<matrix::Dense<double>,double> &vec) const;
+template void monolish::matrix::COO<float>::col(const size_t c,
+                                                 view1D<matrix::Dense<float>,float> &vec) const;
 } // namespace matrix
 } // namespace monolish
