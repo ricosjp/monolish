90ac531d2568fb590e3e4238b041a0558f044dd4
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-03-31 17:32:24 +0900

    chg: generate add2other

---
+++ src/blas/matrix/diag_op/dense_diag_scalar_div.cpp
@@ -0,0 +1,39 @@
+#include "../../../../include/monolish_blas.hpp"
+#include "../../../internal/monolish_internal.hpp"
+
+namespace monolish {
+namespace matrix {
+
+// div scalar
+template <typename T> void Dense<T>::diag_div(const T alpha) {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  T *vald = val.data();
+  const size_t N = get_col();
+  const size_t Len = get_row() > get_col() ? get_row() : get_col();
+
+  if (gpu_status == true) {
+#if MONOLISH_USE_GPU // gpu
+#pragma omp target teams distribute parallel for
+    for (size_t i = 0; i < Len; i++) {
+      vald[N * i + i] /= alpha;
+    }
+#else
+    throw std::runtime_error("error USE_GPU is false, but gpu_status == true");
+#endif
+  } else {
+#pragma omp parallel for
+    for (size_t i = 0; i < Len; i++) {
+      vald[N * i + i] /= alpha;
+    }
+  }
+
+  logger.func_out();
+}
+template void monolish::matrix::Dense<double>::diag_div(const double alpha);
+template void monolish::matrix::Dense<float>::diag_div(const float alpha);
+
+
+} // namespace matrix
+} // namespace monolish
--- src/blas/matrix/diag_op/dense_diag_vector_mul.cpp
+++ src/blas/matrix/diag_op/dense_diag_vector_mul.cpp
@@ -4,8 +4,8 @@
 namespace monolish {
 namespace matrix {
 
-// add vector
-template <typename T> void Dense<T>::diag_add(const vector<T> &vec) {
+// mul vector
+template <typename T> void Dense<T>::diag_mul(const vector<T> &vec) {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
@@ -21,7 +21,7 @@ template <typename T> void Dense<T>::diag_add(const vector<T> &vec) {
 #if MONOLISH_USE_GPU // gpu
 #pragma omp target teams distribute parallel for
     for (size_t i = 0; i < Len; i++) {
-      vald[N * i + i] += vecd[i];
+      vald[N * i + i] *= vecd[i];
     }
 #else
     throw std::runtime_error("error USE_GPU is false, but gpu_status == true");
@@ -29,18 +29,18 @@ template <typename T> void Dense<T>::diag_add(const vector<T> &vec) {
   } else {
 #pragma omp parallel for
     for (size_t i = 0; i < Len; i++) {
-      vald[N * i + i] += vecd[i];
+      vald[N * i + i] *= vecd[i];
     }
   }
 
   logger.func_out();
 }
 template void
-monolish::matrix::Dense<double>::diag_add(const vector<double> &vec);
+monolish::matrix::Dense<double>::diag_mul(const vector<double> &vec);
 template void
-monolish::matrix::Dense<float>::diag_add(const vector<float> &vec);
+monolish::matrix::Dense<float>::diag_mul(const vector<float> &vec);
 
-template <typename T> void Dense<T>::diag_add(const view1D<vector<T>,T> &vec) {
+template <typename T> void Dense<T>::diag_mul(const view1D<vector<T>,T> &vec) {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
@@ -56,7 +56,7 @@ template <typename T> void Dense<T>::diag_add(const view1D<vector<T>,T> &vec) {
 #if MONOLISH_USE_GPU // gpu
 #pragma omp target teams distribute parallel for
     for (size_t i = 0; i < Len; i++) {
-      vald[N * i + i] += vecd[i];
+      vald[N * i + i] *= vecd[i];
     }
 #else
     throw std::runtime_error("error USE_GPU is false, but gpu_status == true");
@@ -64,18 +64,18 @@ template <typename T> void Dense<T>::diag_add(const view1D<vector<T>,T> &vec) {
   } else {
 #pragma omp parallel for
     for (size_t i = 0; i < Len; i++) {
-      vald[N * i + i] += vecd[i];
+      vald[N * i + i] *= vecd[i];
     }
   }
 
   logger.func_out();
 }
 template void
-monolish::matrix::Dense<double>::diag_add(const view1D<vector<double>, double> &vec);
+monolish::matrix::Dense<double>::diag_mul(const view1D<vector<double>, double> &vec);
 template void
-monolish::matrix::Dense<float>::diag_add(const view1D<vector<float>, float> &vec);
+monolish::matrix::Dense<float>::diag_mul(const view1D<vector<float>, float> &vec);
 
-template <typename T> void Dense<T>::diag_add(const matrix::Dense<vector<T>,T> &vec) {
+template <typename T> void Dense<T>::diag_mul(const matrix::Dense<vector<T>,T> &vec) {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
@@ -91,7 +91,7 @@ template <typename T> void Dense<T>::diag_add(const matrix::Dense<vector<T>,T> &
 #if MONOLISH_USE_GPU // gpu
 #pragma omp target teams distribute parallel for
     for (size_t i = 0; i < Len; i++) {
-      vald[N * i + i] += vecd[i];
+      vald[N * i + i] *= vecd[i];
     }
 #else
     throw std::runtime_error("error USE_GPU is false, but gpu_status == true");
@@ -99,15 +99,15 @@ template <typename T> void Dense<T>::diag_add(const matrix::Dense<vector<T>,T> &
   } else {
 #pragma omp parallel for
     for (size_t i = 0; i < Len; i++) {
-      vald[N * i + i] += vecd[i];
+      vald[N * i + i] *= vecd[i];
     }
   }
 
   logger.func_out();
 }
 template void
-monolish::matrix::Dense<double>::diag_add(const view1D<matrix::Dense<double>, double> &vec);
+monolish::matrix::Dense<double>::diag_mul(const view1D<matrix::Dense<double>, double> &vec);
 template void
-monolish::matrix::Dense<float>::diag_add(const view1D<matrix::Dense<float>, float> &vec);
+monolish::matrix::Dense<float>::diag_mul(const view1D<matrix::Dense<float>, float> &vec);
 } // namespace matrix
 } // namespace monolish
