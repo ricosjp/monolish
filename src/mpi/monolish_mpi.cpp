5bcda61c9ae357233fea55b05a2048b2635c0aa7
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-05-17 18:36:36 +0900

    add MPI error check

--- src/mpi/monolish_mpi.cpp
+++ src/mpi/monolish_mpi.cpp
@@ -5,20 +5,21 @@ namespace mpi {
 
 void Comm::Init() {
 #if defined MONOLISH_USE_MPI
-  MPI_Init(nullptr, nullptr);
+  assert(MPI_Init(nullptr, nullptr) == MPI_SUCCESS);
 #endif
 }
 
 void Comm::Init(int argc, char **argv) {
 #if defined MONOLISH_USE_MPI
-  MPI_Init(&argc, &argv);
+  assert(MPI_Init(&argc, &argv) == MPI_SUCCESS);
 #endif
 }
 
 void Comm::Finalize() {
 #if defined MONOLISH_USE_MPI
-  MPI_Finalize();
+  assert(MPI_Finalize() == MPI_SUCCESS);;
 #endif
 }
+
 } // namespace mpi
 } // namespace monolish
