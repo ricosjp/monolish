cbfd81929952397c4541918c11e5f26fcf8f3fef
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-05-23 11:16:49 +0900

    add mpi header

--- include/monolish_mpi.hpp
+++ include/monolish_mpi.hpp
@@ -1,10 +1,21 @@
 #pragma once
 
+#include<climits>
+#include"common/monolish_common.hpp"
+
 #if defined MONOLISH_USE_MPI
 #include <mpi.h>
 #else
 // MPI dammy
 typedef struct ompi_communicator_t *MPI_Comm;
+#endif
+
+#if SIZE_MAX == ULONG_MAX
+   #define MPI_SIZE_T MPI_UNSIGNED_LONG
+#elif SIZE_MAX == ULLONG_MAX
+   #define MPI_SIZE_T MPI_UNSIGNED_LONG_LONG
+#elif SIZE_MAX == UINT_MAX
+   #define MPI_SIZE_T MPI_UNSIGNED
 #endif
 
 namespace monolish {
@@ -41,6 +52,9 @@ public:
   void Init(int argc, char **argv);
   bool Initialized();
   void Finalize();
+
+#include "mpi/allreduce.hpp"
+
 };
 } // namespace mpi
 } // namespace monolish
