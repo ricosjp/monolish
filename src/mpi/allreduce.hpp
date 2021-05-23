5cd665a2443e4490a5e867a0e0dae803811ad425
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-05-23 11:33:42 +0900

    fix: noMPI allreduce

--- src/mpi/allreduce.hpp
+++ src/mpi/allreduce.hpp
@@ -7,6 +7,7 @@ template <typename T> T Allreduce_core(T val, MPI_Op op, MPI_Comm comm) {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
+#if defined MONOLISH_USE_MPI
   if (typeid(double) == typeid(val)) {
     MPI_Allreduce(&val, &val, 1, MPI_DOUBLE, op, comm);
   }
@@ -19,6 +20,7 @@ template <typename T> T Allreduce_core(T val, MPI_Op op, MPI_Comm comm) {
   if (typeid(size_t) == typeid(val)) {
     MPI_Allreduce(&val, &val, 1, MPI_SIZE_T, op, comm);
   }
+#endif
 
   logger.func_out();
   return val;
