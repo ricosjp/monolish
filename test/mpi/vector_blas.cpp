052c123e77fbbc5a176480ef624a85f075ef15cb
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-05-23 18:40:53 +0900

    separate file

--- test/mpi/vector_blas.cpp
+++ test/mpi/vector_blas.cpp
@@ -1,46 +1,4 @@
-#include "../test_utils.hpp"
-#include "monolish_mpi.hpp"
-
-template <typename T>
-T ans_dot(monolish::vector<T> &mx, monolish::vector<T> &my) {
-  if (mx.size() != my.size()) {
-    throw std::runtime_error("x.size != y.size");
-  }
-  T ans = 0;
-
-  for (size_t i = 0; i < mx.size(); i++) {
-    ans += mx[i] * my[i];
-  }
-
-  return ans;
-}
-
-template <typename T> bool test_send_dot(const size_t size, double tol) {
-
-  monolish::vector<T> x(size, 0.0, 1.0);
-  monolish::vector<T> y(size, 0.0, 1.0);
-
-  T ans = ans_dot(x, y);
-
-  monolish::util::send(x, y);
-  T result = monolish::blas::dot(x, y);
-
-  return ans_check<T>(__func__, result, ans, tol);
-}
-
-template <typename T> bool test_dot(const size_t size, double tol) {
-
-  monolish::vector<T> x(size, 0.0, 1.0);
-  monolish::vector<T> y(size, 0.0, 1.0);
-
-  T ans = ans_dot(x, y);
-
-  monolish::util::send(x, y);
-  T result = monolish::blas::dot(x, y);
-
-  return ans_check<T>(__func__, result, ans, tol);
-}
-
+#include "blas/dot.hpp"
 int main(int argc, char **argv) {
 
   if (argc != 2) {
@@ -49,7 +7,7 @@ int main(int argc, char **argv) {
   }
 
   monolish::mpi::Comm &comm = monolish::mpi::Comm::get_instance();
-  comm.Init();
+  comm.Init(argc, argv);
 
   // monolish::util::set_log_level(3);
   // monolish::util::set_log_filename("./monolish_test_log.txt");
