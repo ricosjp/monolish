287b88df29f3eeacb9f5d92e1ca66a4d231ce991
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-05-25 00:30:24 +0900

    add BLAS Lv1 MPI test

---
+++ test/mpi/blas/nrm1.hpp
@@ -0,0 +1,35 @@
+#include "../../test_utils.hpp"
+
+template <typename T> T ans_nrm1(monolish::vector<T> &mx) {
+  T ans = 0;
+
+  for (size_t i = 0; i < mx.size(); i++) {
+    ans += std::abs(mx[i]);
+  }
+
+  monolish::mpi::Comm &comm = monolish::mpi::Comm::get_instance();
+  ans = comm.Allreduce(ans);
+
+  return ans;
+}
+
+template <typename T> bool test_send_nrm1(const size_t size, double tol) {
+
+  monolish::vector<T> x(size, -1.0, 1.0);
+
+  auto ans = ans_nrm1(x);
+  monolish::util::send(x);
+  auto result = monolish::blas::nrm1(x);
+
+  return ans_check<T>(__func__, result, ans, tol);
+}
+
+template <typename T> bool test_nrm1(const size_t size, double tol) {
+
+  monolish::vector<T> x(size, 0.0, 1.0);
+
+  auto result = monolish::blas::nrm1(x);
+  auto ans = ans_nrm1(x);
+
+  return ans_check<T>(__func__, result, ans, tol);
+}
---
+++ test/mpi/blas/nrm2.hpp
@@ -0,0 +1,37 @@
+#include "../../test_utils.hpp"
+
+template <typename T> T ans_nrm2(monolish::vector<T> &mx) {
+  T ans = 0;
+
+  for (size_t i = 0; i < mx.size(); i++) {
+    ans += mx[i] * mx[i];
+  }
+
+  monolish::mpi::Comm &comm = monolish::mpi::Comm::get_instance();
+  ans = comm.Allreduce(ans);
+
+  ans = sqrt(ans);
+
+  return ans;
+}
+
+template <typename T> bool test_send_nrm2(const size_t size, double tol) {
+
+  monolish::vector<T> x(size, 0.0, 1.0);
+
+  auto ans = ans_nrm2(x);
+  monolish::util::send(x);
+  auto result = monolish::blas::nrm2(x);
+
+  return ans_check<T>(__func__, result, ans, tol);
+}
+
+template <typename T> bool test_nrm2(const size_t size, double tol) {
+
+  monolish::vector<T> x(size, 0.0, 1.0);
+
+  auto result = monolish::blas::nrm2(x);
+  auto ans = ans_nrm2(x);
+
+  return ans_check<T>(__func__, result, ans, tol);
+}
