1f5cb3d779e68108f120f3ee004b1b8c28eb98f4
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-05-25 17:50:15 +0900

    clang format

--- include/monolish_mpi.hpp
+++ include/monolish_mpi.hpp
@@ -23,7 +23,8 @@ namespace monolish {
  * @brief
  * C++ template MPI class, Functions of this class do nothing when MPI is
  * disabled.
- * Functions in this class are under development. Many BLAS functions don't support MPI.
+ * Functions in this class are under development. Many BLAS functions don't
+ * support MPI.
  */
 namespace mpi {
 /**
