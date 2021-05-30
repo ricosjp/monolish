b28dba35f0735d5b2f055caceaf03be8fed33032
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-05-30 23:30:38 +0900

    add mpi::get_type()

--- src/internal/mpi/mpi_util.hpp
+++ src/internal/mpi/mpi_util.hpp
@@ -1,20 +1,24 @@
+#include<typeinfo>
+#include<cstdlib>
+
 #if defined MONOLISH_USE_MPI
 namespace monolish::internal::mpi {
   template<typename T>
-  auto get_type(T val){
-  if (typeid(double) == typeid(val)) {
-    return MPI_DOUBLE;
-  }
-  if (typeid(float) == typeid(val)) {
-    return MPI_FLOAT;
-  }
-  if (typeid(int) == typeid(val)) {
-    return MPI_INT;
-  }
-  if (typeid(size_t) == typeid(val)) {
-    return MPI_SIZE_T;
-  }
-  assert();
-  }
+    auto get_type(T val){
+      if (typeid(double) == typeid(val)) {
+        return MPI_DOUBLE;
+      }
+      if (typeid(float) == typeid(val)) {
+        return MPI_FLOAT;
+      }
+      if (typeid(int) == typeid(val)) {
+        return MPI_INT;
+      }
+      if (typeid(size_t) == typeid(val)) {
+        return MPI_SIZE_T;
+      }
+      std::cout << "error, support types of monolish::mpi are double, float, int, size_t" << std::endl;
+      std::exit(-1);
+    }
 }
 #endif
