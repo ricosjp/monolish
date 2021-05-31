6a24fc0516e66414f7463d9a66f2ffb080422efd
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-05-31 22:44:26 +0900

    merge allreduce to collective_comm.sh

--- include/mpi/gen_collective_comm.sh
+++ include/mpi/gen_collective_comm.sh
@@ -1,5 +1,51 @@
 #/bin/bash
 
+## allreduce
+for prec in double float int size_t; do
+  echo "
+  /**
+  * @brief MPI_Allreduce (MPI_SUM) for scalar. Combines values from all processes and distributes the result back to all processes.
+  * @param val scalar value
+  */ "
+  echo "[[nodiscard]] $prec Allreduce ($prec val) const;"
+done
+
+for prec in double float int size_t; do
+  echo "
+  /**
+  * @brief MPI_Allreduce (MPI_SUM) for scalar. Combines values from all processes and distributes the result back to all processes.
+  * @param val scalar value
+  */ "
+  echo "[[nodiscard]] $prec Allreduce_sum ($prec val) const;"
+done
+
+for prec in double float int size_t; do
+  echo "
+  /**
+  * @brief MPI_Allreduce (MPI_PROD) for scalar. Combines values from all processes and distributes the result back to all processes.
+  * @param val scalar value
+  */ "
+  echo "[[nodiscard]] $prec Allreduce_prod ($prec val) const;"
+done
+
+for prec in double float int size_t; do
+  echo "
+  /**
+  * @brief MPI_Allreduce (MPI_MAX) for scalar. Combines values from all processes and distributes the result back to all processes.
+  * @param val scalar value
+  */ "
+  echo "[[nodiscard]] $prec Allreduce_max ($prec val) const;"
+done
+
+for prec in double float int size_t; do
+  echo "
+  /**
+  * @brief MPI_Allreduce (MPI_MIN) for scalar. Combines values from all processes and distributes the result back to all processes.
+  * @param val scalar value
+  */ "
+  echo "[[nodiscard]] $prec Allreduce_min ($prec val) const;"
+done
+
 ## Bcast
 for prec in double float int size_t; do
   echo "
