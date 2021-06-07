65d87433a4c8b2d4590cbf8c383db2b984f807b4
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-06-08 02:46:33 +0900

    format

--- src/mpi/Bcast.hpp
+++ src/mpi/Bcast.hpp
@@ -3,50 +3,53 @@
 
 namespace monolish {
 namespace {
-//Scalar
-template <typename T> 
+// Scalar
+template <typename T>
 void Bcast_core(T &val, int root, MPI_Comm comm, bool gpu_sync) {
   Logger &logger = Logger::get_instance();
   logger.util_in(monolish_func);
 
 #if defined MONOLISH_USE_MPI
-   MPI_Bcast(&val, 1, internal::mpi::get_type(val), root, comm);
+  MPI_Bcast(&val, 1, internal::mpi::get_type(val), root, comm);
 #endif
 
   logger.util_out();
 }
 
-//std::vectror
-template <typename T> 
+// std::vectror
+template <typename T>
 void Bcast_core(std::vector<T> &vec, int root, MPI_Comm comm, bool gpu_sync) {
   Logger &logger = Logger::get_instance();
   logger.util_in(monolish_func);
 
 #if defined MONOLISH_USE_MPI
-   MPI_Bcast(vec.data(), vec.size(), internal::mpi::get_type(vec[0]), root, comm);
+  MPI_Bcast(vec.data(), vec.size(), internal::mpi::get_type(vec[0]), root,
+            comm);
 #endif
 
   logger.util_out();
 }
 
-//monolish::vectror
-template <typename T> 
-void Bcast_core(monolish::vector<T> &vec, int root, MPI_Comm comm, bool gpu_sync) {
+// monolish::vectror
+template <typename T>
+void Bcast_core(monolish::vector<T> &vec, int root, MPI_Comm comm,
+                bool gpu_sync) {
   Logger &logger = Logger::get_instance();
   logger.util_in(monolish_func);
 
 #if defined MONOLISH_USE_GPU
-  if(gpu_sync==true){
+  if (gpu_sync == true) {
     vec.recv();
   }
 #endif
 
 #if defined MONOLISH_USE_MPI
-   MPI_Bcast(vec.data(), vec.size(), internal::mpi::get_type(vec[0]), root, comm);
+  MPI_Bcast(vec.data(), vec.size(), internal::mpi::get_type(vec[0]), root,
+            comm);
 #endif
 
 #if defined MONOLISH_USE_GPU
-  if(gpu_sync==true){
+  if (gpu_sync == true) {
     vec.send();
   }
 #endif
--- src/mpi/allreduce.hpp
+++ src/mpi/allreduce.hpp
@@ -8,7 +8,7 @@ template <typename T> T Allreduce_core(T val, MPI_Op op, MPI_Comm comm) {
   logger.util_in(monolish_func);
 
 #if defined MONOLISH_USE_MPI
-  MPI_Allreduce(&val, &val, 1,internal::mpi::get_type(val), op, comm);
+  MPI_Allreduce(&val, &val, 1, internal::mpi::get_type(val), op, comm);
 #endif
 
   logger.util_out();
