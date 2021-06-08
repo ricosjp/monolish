fa28fa86627e3721a044b322eed70c4a0f7af82b
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-06-08 16:36:40 +0900

    fix typo

--- include/mpi/gen_collective_comm.sh
+++ include/mpi/gen_collective_comm.sh
@@ -64,7 +64,7 @@ for prec in double float; do
   * @brief MPI_Bcast, Broadcasts a message from the process with rank "root" to all other processes
   * @param vec monolish vector (size N)
   * @param root root rank number
-  * @param gpu_sync sync gpu data.
+  * @param gpu_sync sync gpu data.This option does not work because std::vector is not support GPU.
   * @warning
   * When "only_cpu flag is enabled" and "send data is on the GPU", data is received from the GPU, 
   * MPI communication is performed, and finally data is sent to the GPU.
@@ -91,7 +91,7 @@ for prec in double float; do
   * @brief MPI_Gather, Gathers vector from all processes
   * The data is evenly divided and transmitted to each process.
   * @param sendvec send data, monolish vector (size N)
-  * @param recvvec recv data, std::vector (size N * # of procs)
+  * @param recvvec recv data, monolish vector (size N * # of procs)
   * @param val root rank number
   * @param gpu_sync sync gpu data. It receives sendvec, then performs MPI communication, and finally sends recvvec.
   * @warning
@@ -107,8 +107,8 @@ for prec in double float int size_t; do
   /**
   * @brief MPI_Gather, Gathers vector from all processes
   * The data is evenly divided and transmitted to each process.
-  * @param sendvec send data, monolish vector (size N)
-  * @param recvvec recv data, monolish vector (size N * # of procs)
+  * @param sendvec send data, std vector (size N)
+  * @param recvvec recv data, std vector (size N * # of procs)
   * @param val root rank number
   * @param gpu_sync sync gpu data. This option does not work because std::vector is not support GPU
   */
