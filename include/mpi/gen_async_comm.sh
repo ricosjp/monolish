09c3103f8a455af87d62781df285275daa8d4efc
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-06-15 01:16:54 +0900

    del gpu_sync

--- include/mpi/gen_async_comm.sh
+++ include/mpi/gen_async_comm.sh
@@ -8,13 +8,14 @@ for prec in double float int size_t; do
   * @param val scalar value
   * @param dst rank of dstination
   * @param tag message tag
+  * @note
+  * There is not MPI_Wait() in monolish::mpi, all communication is synchronized by using Waitall() function.
   * @Warning
-  * This function send data asynchronously. There is not MPI wait() functions in monolish.
-  * Asynchronous send data is synchronized in the recv() function.
-  * This function does not support GPU.
-  * The user needs to communicate with the GPU before and after the call to this function if necessary.
+  * MPI functions do not support GPUs.
+  * The user needs to send and receive data to and from the GPU before and after the MPI function.
+  * This function is not thread-safe.
   */"
-  echo "MPI_Request send($prec val, int dst, int tag, bool only_cpu = false) const;"
+  echo "MPI_Request send($prec val, int dst, int tag) const;"
 done
 
 for prec in double float int size_t; do
@@ -24,8 +25,14 @@ for prec in double float int size_t; do
   * @param vec std::vector (size N)
   * @param dst rank of dstination
   * @param tag message tag
+  * @note
+  * There is not MPI_Wait() in monolish::mpi, all communication is synchronized by using Waitall() function.
+  * @Warning
+  * MPI functions do not support GPUs.
+  * The user needs to send and receive data to and from the GPU before and after the MPI function.
+  * This function is not thread-safe.
   */"
-  echo "MPI_Request send(std::vector<$prec> &vec, int dst, int tag, bool only_cpu = false) const;"
+  echo "MPI_Request send(std::vector<$prec> &vec, int dst, int tag) const;"
 done
 
 for prec in double float; do
@@ -35,12 +42,14 @@ for prec in double float; do
   * @param vec std::vector (size N)
   * @param dst rank of dstination
   * @param tag message tag
-  * @warning
-  * When "only_cpu flag is enabled" and "send data is on the GPU", data is received from the GPU, 
-  * MPI communication is performed, and finally data is sent to the GPU.
-  * If there is no GPU, or if there is no data on the GPU, no error will occur even if this flag is set to true.
+  * @note
+  * There is not MPI_Wait() in monolish::mpi, all communication is synchronized by using Waitall() function.
+  * @Warning
+  * MPI functions do not support GPUs.
+  * The user needs to send and receive data to and from the GPU before and after the MPI function.
+  * This function is not thread-safe.
   */ "
-  echo "MPI_Request send(monolish::vector<$prec> &vec, int dst, int tag, bool only_cpu = false) const;"
+  echo "MPI_Request send(monolish::vector<$prec> &vec, int dst, int tag) const;"
 done
 
 
@@ -52,8 +61,14 @@ for prec in double float int size_t; do
   * @param val scalar value
   * @param src rank of source
   * @param tag message tag
+  * @note
+  * There is not MPI_Wait() in monolish::mpi, all communication is synchronized by using Waitall() function.
+  * @Warning
+  * MPI functions do not support GPUs.
+  * The user needs to send and receive data to and from the GPU before and after the MPI function.
+  * This function is not thread-safe.
   */"
-  echo "void Irecv($prec val, int src, int tag, bool only_cpu = false) const;"
+  echo "void Irecv($prec val, int src, int tag) const;"
 done
 
 for prec in double float int size_t; do
@@ -63,8 +78,14 @@ for prec in double float int size_t; do
   * @param vec std::vector (size N)
   * @param src rank of source
   * @param tag message tag
+  * @note
+  * There is not MPI_Wait() in monolish::mpi, all communication is synchronized by using Waitall() function.
+  * @Warning
+  * MPI functions do not support GPUs.
+  * The user needs to send and receive data to and from the GPU before and after the MPI function.
+  * This function is not thread-safe.
   */"
-  echo "void Irecv(std::vector<$prec> &vec, int src, int tag, bool only_cpu = false) const;"
+  echo "void Irecv(std::vector<$prec> &vec, int src, int tag) const;"
 done
 
 for prec in double float; do
@@ -74,12 +95,14 @@ for prec in double float; do
   * @param vec monolish::vector (size N)
   * @param src rank of source
   * @param tag message tag
-  * @warning
-  * When "only_cpu flag is enabled" and "recv data is on the GPU", data is received from the GPU, 
-  * MPI communication is performed, and finally data is sent to the GPU.
-  * If there is no GPU, or if there is no data on the GPU, no error will occur even if this flag is set to true.
+  * @note
+  * There is not MPI_Wait() in monolish::mpi, all communication is synchronized by using Waitall() function.
+  * @Warning
+  * MPI functions do not support GPUs.
+  * The user needs to send and receive data to and from the GPU before and after the MPI function.
+  * This function is not thread-safe.
   */ "
-  echo "void Irecv(monolish::vector<$prec> &vec, int src, int tag, bool only_cpu = false) const;"
+  echo "void Irecv(monolish::vector<$prec> &vec, int src, int tag) const;"
 done
 
 echo "
