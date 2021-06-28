570b7f7ba1b271b4f5fd51deb3059aaa48b8ed97
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-06-28 16:31:12 +0900

    add cpu sample

---
+++ examples/blas/innerproduct_only_cpu/innerproduct.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <monolish_blas.hpp>
+int main() {
+
+  // Output log if you need
+  // monolish::util::set_log_level(3);
+  // monolish::util::set_log_filename("./monolish_test_log.txt");
+
+  size_t N = 100;
+
+  // x = {1,1,...,1}, length N
+  monolish::vector<double> x(N, 1.0);
+
+  // Random vector length N with random values in the range 1.0 to 2.0
+  monolish::vector<double> y(N, 1.0, 2.0);
+
+  // compute innerproduct
+  double ans = monolish::blas::dot(x, y);
+
+  std::cout << ans << std::endl;
+
+  return 0;
+}
