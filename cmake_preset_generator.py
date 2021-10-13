9d0412f0ecb29bd27ee7e7cfdfba863b45450f79
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-10-14 08:42:37 +0900

    package -> arch

--- cmake_preset_generator.py
+++ cmake_preset_generator.py
@@ -4,46 +4,40 @@
 import json
 
 
-def display_name(p, avx, mpi, package):
+def display_name(p, avx, mpi, arch):
     s = "monolish with " + p.upper()
+    if arch:
+        s += " for all CUDA arch"
     if avx == "avx":
         s += ", AVX"
     if mpi == "mpi":
         s += ", MPI"
-    if package:
-        s += " for packaging"
     return s
 
 
-def cache_variables(p, avx, mpi, package):
+def cache_variables(p, avx, mpi, arch):
     return {
         "MONOLISH_USE_AVX": "ON" if avx == "avx" else "OFF",
         "MONOLISH_USE_MPI": "ON" if mpi == "mpi" else "OFF",
         "MONOLISH_USE_NVIDIA_GPU": "ON" if p == "gpu" else "OFF",
-        "MONOLISH_NVIDIA_GPU_ARCH_ALL": "ON" if package else "OFF",
+        "MONOLISH_NVIDIA_GPU_ARCH_ALL": "ON" if arch else "OFF",
     }
-
-
-def base_dir(package):
-    if package:
-        return "${sourceDir}/package/"
-    else:
-        return "${sourceDir}/build/"
 
 
 def main():
     targets = [
         {
-            "name": f"{p}-{avx}-{mpi}{package}",
+            "name": f"{p}-{avx}-{mpi}{arch}",
             "generator": "Ninja",
-            "displayName": display_name(p, avx, mpi, package),
-            "binaryDir": base_dir(package) + f"{p}-{avx}-{mpi}",
-            "cacheVariables": cache_variables(p, avx, mpi, package),
+            "displayName": display_name(p, avx, mpi, arch),
+            "binaryDir": "${sourceDir}/build/" + f"{p}-{avx}-{mpi}{arch}",
+            "cacheVariables": cache_variables(p, avx, mpi, arch),
         }
         for p in ["cpu", "gpu"]
         for avx in ["none", "avx"]
         for mpi in ["none", "mpi"]
-        for package in ["", "-package"]
+        for arch in ["", "-all"]
+        if not (p == "cpu" and arch == "-all")
     ]
     preset = {
         "version": 2,
