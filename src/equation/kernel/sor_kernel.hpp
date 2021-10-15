be8ed91e3f7353b91b1ef073e8933b12f576b7b7
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-10-15 11:04:23 +0900

    add logger

--- src/equation/kernel/sor_kernel.hpp
+++ src/equation/kernel/sor_kernel.hpp
@@ -6,7 +6,9 @@ template <typename T>
 void sor_kernel_lower(const monolish::matrix::CRS<T> &A, const vector<T> &D,
                       vector<T> &x, const vector<T> &b) {
 
-  for (int i = 0; i < A.get_row(); i++) {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+  for (int i = 0; i < (int)A.get_row(); i++) {
     auto tmp = b.data()[i];
     for (int j = A.row_ptr[i]; j < A.row_ptr[i + 1]; j++) {
       if (i > A.col_ind[j]) { // lower
@@ -17,19 +19,23 @@ void sor_kernel_lower(const monolish::matrix::CRS<T> &A, const vector<T> &D,
     }
     x.data()[i] = tmp * D.data()[i];
   }
+  logger.func_out();
 }
 
 template <typename T>
 void sor_kernel_lower(const monolish::matrix::Dense<T> &A, const vector<T> &D,
                       vector<T> &x, const vector<T> &b) {
 
-  for (int i = 0; i < A.get_row(); i++) {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+  for (int i = 0; i < (int)A.get_row(); i++) {
     auto tmp = b.data()[i];
     for (int j = 0; j < i; j++) {
       tmp -= A.val[i * A.get_col() + j] * x.data()[j];
     }
     x.data()[i] = tmp * D.data()[i];
   }
+  logger.func_out();
 }
 } // namespace
 } // namespace monolish
