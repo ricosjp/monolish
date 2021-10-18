a26bc0e0ccdec5e45e16cc31722f25fcf1467003
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-10-18 14:33:29 +0900

    Dense sor precon

--- src/equation/kernel/sor_kernel.hpp
+++ src/equation/kernel/sor_kernel.hpp
@@ -71,6 +71,7 @@ void sor_kernel_precond(const monolish::matrix::CRS<T> &A, const vector<T> &D,
 
     logger.func_out();
 }
+
 template <typename T>
 void sor_kernel_precond(const monolish::matrix::Dense<T> &A, const vector<T> &D,
         vector<T> &x, const vector<T> &b) {
@@ -78,6 +79,29 @@ void sor_kernel_precond(const monolish::matrix::Dense<T> &A, const vector<T> &D,
     Logger &logger = Logger::get_instance();
     logger.func_in(monolish_func);
 
+    vector<T> b_tmp(b);
+    b_tmp.nonfree_recv();
+
+    for (int i = 0; i < (int)A.get_row(); i++) {
+        auto tmp = b_tmp.data()[i];
+        for (int j = A.row_ptr[i]; j < A.row_ptr[i + 1]; j++) {
+            if (i > A.col_ind[j]) { // lower
+                tmp -= A.val[i * A.get_col() + j] * x.data()[j];
+            }
+        }
+            x.data()[i]   = tmp * D.data()[i];
+    }
+    for (int i = (int)A.get_row() -1; i >= 0; i--) {
+        auto tmp = 0.0;
+        for (int j = A.row_ptr[i]; j < A.row_ptr[i + 1]; j++) {
+            if (i < A.col_ind[j] ){ //upper
+                tmp += A.val[i * A.get_col() + j] * x.data()[j];
+                
+            }
+        }
+            x.data()[i]  -= tmp * D.data()[i];
+    }
+    x.send();
 
     logger.func_out();
 }
