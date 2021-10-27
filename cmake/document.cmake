94783f9dc758d3440cb6d40368d053321cec5835
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-10-27 20:08:18 +0900

    Eval all templates (*.in files) in doc/

--- cmake/document.cmake
+++ cmake/document.cmake
@@ -7,15 +7,23 @@ set(monolish_deb_oss        "monolish_${PROJECT_VERSION}+oss_amd64.deb")
 set(monolish_deb_mkl        "monolish_${PROJECT_VERSION}+mkl_amd64.deb")
 set(monolish_deb_oss_nvidia "monolish_${PROJECT_VERSION}+oss-nvidia_amd64.deb")
 set(monolish_deb_mkl_nvidia "monolish_${PROJECT_VERSION}+mkl-nvidia_amd64.deb")
-configure_file(
-  ${PROJECT_SOURCE_DIR}/doc/installation/installation.md.in
-  ${PROJECT_SOURCE_DIR}/doc/installation/installation.md
+
+#
+# Eval templates in doc/
+#
+file(GLOB_RECURSE
+  monolish_doc_templates
+  RELATIVE ${PROJECT_SOURCE_DIR}
+  ${PROJECT_SOURCE_DIR}/doc/**/*.in
 )
-configure_file(
-  ${PROJECT_SOURCE_DIR}/doc/installation/monolish_docker.md.in
-  ${PROJECT_SOURCE_DIR}/doc/installation/monolish_docker.md
-)
+foreach(template IN LISTS monolish_doc_templates)
+  string(REPLACE ".in" "" dest ${template})
+  message(STATUS "Generate ${dest}")
+  configure_file(${PROJECT_SOURCE_DIR}/${template} ${PROJECT_SOURCE_DIR}/${dest})
+endforeach()
+
 configure_file(Doxyfile.in Doxyfile)
+
 add_custom_target(document
   COMMAND doxygen
   WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
