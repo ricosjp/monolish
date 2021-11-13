6e22e4d434117e3c9916ba6e6f8820db6d0d9257
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-11-13 12:45:06 +0900

    delete Lt

--- include/monolish_equation.hpp
+++ include/monolish_equation.hpp
@@ -363,7 +363,7 @@ template <typename MATRIX, typename Float>
 class IC : public monolish::solver::solver<MATRIX, Float> {
 private:
   int cusparse_IC(MATRIX &A, vector<Float> &x, vector<Float> &b);
-  void *matM = 0, *matL = 0, *matLt = 0;
+  void *matM = 0, *matL = 0;
   void *infoM = 0, *infoL = 0, *infoLt = 0;
   void *cusparse_handle;
   int bufsize;
