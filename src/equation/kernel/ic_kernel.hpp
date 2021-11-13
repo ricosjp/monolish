0ecfd944318bc6ba37a78c8727f694ce386449bb
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2021-11-13 12:47:39 +0900

    delete Lt and clean file

--- src/equation/kernel/ic_kernel.hpp
+++ src/equation/kernel/ic_kernel.hpp
@@ -6,9 +6,9 @@ namespace {
 
 #if MONOLISH_USE_NVIDIA_GPU
 void cusolver_ic_create_descr(
-    matrix::CRS<double> &A, cusparseMatDescr_t &descr_M, csric02Info_t &info_M,
+    matrix::CRS<float> &A, cusparseMatDescr_t &descr_M, csric02Info_t &info_M,
     cusparseMatDescr_t &descr_L, csrsv2Info_t &info_L,
-    cusparseMatDescr_t &descr_Lt, csrsv2Info_t &info_Lt,
+    csrsv2Info_t &info_Lt,
     const cusparseHandle_t &handle) {
 
   Logger &logger = Logger::get_instance();
@@ -41,9 +41,9 @@ void cusolver_ic_create_descr(
 }
 
 void cusolver_ic_create_descr(
-    matrix::CRS<float> &A, cusparseMatDescr_t &descr_M, csric02Info_t &info_M,
+    matrix::CRS<double> &A, cusparseMatDescr_t &descr_M, csric02Info_t &info_M,
     cusparseMatDescr_t &descr_L, csrsv2Info_t &info_L,
-    cusparseMatDescr_t &descr_Lt, csrsv2Info_t &info_Lt,
+    csrsv2Info_t &info_Lt,
     const cusparseHandle_t &handle) {
 
   Logger &logger = Logger::get_instance();
@@ -79,47 +79,10 @@ void cusolver_ic_create_descr(
 /////////////////////////////////////////////////////////////////////////
 
 int cusolver_ic_get_buffersize(
-    matrix::CRS<double> &A, const cusparseMatDescr_t &descr_M,
-    const csric02Info_t &info_M, const cusparseMatDescr_t &descr_L,
-    const csrsv2Info_t &info_L, const cusparseOperation_t &trans_L,
-    const cusparseMatDescr_t &descr_Lt, const csrsv2Info_t &info_Lt,
-    const cusparseOperation_t &trans_Lt, const cusparseHandle_t &handle) {
-
-  Logger &logger = Logger::get_instance();
-  logger.func_in(monolish_func);
-
-  auto M = A.get_row();
-  auto nnz = A.get_nnz();
-  int *d_csrRowPtr = A.row_ptr.data();
-  int *d_csrColInd = A.col_ind.data();
-  auto *d_csrVal = A.val.data();
-
-  int bufsize;
-  int bufsize_M;
-  int bufsize_L;
-  int bufsize_Lt;
-
-#pragma omp target data use_device_ptr(d_csrVal, d_csrRowPtr, d_csrColInd)
-  {
-    cusparseDcsric02_bufferSize(handle, M, nnz, descr_M, d_csrVal, d_csrRowPtr,
-                                 d_csrColInd, info_M, &bufsize_M);
-    cusparseDcsrsv2_bufferSize(handle, trans_L, M, nnz, descr_L, d_csrVal,
-                               d_csrRowPtr, d_csrColInd, info_L, &bufsize_L);
-    cusparseDcsrsv2_bufferSize(handle, trans_Lt, M, nnz, descr_L, d_csrVal,
-                               d_csrRowPtr, d_csrColInd, info_Lt, &bufsize_Lt);
-
-    bufsize = std::max(bufsize_M, std::max(bufsize_L, bufsize_Lt));
-  }
-
-  logger.func_out();
-  return bufsize;
-}
-
-int cusolver_ic_get_buffersize(
     matrix::CRS<float> &A, const cusparseMatDescr_t &descr_M,
     const csric02Info_t &info_M, const cusparseMatDescr_t &descr_L,
     const csrsv2Info_t &info_L, const cusparseOperation_t &trans_L,
-    const cusparseMatDescr_t &descr_Lt, const csrsv2Info_t &info_Lt,
+    const csrsv2Info_t &info_Lt,
     const cusparseOperation_t &trans_Lt, const cusparseHandle_t &handle) {
 
   Logger &logger = Logger::get_instance();
@@ -152,17 +115,12 @@ int cusolver_ic_get_buffersize(
   return bufsize;
 }
 
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-
-bool cusolver_ilu(
-    matrix::CRS<double> &A, double *d_csrVal, const cusparseMatDescr_t &descr_M,
-    const csric02Info_t &info_M, const cusparseSolvePolicy_t &policy_M,
-    const cusparseMatDescr_t &descr_L, const csrsv2Info_t &info_L,
-    const cusparseSolvePolicy_t &policy_L, const cusparseOperation_t &trans_L,
-    const cusparseMatDescr_t &descr_Lt, const csrsv2Info_t &info_Lt,
-    const cusparseSolvePolicy_t &policy_Lt, const cusparseOperation_t &trans_Lt,
-    vector<double> &buf, const cusparseHandle_t &handle) {
+int cusolver_ic_get_buffersize(
+    matrix::CRS<double> &A, const cusparseMatDescr_t &descr_M,
+    const csric02Info_t &info_M, const cusparseMatDescr_t &descr_L,
+    const csrsv2Info_t &info_L, const cusparseOperation_t &trans_L,
+    const csrsv2Info_t &info_Lt,
+    const cusparseOperation_t &trans_Lt, const cusparseHandle_t &handle) {
 
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
@@ -171,55 +129,38 @@ bool cusolver_ilu(
   auto nnz = A.get_nnz();
   int *d_csrRowPtr = A.row_ptr.data();
   int *d_csrColInd = A.col_ind.data();
+  auto *d_csrVal = A.val.data();
 
-  double *pBuffer = buf.data();
+  int bufsize;
+  int bufsize_M;
+  int bufsize_L;
+  int bufsize_Lt;
 
-  int structural_zero;
-  int numerical_zero;
-
-#pragma omp target data use_device_ptr(d_csrVal, d_csrRowPtr, d_csrColInd,     \
-                                       pBuffer)
+#pragma omp target data use_device_ptr(d_csrVal, d_csrRowPtr, d_csrColInd)
   {
-    // step 4: perform analysis of incomplete Cholesky on M
-    //         perform analysis of triangular solve on L
-    //         perform analysis of triangular solve on L'
-    // The lower triangular part of M has the same sparsity pattern as L, so
-    // we can do analysis of csric02 and csrsv2 simultaneously.
-    cusparseDcsric02_analysis(handle, M, nnz, descr_M, d_csrVal, d_csrRowPtr,
-                               d_csrColInd, info_M, policy_M, pBuffer);
-    auto status = cusparseXcsric02_zeroPivot(handle, info_M, &structural_zero);
+    cusparseDcsric02_bufferSize(handle, M, nnz, descr_M, d_csrVal, d_csrRowPtr,
+                                 d_csrColInd, info_M, &bufsize_M);
+    cusparseDcsrsv2_bufferSize(handle, trans_L, M, nnz, descr_L, d_csrVal,
+                               d_csrRowPtr, d_csrColInd, info_L, &bufsize_L);
+    cusparseDcsrsv2_bufferSize(handle, trans_Lt, M, nnz, descr_L, d_csrVal,
+                               d_csrRowPtr, d_csrColInd, info_Lt, &bufsize_Lt);
 
-    if (CUSPARSE_STATUS_ZERO_PIVOT == status) {
-      printf("A(%d,%d) is missing\n", structural_zero, structural_zero);
-    }
-
-    cusparseDcsrsv2_analysis(handle, trans_L, M, nnz, descr_L, d_csrVal,
-                             d_csrRowPtr, d_csrColInd, info_L, policy_L,
-                             pBuffer);
-
-    cusparseDcsrsv2_analysis(handle, trans_Lt, M, nnz, descr_L, d_csrVal,
-                             d_csrRowPtr, d_csrColInd, info_Lt, policy_Lt,
-                             pBuffer);
-
-    // step 5: M = L * U
-    cusparseDcsric02(handle, M, nnz, descr_M, d_csrVal, d_csrRowPtr,
-                      d_csrColInd, info_M, policy_M, pBuffer);
-    status = cusparseXcsric02_zeroPivot(handle, info_M, &numerical_zero);
-    if (CUSPARSE_STATUS_ZERO_PIVOT == status) {
-      printf("U(%d,%d) is zero\n", numerical_zero, numerical_zero);
-    }
+    bufsize = std::max(bufsize_M, std::max(bufsize_L, bufsize_Lt));
   }
 
   logger.func_out();
-  return true;
+  return bufsize;
 }
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 
 bool cusolver_ilu(
     matrix::CRS<float> &A, float *d_csrVal, const cusparseMatDescr_t &descr_M,
     const csric02Info_t &info_M, const cusparseSolvePolicy_t &policy_M,
     const cusparseMatDescr_t &descr_L, const csrsv2Info_t &info_L,
     const cusparseSolvePolicy_t &policy_L, const cusparseOperation_t &trans_L,
-    const cusparseMatDescr_t &descr_Lt, const csrsv2Info_t &info_Lt,
+    const csrsv2Info_t &info_Lt,
     const cusparseSolvePolicy_t &policy_Lt, const cusparseOperation_t &trans_Lt,
     vector<double> buf, const cusparseHandle_t &handle) {
 
@@ -260,7 +201,7 @@ bool cusolver_ilu(
                              d_csrRowPtr, d_csrColInd, info_Lt, policy_Lt,
                              pBuffer);
 
-    // step 5: M = L * U
+    // step 5: M = L * Lt 
     cusparseScsric02(handle, M, nnz, descr_M, d_csrVal, d_csrRowPtr,
                       d_csrColInd, info_M, policy_M, pBuffer);
     status = cusparseXcsric02_zeroPivot(handle, info_M, &numerical_zero);
@@ -274,18 +215,14 @@ bool cusolver_ilu(
   return true;
 }
 
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-
-bool cusolver_ilu_solve(
+bool cusolver_ilu(
     matrix::CRS<double> &A, double *d_csrVal, const cusparseMatDescr_t &descr_M,
     const csric02Info_t &info_M, const cusparseSolvePolicy_t &policy_M,
     const cusparseMatDescr_t &descr_L, const csrsv2Info_t &info_L,
     const cusparseSolvePolicy_t &policy_L, const cusparseOperation_t &trans_L,
-    const cusparseMatDescr_t &descr_Lt, const csrsv2Info_t &info_Lt,
+    const csrsv2Info_t &info_Lt,
     const cusparseSolvePolicy_t &policy_Lt, const cusparseOperation_t &trans_Lt,
-    double *d_x, double *d_b, double *d_tmp, vector<double> &buf,
-    const cusparseHandle_t &handle) {
+    vector<double> &buf, const cusparseHandle_t &handle) {
 
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
@@ -296,32 +233,56 @@ bool cusolver_ilu_solve(
   int *d_csrColInd = A.col_ind.data();
 
   double *pBuffer = buf.data();
-  const double alpha = 1.0;
+
+  int structural_zero;
+  int numerical_zero;
 
 #pragma omp target data use_device_ptr(d_csrVal, d_csrRowPtr, d_csrColInd,     \
-                                       d_x, d_b, d_tmp, pBuffer)
+                                       pBuffer)
   {
-    // step 6: solve L*tmp = b
-    cusparseDcsrsv2_solve(handle, trans_L, M, nnz, &alpha, descr_L, d_csrVal,
-                          d_csrRowPtr, d_csrColInd, info_L, d_b, d_tmp,
-                          policy_L, pBuffer);
+    // step 4: perform analysis of incomplete Cholesky on M
+    //         perform analysis of triangular solve on L
+    //         perform analysis of triangular solve on L'
+    // The lower triangular part of M has the same sparsity pattern as L, so
+    // we can do analysis of csric02 and csrsv2 simultaneously.
+    cusparseDcsric02_analysis(handle, M, nnz, descr_M, d_csrVal, d_csrRowPtr,
+                               d_csrColInd, info_M, policy_M, pBuffer);
+    auto status = cusparseXcsric02_zeroPivot(handle, info_M, &structural_zero);
 
-    // step 7: solve U*x = tmp
-    cusparseDcsrsv2_solve(handle, trans_Lt, M, nnz, &alpha, descr_L, d_csrVal,
-                          d_csrRowPtr, d_csrColInd, info_Lt, d_tmp, d_x,
-                          policy_Lt, pBuffer);
+    if (CUSPARSE_STATUS_ZERO_PIVOT == status) {
+      printf("A(%d,%d) is missing\n", structural_zero, structural_zero);
+    }
+
+    cusparseDcsrsv2_analysis(handle, trans_L, M, nnz, descr_L, d_csrVal,
+                             d_csrRowPtr, d_csrColInd, info_L, policy_L,
+                             pBuffer);
+
+    cusparseDcsrsv2_analysis(handle, trans_Lt, M, nnz, descr_L, d_csrVal,
+                             d_csrRowPtr, d_csrColInd, info_Lt, policy_Lt,
+                             pBuffer);
+
+    // step 5: M = L * Lt 
+    cusparseDcsric02(handle, M, nnz, descr_M, d_csrVal, d_csrRowPtr,
+                      d_csrColInd, info_M, policy_M, pBuffer);
+    status = cusparseXcsric02_zeroPivot(handle, info_M, &numerical_zero);
+    if (CUSPARSE_STATUS_ZERO_PIVOT == status) {
+      printf("U(%d,%d) is zero\n", numerical_zero, numerical_zero);
+    }
   }
 
   logger.func_out();
   return true;
 }
 
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
 bool cusolver_ilu_solve(
     matrix::CRS<float> &A, float *d_csrVal, const cusparseMatDescr_t &descr_M,
     const csric02Info_t &info_M, const cusparseSolvePolicy_t &policy_M,
     const cusparseMatDescr_t &descr_L, const csrsv2Info_t &info_L,
     const cusparseSolvePolicy_t &policy_L, const cusparseOperation_t &trans_L,
-    const cusparseMatDescr_t &descr_Lt, const csrsv2Info_t &info_Lt,
+    const csrsv2Info_t &info_Lt,
     const cusparseSolvePolicy_t &policy_Lt, const cusparseOperation_t &trans_Lt,
     float *d_x, float *d_b, float *d_tmp, vector<double> &buf,
     const cusparseHandle_t &handle) {
@@ -345,8 +306,47 @@ bool cusolver_ilu_solve(
                           d_csrRowPtr, d_csrColInd, info_L, d_b, d_tmp,
                           policy_L, pBuffer);
 
-    // step 7: solve U*x = tmp
+    // step 7: solve Lt*x = tmp
     cusparseScsrsv2_solve(handle, trans_Lt, M, nnz, &alpha, descr_L, d_csrVal,
+                          d_csrRowPtr, d_csrColInd, info_Lt, d_tmp, d_x,
+                          policy_Lt, pBuffer);
+  }
+
+  logger.func_out();
+  return true;
+}
+
+bool cusolver_ilu_solve(
+    matrix::CRS<double> &A, double *d_csrVal, const cusparseMatDescr_t &descr_M,
+    const csric02Info_t &info_M, const cusparseSolvePolicy_t &policy_M,
+    const cusparseMatDescr_t &descr_L, const csrsv2Info_t &info_L,
+    const cusparseSolvePolicy_t &policy_L, const cusparseOperation_t &trans_L,
+    const csrsv2Info_t &info_Lt,
+    const cusparseSolvePolicy_t &policy_Lt, const cusparseOperation_t &trans_Lt,
+    double *d_x, double *d_b, double *d_tmp, vector<double> &buf,
+    const cusparseHandle_t &handle) {
+
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  auto M = A.get_row();
+  auto nnz = A.get_nnz();
+  int *d_csrRowPtr = A.row_ptr.data();
+  int *d_csrColInd = A.col_ind.data();
+
+  double *pBuffer = buf.data();
+  const double alpha = 1.0;
+
+#pragma omp target data use_device_ptr(d_csrVal, d_csrRowPtr, d_csrColInd,     \
+                                       d_x, d_b, d_tmp, pBuffer)
+  {
+    // step 6: solve L*tmp = b
+    cusparseDcsrsv2_solve(handle, trans_L, M, nnz, &alpha, descr_L, d_csrVal,
+                          d_csrRowPtr, d_csrColInd, info_L, d_b, d_tmp,
+                          policy_L, pBuffer);
+
+    // step 7: solve Lt*x = tmp
+    cusparseDcsrsv2_solve(handle, trans_Lt, M, nnz, &alpha, descr_L, d_csrVal,
                           d_csrRowPtr, d_csrColInd, info_Lt, d_tmp, d_x,
                           policy_Lt, pBuffer);
   }
