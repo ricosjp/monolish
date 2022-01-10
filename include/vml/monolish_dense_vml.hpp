ce25fc5765a8190b8e9a26d02d407f06ca3ec11c
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-01-10 15:48:39 +0900

    dense vml document refactoring

--- include/vml/monolish_dense_vml.hpp
+++ include/vml/monolish_dense_vml.hpp
@@ -11,8 +11,18 @@ namespace monolish::vml {
 
 
 /**
- * @brief element by element addition Dense matrix A and
- * Dense matrix B.
+ * @addtogroup Dense_VML
+ * @{
+ */
+
+
+/**
+ * \defgroup vml_dnsadd monolish::vml::add
+ * @brief element by element addition Dense matrix A and Dense matrix B.
+ * @{
+ */
+/**
+ * @brief element by element addition Dense matrix A and Dense matrix B.
  * @param A monolish Dense Matrix (size M x N)
  * @param B monolish Dense Matrix (size M x N)
  * @param C monolish Dense Matrix (size M x N)
@@ -24,10 +34,15 @@ namespace monolish::vml {
 */ 
 void add(const matrix::Dense<double> &A, const matrix::Dense<double> &B, matrix::Dense<double> &C);
 void add(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::Dense<float> &C);
+/**@}*/
 
 /**
- * @brief element by element subtract Dense matrix A and
- * Dense matrix B.
+ * \defgroup vml_dnssub monolish::vml::sub
+ * @brief element by element subtract Dense matrix A and Dense matrix B.
+ * @{
+ */
+/**
+ * @brief element by element subtract Dense matrix A and Dense matrix B.
  * @param A monolish Dense Matrix (size M x N)
  * @param B monolish Dense Matrix (size M x N)
  * @param C monolish Dense Matrix (size M x N)
@@ -39,10 +54,15 @@ void add(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::D
 */ 
 void sub(const matrix::Dense<double> &A, const matrix::Dense<double> &B, matrix::Dense<double> &C);
 void sub(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::Dense<float> &C);
+/**@}*/
 
 /**
- * @brief element by element multiplication Dense matrix A and
- * Dense matrix B.
+ * \defgroup vml_dnsmul monolish::vml::mul
+ * @brief element by element multiplication Dense matrix A and Dense matrix B.
+ * @{
+ */
+/**
+ * @brief element by element multiplication Dense matrix A and Dense matrix B.
  * @param A monolish Dense Matrix (size M x N)
  * @param B monolish Dense Matrix (size M x N)
  * @param C monolish Dense Matrix (size M x N)
@@ -54,10 +74,15 @@ void sub(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::D
 */ 
 void mul(const matrix::Dense<double> &A, const matrix::Dense<double> &B, matrix::Dense<double> &C);
 void mul(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::Dense<float> &C);
+/**@}*/
 
 /**
- * @brief element by element division Dense matrix A and
- * Dense matrix B.
+ * \defgroup vml_dnsdiv monolish::vml::div
+ * @brief element by element division Dense matrix A and Dense matrix B.
+ * @{
+ */
+/**
+ * @brief element by element division Dense matrix A and Dense matrix B.
  * @param A monolish Dense Matrix (size M x N)
  * @param B monolish Dense Matrix (size M x N)
  * @param C monolish Dense Matrix (size M x N)
@@ -69,11 +94,16 @@ void mul(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::D
 */ 
 void div(const matrix::Dense<double> &A, const matrix::Dense<double> &B, matrix::Dense<double> &C);
 void div(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::Dense<float> &C);
+/**@}*/
 
 
 /**
- * @brief element by element addition Dense matrix A and
- * Dense matrix B.
+ * \defgroup vml_sdnsadd monolish::vml::add
+ * @brief element by element addition scalar alpha and Dense matrix A.
+ * @{
+ */
+/**
+ * @brief element by element addition scalar alpha and Dense matrix A.
  * @param A monolish Dense Matrix (size M x N)
  * @param alpha scalar value
  * @param C monolish Dense Matrix (size M x N)
@@ -85,10 +115,15 @@ void div(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::D
 */ 
 void add(const matrix::Dense<double> &A, const double alpha, matrix::Dense<double> &C);
 void add(const matrix::Dense<float> &A, const float alpha, matrix::Dense<float> &C);
+/**@}*/
 
 /**
- * @brief element by element subtract Dense matrix A and
- * Dense matrix B.
+ * \defgroup vml_sdnssub monolish::vml::sub
+ * @brief element by element subtract scalar alpha and Dense matrix A.
+ * @{
+ */
+/**
+ * @brief element by element subtract scalar alpha and Dense matrix A.
  * @param A monolish Dense Matrix (size M x N)
  * @param alpha scalar value
  * @param C monolish Dense Matrix (size M x N)
@@ -100,10 +135,15 @@ void add(const matrix::Dense<float> &A, const float alpha, matrix::Dense<float> 
 */ 
 void sub(const matrix::Dense<double> &A, const double alpha, matrix::Dense<double> &C);
 void sub(const matrix::Dense<float> &A, const float alpha, matrix::Dense<float> &C);
+/**@}*/
 
 /**
- * @brief element by element multiplication Dense matrix A and
- * Dense matrix B.
+ * \defgroup vml_sdnsmul monolish::vml::mul
+ * @brief element by element multiplication scalar alpha and Dense matrix A.
+ * @{
+ */
+/**
+ * @brief element by element multiplication scalar alpha and Dense matrix A.
  * @param A monolish Dense Matrix (size M x N)
  * @param alpha scalar value
  * @param C monolish Dense Matrix (size M x N)
@@ -115,10 +155,15 @@ void sub(const matrix::Dense<float> &A, const float alpha, matrix::Dense<float> 
 */ 
 void mul(const matrix::Dense<double> &A, const double alpha, matrix::Dense<double> &C);
 void mul(const matrix::Dense<float> &A, const float alpha, matrix::Dense<float> &C);
+/**@}*/
 
 /**
- * @brief element by element division Dense matrix A and
- * Dense matrix B.
+ * \defgroup vml_sdnsdiv monolish::vml::div
+ * @brief element by element division scalar alpha and Dense matrix A.
+ * @{
+ */
+/**
+ * @brief element by element division scalar alpha and Dense matrix A.
  * @param A monolish Dense Matrix (size M x N)
  * @param alpha scalar value
  * @param C monolish Dense Matrix (size M x N)
@@ -130,10 +175,16 @@ void mul(const matrix::Dense<float> &A, const float alpha, matrix::Dense<float> 
 */ 
 void div(const matrix::Dense<double> &A, const double alpha, matrix::Dense<double> &C);
 void div(const matrix::Dense<float> &A, const float alpha, matrix::Dense<float> &C);
+/**@}*/
 
 
 /**
- *@brief power to Dense matrix elements Dense matrix (C[0:N] = pow(A[0:N], B[0:N]))
+ * \defgroup vml_dnspow monolish::vml::pow
+ *@brief power to Dense matrix elements (C[0:N] = pow(A[0:N], B[0:N]))
+ * @{
+ */
+/**
+ *@brief power to Dense matrix elements (C[0:N] = pow(A[0:N], B[0:N]))
  * @param A monolish Dense Matrix (size M x N)
  * @param B monolish Dense Matrix (size M x N)
  * @param C monolish Dense Matrix (size M x N)
@@ -144,7 +195,13 @@ void div(const matrix::Dense<float> &A, const float alpha, matrix::Dense<float> 
 */ 
 void pow(const matrix::Dense<double> &A, const matrix::Dense<double> &B, matrix::Dense<double> &C);
 void pow(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_sdnspow monolish::vml::pow
+ * @brief power to Dense matrix elements by scalar value (C[0:N] = pow(A[0:N], alpha))
+ * @{
+ */
 /**
  * @brief power to Dense matrix elements by scalar value (C[0:N] = pow(A[0:N], alpha))
  * @param A monolish Dense Matrix (size M x N)
@@ -157,8 +214,14 @@ void pow(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::D
 */ 
 void pow(const matrix::Dense<double> &A, const double alpha, matrix::Dense<double> &C);
 void pow(const matrix::Dense<float> &A, const float alpha, matrix::Dense<float> &C);
+/**@}*/
 
 
+/**
+ * \defgroup vml_dnssin monolish::vml::sin
+ * @brief sin to Dense matrix elements (C[0:nnz] = sin(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief sin to Dense matrix elements (C[0:nnz] = sin(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -170,7 +233,13 @@ void pow(const matrix::Dense<float> &A, const float alpha, matrix::Dense<float> 
 */ 
 void sin(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void sin(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_dnssqrt monolish::vml::sqrt
+ * @brief sqrt to Dense matrix elements (C[0:nnz] = sqrt(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief sqrt to Dense matrix elements (C[0:nnz] = sqrt(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -182,7 +251,13 @@ void sin(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void sqrt(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void sqrt(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_dnssinh monolish::vml::sinh
+ * @brief sinh to Dense matrix elements (C[0:nnz] = sinh(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief sinh to Dense matrix elements (C[0:nnz] = sinh(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -194,7 +269,13 @@ void sqrt(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void sinh(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void sinh(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_dnsasin monolish::vml::asin
+ * @brief asin to Dense matrix elements (C[0:nnz] = asin(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief asin to Dense matrix elements (C[0:nnz] = asin(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -206,7 +287,13 @@ void sinh(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void asin(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void asin(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_dnsasinh monolish::vml::asinh
+ * @brief asinh to Dense matrix elements (C[0:nnz] = asinh(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief asinh to Dense matrix elements (C[0:nnz] = asinh(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -218,7 +305,13 @@ void asin(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void asinh(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void asinh(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_dnstan monolish::vml::tan
+ * @brief tan to Dense matrix elements (C[0:nnz] = tan(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief tan to Dense matrix elements (C[0:nnz] = tan(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -230,7 +323,13 @@ void asinh(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void tan(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void tan(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_dnstanh monolish::vml::tanh
+ * @brief tanh to Dense matrix elements (C[0:nnz] = tanh(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief tanh to Dense matrix elements (C[0:nnz] = tanh(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -242,7 +341,13 @@ void tan(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void tanh(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void tanh(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_dnsatan monolish::vml::atan
+ * @brief atan to Dense matrix elements (C[0:nnz] = atan(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief atan to Dense matrix elements (C[0:nnz] = atan(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -254,7 +359,13 @@ void tanh(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void atan(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void atan(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_dnsatanh monolish::vml::atanh
+ * @brief atanh to Dense matrix elements (C[0:nnz] = atanh(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief atanh to Dense matrix elements (C[0:nnz] = atanh(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -266,7 +377,13 @@ void atan(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void atanh(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void atanh(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_dnsceil monolish::vml::ceil
+ * @brief ceil to Dense matrix elements (C[0:nnz] = ceil(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief ceil to Dense matrix elements (C[0:nnz] = ceil(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -278,7 +395,13 @@ void atanh(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void ceil(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void ceil(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_dnsfloor monolish::vml::floor
+ * @brief floor to Dense matrix elements (C[0:nnz] = floor(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief floor to Dense matrix elements (C[0:nnz] = floor(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -290,7 +413,13 @@ void ceil(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void floor(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void floor(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+ * \defgroup vml_dnssign monolish::vml::sign
+ * @brief sign to Dense matrix elements (C[0:nnz] = sign(A[0:nnz]))
+ * @{
+ */
 /**
  * @brief sign to Dense matrix elements (C[0:nnz] = sign(A[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -302,8 +431,14 @@ void floor(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void sign(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void sign(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
 
 
+/**
+* \defgroup vml_dnsdnsmax monolish::vml::max
+ * @brief Create a new Dense matrix with greatest elements of two matrices (C[0:nnz] = max(A[0:nnz], B[0:nnz]))
+ * @{
+ */
 /**
  * @brief Create a new Dense matrix with greatest elements of two matrices (C[0:nnz] = max(A[0:nnz], B[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -317,7 +452,13 @@ void sign(const matrix::Dense<float> &A, matrix::Dense<float> &C);
 */ 
 void max(const matrix::Dense<double> &A, const matrix::Dense<double> &B, matrix::Dense<double> &C);
 void max(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::Dense<float> &C);
+/**@}*/
 
+/**
+* \defgroup vml_dnsdnsmin monolish::vml::min
+ * @brief Create a new Dense matrix with smallest elements of two matrices (C[0:nnz] = min(A[0:nnz], B[0:nnz]))
+ * @{
+ */
 /**
  * @brief Create a new Dense matrix with smallest elements of two matrices (C[0:nnz] = min(A[0:nnz], B[0:nnz]))
  * @param A monolish Dense matrix (size M x N)
@@ -331,8 +472,14 @@ void max(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::D
 */ 
 void min(const matrix::Dense<double> &A, const matrix::Dense<double> &B, matrix::Dense<double> &C);
 void min(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::Dense<float> &C);
+/**@}*/
 
 
+/**
+* \defgroup vml_dnsmax monolish::vml::max
+ * @brief Finds the greatest element in Dense matrix (max(C[0:nnz]))
+ * @{
+ */
 /**
  * @brief Finds the greatest element in Dense matrix (max(C[0:nnz]))
  * @param C monolish Dense matrix (size M x N)
@@ -344,7 +491,13 @@ void min(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::D
 */ 
 [[nodiscard]] double max(const matrix::Dense<double> &C);
 [[nodiscard]] float max(const matrix::Dense<float> &C);
+/**@}*/
 
+/**
+* \defgroup vml_dnsmin monolish::vml::min
+ * @brief Finds the smallest element in Dense matrix (min(C[0:nnz]))
+ * @{
+ */
 /**
  * @brief Finds the smallest element in Dense matrix (min(C[0:nnz]))
  * @param C monolish Dense matrix (size M x N)
@@ -356,10 +509,16 @@ void min(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::D
 */ 
 [[nodiscard]] double min(const matrix::Dense<double> &C);
 [[nodiscard]] float min(const matrix::Dense<float> &C);
+/**@}*/
 
 
 /**
- * @brief reciprocal to Dense matrix elements (C[0:nnz] = 1 / A[0:nnz])
+* \defgroup vml_dnsreciprocal monolish::vml::reciprocal
+ * @brief Compute reciprocal to Dense matrix elements (C[0:nnz] = 1 / A[0:nnz])
+ * @{
+ */
+/**
+ * @brief Compute reciprocal to Dense matrix elements (C[0:nnz] = 1 / A[0:nnz])
  * @param A monolish Dense matrix (size M x N)
  * @param C monolish Dense matrix (size M x N)
  * @note
@@ -369,4 +528,6 @@ void min(const matrix::Dense<float> &A, const matrix::Dense<float> &B, matrix::D
 */ 
 void reciprocal(const matrix::Dense<double> &A, matrix::Dense<double> &C);
 void reciprocal(const matrix::Dense<float> &A, matrix::Dense<float> &C);
+/**@}*/
+/**@}*/
 }
