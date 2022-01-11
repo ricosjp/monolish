0900bcec78516aed42fc1cc41cdcb0d80e39b944
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-01-11 11:39:36 +0900

    document list

--- include/monolish_doxygen_list.hpp
+++ include/monolish_doxygen_list.hpp
@@ -1,27 +1,83 @@
 /**
  * \defgroup Classes Basic Classes
+ * @{
+ * \defgroup Vector_class monolsh::vector
+ * @brief vector class
+ *
+ * \defgroup View1D_class monolsh::view1D
+ * @brief 1D view class
+ *
+ * \defgroup Dense_class monolsh::matrix::Dense
+ * @brief Dense format Matrix
+ *
+ * \defgroup COO_class monolsh::matrix::COO
+ * @brief Coodinate (COO) format Matrix
+ *
+ * \defgroup CRS_class monolsh::matrix::CRS
+ * @brief Compressed Row Storage (CRS) format Matrix
+ *
+ * \defgroup LO_class monolsh::matrix::LinearOperator
+ * @brief Linear Operator imitating Matrix
+ * @}
+ */
+
+/**
  * \defgroup Operations BLAS
  * @brief Basic Linear Algebra Subprograms for Dense Matrix, Sparse Matrix,
  * Vector and Scalar
  * @{
  * \defgroup BLASLV1 Vector Operations
  * @brief BLAS Lv1 vector operations
+ *
  * \defgroup BLASLV2 Matrix-Vector Operations
  * @brief BLAS Lv2 matrix-vector operations
+ *
  * \defgroup BLASLV3 Matrix-Matrix Operations
  * @brief BLAS Lv3 matrix-matrix operations
  * @}
+ */
+
+/**
  * \defgroup VML VML
- * @brief Vector Math Library for Dense Matrix, Sparse Matrix, Vector and Scalar
+ * @brief Vector Math Library (VML) for Dense Matrix, Sparse Matrix, Vector and
+ * Scalar
  * @{
  * \defgroup Vector_VML VML for Vector
  * @brief VML for vector
+ *
  * \defgroup Dense_VML VML for Dense
  * @brief VML for Dense matrix
+ *
  * \defgroup CRS_VML VML for CRS
  * @brief VML for CRS matrix
+ *
  * \defgroup LO_VML VML for LinearOperator
  * @brief VML for LinearOperator
  * @}
+ */
+
+/**
  * \defgroup Solvers Solvers
+ * @brief Linear equation solvers for Dense and sparse matrix
+ * @{
+ * \defgroup solver_base Base Class
+ * @brief Solver base class
+ *
+ * \defgroup equations Linear equations
+ * @brief Linear equation solvers for Dense and sparse matrix
+ *
+ * \defgroup sEigen Standard eigen
+ * @brief Solve eigenvalues and eigenvectors problem
+ *
+ * \defgroup gEigen Generalized eigen
+ * @brief Solve generalized eigenvalues and eigenvectors problem
+ * @}
+ */
+
+/**
+ * \defgroup utils Utilities
+ */
+
+/**
+ * \defgroup MPI MPI class (beta)
  */
