784265a9ab4723609536cce944699eefaa00a09a
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-03-09 17:35:04 +0900

    add trans flag (but, trans is not impl.)

--- src/blas/matrix/matvec/dense_matvec.hpp
+++ src/blas/matrix/matvec/dense_matvec.hpp
@@ -5,7 +5,7 @@ namespace monolish {
 namespace {
 // double ///////////////////
 template <typename VEC1, typename VEC2>
-void Dmatvec_core(const matrix::Dense<double> &A, const VEC1 &x, VEC2 &y) {
+void Dmatvec_core(const matrix::Dense<double> &A, const VEC1 &x, VEC2 &y, bool trans) {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
@@ -49,7 +49,7 @@ void Dmatvec_core(const matrix::Dense<double> &A, const VEC1 &x, VEC2 &y) {
 
 // float ///////////////////
 template <typename VEC1, typename VEC2>
-void Smatvec_core(const matrix::Dense<float> &A, const VEC1 &x, VEC2 &y) {
+void Smatvec_core(const matrix::Dense<float> &A, const VEC1 &x, VEC2 &y, bool trans) {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
