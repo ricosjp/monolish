194cc6037b5bf13347fe2aab10a7596c12e3e66a
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-03-11 00:54:24 +0900

    impl transposed gemv

--- src/blas/matrix/matvec/dense_matvec.hpp
+++ src/blas/matrix/matvec/dense_matvec.hpp
@@ -32,7 +32,7 @@ void Dmatvec_core(const matrix::Dense<double> &A, const VEC1 &x, VEC2 &y,
 #pragma omp target data use_device_ptr(xd, yd, vald)
     {
       // cublas is col major
-      internal::check_CUDA(cublasDgemv(h, CUBLAS_OP_T, n, m, &alpha, vald, n,
+      internal::check_CUDA(cublasDgemv(h, internal::get_cublas_trans(!transA), n, m, &alpha, vald, n,
                                        xd + xoffset, 1, &beta, yd + yoffset,
                                        1));
     }
@@ -41,7 +41,7 @@ void Dmatvec_core(const matrix::Dense<double> &A, const VEC1 &x, VEC2 &y,
     throw std::runtime_error("error USE_GPU is false, but gpu_status == true");
 #endif
   } else {
-    cblas_dgemv(CblasRowMajor, CblasNoTrans, m, n, alpha, vald, n, xd + xoffset,
+    cblas_dgemv(CblasRowMajor, internal::get_cblas_trans(transA), m, n, alpha, vald, n, xd + xoffset,
                 1, beta, yd + yoffset, 1);
   }
 
@@ -77,7 +77,7 @@ void Smatvec_core(const matrix::Dense<float> &A, const VEC1 &x, VEC2 &y,
 #pragma omp target data use_device_ptr(xd, yd, vald)
     {
       // cublas is col major
-      internal::check_CUDA(cublasSgemv(h, CUBLAS_OP_T, m, n, &alpha, vald, m,
+      internal::check_CUDA(cublasSgemv(h, internal::get_cublas_trans(!transA), m, n, &alpha, vald, m,
                                        xd + xoffset, 1, &beta, yd + yoffset,
                                        1));
     }
@@ -86,7 +86,7 @@ void Smatvec_core(const matrix::Dense<float> &A, const VEC1 &x, VEC2 &y,
     throw std::runtime_error("error USE_GPU is false, but gpu_status == true");
 #endif
   } else {
-    cblas_sgemv(CblasRowMajor, CblasNoTrans, n, m, alpha, vald, m, xd + xoffset,
+    cblas_sgemv(CblasRowMajor, internal::get_cblas_trans(transA), n, m, alpha, vald, m, xd + xoffset,
                 1, beta, yd + yoffset, 1);
   }
 
