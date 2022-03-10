3f73e542a007158abb54f63959f7350aafae086a
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-03-10 16:33:29 +0900

    get blas trans flag

--- src/internal/blas_util/get_trans.cpp
+++ src/internal/blas_util/get_trans.cpp
@@ -2,33 +2,30 @@
 
 namespace monolish::internal {
 
-    CBLAS_TRANSPOSE get_cblas_trans(bool flag){
-        if(flag ==true){
-            return CblasTrans;
-        }
-        else{
-            return CblasNoTrans;
-        }
-    }
+CBLAS_TRANSPOSE get_cblas_trans(bool flag) {
+  if (flag == true) {
+    return CblasTrans;
+  } else {
+    return CblasNoTrans;
+  }
+}
 
 #ifdef MONOLISH_USE_NVIDIA_GPU
-    cusparseOperation_t get_cuspasrse_trans(bool flag){
-        if(flag ==true){
-            return CUSPARSE_OPERATION_TRANSPOSE;
-        }
-        else{
-            return CUSPARSE_OPERATION_NON_TRANSPOSE;
-        }
-    }
+cusparseOperation_t get_cuspasrse_trans(bool flag) {
+  if (flag == true) {
+    return CUSPARSE_OPERATION_TRANSPOSE;
+  } else {
+    return CUSPARSE_OPERATION_NON_TRANSPOSE;
+  }
+}
 
-    cublasOperation_t get_cublas_trans(bool flag){
-        if(flag ==true){
-            return CUBLAS_OP_T;
-        }
-        else{
-            return CUBLAS_OP_N;
-        }
-    }
+cublasOperation_t get_cublas_trans(bool flag) {
+  if (flag == true) {
+    return CUBLAS_OP_T;
+  } else {
+    return CUBLAS_OP_N;
+  }
+}
 #endif
 
-}
+} // namespace monolish::internal
