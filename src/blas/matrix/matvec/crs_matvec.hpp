ea49247a624194d216981f75803e450e821f1540
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-03-28 04:53:48 +0900

    impl OSS tspmv

--- src/blas/matrix/matvec/crs_matvec.hpp
+++ src/blas/matrix/matvec/crs_matvec.hpp
@@ -92,6 +92,17 @@ void Dmatvec_core(const matrix::CRS<double> &A, const VEC1 &x, VEC2 &y,
     // OSS
 #else
     if (transA == true) {
+#pragma omp parallel for
+          for (auto i = decltype(y.size()){0}; i < y.size(); i++) {
+                  (yd+yoffset)[i] = 0.0;
+          }
+
+          for (auto i = decltype(A.get_row()){0}; i < A.get_row(); i++) {
+              for (auto j = rowd[i]; j < rowd[i + 1]; j++) {
+                  (yd+yoffset)[cold[j]] += vald[j] * (xd + xoffset)[i];
+              }
+          }
+      
     } else {
 #pragma omp parallel for
       for (auto i = decltype(A.get_row()){0}; i < A.get_row(); i++) {
