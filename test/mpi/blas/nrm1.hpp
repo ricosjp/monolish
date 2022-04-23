b40bb88404ec9ee606740ed44289209efdf37fde
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-04-24 00:50:36 +0900

    specify test random seed

--- test/mpi/blas/nrm1.hpp
+++ test/mpi/blas/nrm1.hpp
@@ -27,7 +27,7 @@ template <typename T> bool test_send_nrm1(const size_t size, double tol) {
 
 template <typename T> bool test_nrm1(const size_t size, double tol) {
 
-  monolish::vector<T> x(size, 0.0, 1.0);
+  monolish::vector<T> x(size, 0.0, 1.0, test_random_engine());
 
   auto result = monolish::blas::nrm1(x);
   auto ans = ans_nrm1(x);
