3bf3909aa7364b6e23f91c7e77db105e5ccffd8f
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-05-02 00:54:26 +0900

    C++17->C++11

--- test/test_sxat.mk
+++ test/test_sxat.mk
@@ -6,7 +6,7 @@ BLAS_LIB = -I/opt/nec/ve/nlc/2.3.0/include/
 BLAS_LIB += -L/opt/nec/ve/nlc/2.3.0/lib/
 BLAS_LIB += -llapack -lcblas -lblas_openmp
 
-CXXFLAGS=-O3 -lm -fopenmp -pthread -std=c++17
+CXXFLAGS=-O3 -lm -fopenmp -pthread -std=c++11
 
 LIBS=-I $(MONOLISH_DIR)/include/ -L$(MONOLISH_DIR)/lib/ -lmonolish_sxat $(BLAS_LIB) 
 
