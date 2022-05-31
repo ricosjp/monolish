bbe27c99c2367faf9292ff5bcd75f0bf35c9ca6f
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-05-31 16:09:40 +0900

    generate add2other crs diag_op

--- src/blas/matrix/diag_op/add2other.sh
+++ src/blas/matrix/diag_op/add2other.sh
@@ -1,3 +1,7 @@
 sed "s/add/sub/g" ./dense_diag_add.cpp | sed "s/+=/-=/g" > ./dense_diag_sub.cpp
 sed "s/add/mul/g" ./dense_diag_add.cpp | sed "s/+=/*=/g" > ./dense_diag_mul.cpp
 sed "s/add/div/g" ./dense_diag_add.cpp | sed "s/+=/\/=/g" > ./dense_diag_div.cpp
+
+sed "s/add/sub/g" ./crs_diag_add.cpp | sed "s/+=/-=/g" > ./crs_diag_sub.cpp
+sed "s/add/mul/g" ./crs_diag_add.cpp | sed "s/+=/*=/g" > ./crs_diag_mul.cpp
+sed "s/add/div/g" ./crs_diag_add.cpp | sed "s/+=/\/=/g" > ./crs_diag_div.cpp
