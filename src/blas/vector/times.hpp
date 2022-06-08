60daa3ac4034ae0d84b8005a8626a6e8fe67b649
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-06-08 16:08:31 +0900

    times scalar

---
+++ src/blas/vector/times.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace monolish {
+namespace {
+
+template <typename F1, typename F2, typename F3>
+void times_scalar_core(const F1 &alpha, const F2 &a, F3 &y) {
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
+
+  const auto boffset = a.get_offset();
+  const auto yoffset = y.get_offset();
+
+  // err
+  assert(util::is_same_size(a, y));
+  assert(util::is_same_device_mem_stat(a, y));
+
+  internal::vmul(y.size(), a.data() + aoffset, alpha,
+                 y.data() + yoffset, y.get_device_mem_stat());
+
+  logger.func_out();
+}
+} // namespace
+} // namespace monolish
