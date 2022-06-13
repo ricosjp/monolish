729ab39f4ed96b7c772a36a38f8a639790d36c1b
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-06-14 03:48:29 +0900

    format

--- src/blas/matrix/times/crs_times.cpp
+++ src/blas/matrix/times/crs_times.cpp
@@ -8,172 +8,172 @@ namespace {
 // scalar-matrix times //
 template <typename T>
 void times_core(const T alpha, const matrix::CRS<T> &A, matrix::CRS<T> &C) {
-    Logger &logger = Logger::get_instance();
-    logger.func_in(monolish_func);
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
 
-    // err
-    assert(util::is_same_structure(A, C));
-    assert(util::is_same_device_mem_stat(A, C));
+  // err
+  assert(util::is_same_structure(A, C));
+  assert(util::is_same_device_mem_stat(A, C));
 
-    internal::vmul(A.get_nnz(), A.val.data(), alpha, C.val.data(),
-                   A.get_device_mem_stat());
+  internal::vmul(A.get_nnz(), A.val.data(), alpha, C.val.data(),
+                 A.get_device_mem_stat());
 
-    logger.func_out();
+  logger.func_out();
 }
 
 // vector-matrix_row times all //
 template <typename T, typename VEC>
 void times_row_core(const matrix::CRS<T> &A, const VEC &x, matrix::CRS<T> &C) {
-    Logger &logger = Logger::get_instance();
-    logger.func_in(monolish_func);
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
 
-    assert(util::is_same_structure(A, C));
-    assert(util::is_same_device_mem_stat(A, x, C));
-    assert(A.get_col() == x.size());
+  assert(util::is_same_structure(A, C));
+  assert(util::is_same_device_mem_stat(A, x, C));
+  assert(A.get_col() == x.size());
 
-    const auto *Ad = A.val.data();
-    auto *Cd = C.val.data();
-    const auto *rowd = A.row_ptr.data();
-    const auto *cold = A.col_ind.data();
-    const auto n = A.get_row();
+  const auto *Ad = A.val.data();
+  auto *Cd = C.val.data();
+  const auto *rowd = A.row_ptr.data();
+  const auto *cold = A.col_ind.data();
+  const auto n = A.get_row();
 
-    const auto *xd = x.data();
-    const auto xoffset = x.get_offset();
+  const auto *xd = x.data();
+  const auto xoffset = x.get_offset();
 
-    if (A.get_device_mem_stat() == true) {
+  if (A.get_device_mem_stat() == true) {
 #if MONOLISH_USE_NVIDIA_GPU
 #pragma omp target teams distribute parallel for
-        for (auto i = decltype(n){0}; i < n; i++) {
-            for (auto j = rowd[i]; j < rowd[i + 1]; j++) {
-                Cd[j] = Ad[j] * xd[cold[j] + xoffset];
-            }
-        }
-#else
-        throw std::runtime_error(
-            "error USE_GPU is false, but get_device_mem_stat() == true");
-#endif
-    } else {
-#pragma omp parallel for
-        for (auto i = decltype(n){0}; i < n; i++) {
-            for (auto j = rowd[i]; j < rowd[i + 1]; j++) {
-                Cd[j] = Ad[j] * xd[cold[j] + xoffset];
-            }
-        }
+    for (auto i = decltype(n){0}; i < n; i++) {
+      for (auto j = rowd[i]; j < rowd[i + 1]; j++) {
+        Cd[j] = Ad[j] * xd[cold[j] + xoffset];
+      }
     }
+#else
+    throw std::runtime_error(
+        "error USE_GPU is false, but get_device_mem_stat() == true");
+#endif
+  } else {
+#pragma omp parallel for
+    for (auto i = decltype(n){0}; i < n; i++) {
+      for (auto j = rowd[i]; j < rowd[i + 1]; j++) {
+        Cd[j] = Ad[j] * xd[cold[j] + xoffset];
+      }
+    }
+  }
 
-    logger.func_out();
+  logger.func_out();
 }
 
 // vector-matrix_col times all //
 template <typename T, typename VEC>
 void times_col_core(const matrix::CRS<T> &A, const VEC &x, matrix::CRS<T> &C) {
-    Logger &logger = Logger::get_instance();
-    logger.func_in(monolish_func);
+  Logger &logger = Logger::get_instance();
+  logger.func_in(monolish_func);
 
-    assert(util::is_same_structure(A, C));
-    assert(util::is_same_device_mem_stat(A, x, C));
-    assert(A.get_row() == x.size());
+  assert(util::is_same_structure(A, C));
+  assert(util::is_same_device_mem_stat(A, x, C));
+  assert(A.get_row() == x.size());
 
-    const auto *Ad = A.val.data();
-    auto *Cd = C.val.data();
-    const auto *rowd = A.row_ptr.data();
-    const auto n = A.get_row();
+  const auto *Ad = A.val.data();
+  auto *Cd = C.val.data();
+  const auto *rowd = A.row_ptr.data();
+  const auto n = A.get_row();
 
-    const auto *xd = x.data();
-    const auto xoffset = x.get_offset();
+  const auto *xd = x.data();
+  const auto xoffset = x.get_offset();
 
-    if (A.get_device_mem_stat() == true) {
+  if (A.get_device_mem_stat() == true) {
 #if MONOLISH_USE_NVIDIA_GPU
 #pragma omp target teams distribute parallel for
-        for (auto i = decltype(n){0}; i < n; i++) {
-            for (auto j = rowd[i]; j < rowd[i + 1]; j++) {
-                Cd[j] = Ad[j] * xd[i + xoffset];
-            }
-        }
-#else
-        throw std::runtime_error(
-            "error USE_GPU is false, but get_device_mem_stat() == true");
-#endif
-    } else {
-#pragma omp parallel for
-        for (auto i = decltype(n){0}; i < n; i++) {
-            for (auto j = rowd[i]; j < rowd[i + 1]; j++) {
-                Cd[j] = Ad[j] * xd[i + xoffset];
-            }
-        }
+    for (auto i = decltype(n){0}; i < n; i++) {
+      for (auto j = rowd[i]; j < rowd[i + 1]; j++) {
+        Cd[j] = Ad[j] * xd[i + xoffset];
+      }
     }
+#else
+    throw std::runtime_error(
+        "error USE_GPU is false, but get_device_mem_stat() == true");
+#endif
+  } else {
+#pragma omp parallel for
+    for (auto i = decltype(n){0}; i < n; i++) {
+      for (auto j = rowd[i]; j < rowd[i + 1]; j++) {
+        Cd[j] = Ad[j] * xd[i + xoffset];
+      }
+    }
+  }
 
-    logger.func_out();
+  logger.func_out();
 }
-}  // namespace
+} // namespace
 
 namespace blas {
 void times(const double alpha, const matrix::CRS<double> &A,
            matrix::CRS<double> &C) {
-    times_core(alpha, A, C);
+  times_core(alpha, A, C);
 }
 void times(const float alpha, const matrix::CRS<float> &A,
            matrix::CRS<float> &C) {
-    times_core(alpha, A, C);
+  times_core(alpha, A, C);
 }
 
 // vector-matrix_row times all //
 void times_row(const matrix::CRS<double> &A, const vector<double> &x,
                matrix::CRS<double> &C) {
-    times_row_core(A, x, C);
+  times_row_core(A, x, C);
 }
 void times_row(const matrix::CRS<double> &A,
                const view1D<vector<double>, double> &x,
                matrix::CRS<double> &C) {
-    times_row_core(A, x, C);
+  times_row_core(A, x, C);
 }
 void times_row(const matrix::CRS<double> &A,
                const view1D<matrix::CRS<double>, double> &x,
                matrix::CRS<double> &C) {
-    times_row_core(A, x, C);
+  times_row_core(A, x, C);
 }
 void times_row(const matrix::CRS<float> &A, const vector<float> &x,
                matrix::CRS<float> &C) {
-    times_row_core(A, x, C);
+  times_row_core(A, x, C);
 }
 void times_row(const matrix::CRS<float> &A,
                const view1D<vector<float>, float> &x, matrix::CRS<float> &C) {
-    times_row_core(A, x, C);
+  times_row_core(A, x, C);
 }
 void times_row(const matrix::CRS<float> &A,
                const view1D<matrix::CRS<float>, float> &x,
                matrix::CRS<float> &C) {
-    times_row_core(A, x, C);
+  times_row_core(A, x, C);
 }
 
 // vector-matrix_col times all //
 void times_col(const matrix::CRS<double> &A, const vector<double> &x,
                matrix::CRS<double> &C) {
-    times_col_core(A, x, C);
+  times_col_core(A, x, C);
 }
 void times_col(const matrix::CRS<double> &A,
                const view1D<vector<double>, double> &x,
                matrix::CRS<double> &C) {
-    times_col_core(A, x, C);
+  times_col_core(A, x, C);
 }
 void times_col(const matrix::CRS<double> &A,
                const view1D<matrix::CRS<double>, double> &x,
                matrix::CRS<double> &C) {
-    times_col_core(A, x, C);
+  times_col_core(A, x, C);
 }
 void times_col(const matrix::CRS<float> &A, const vector<float> &x,
                matrix::CRS<float> &C) {
-    times_col_core(A, x, C);
+  times_col_core(A, x, C);
 }
 void times_col(const matrix::CRS<float> &A,
                const view1D<vector<float>, float> &x, matrix::CRS<float> &C) {
-    times_col_core(A, x, C);
+  times_col_core(A, x, C);
 }
 void times_col(const matrix::CRS<float> &A,
                const view1D<matrix::CRS<float>, float> &x,
                matrix::CRS<float> &C) {
-    times_col_core(A, x, C);
+  times_col_core(A, x, C);
 }
 
-}  // namespace blas
-}  // namespace monolish
+} // namespace blas
+} // namespace monolish
--- test/matrix/blas/times_col.hpp
+++ test/matrix/blas/times_col.hpp
@@ -4,75 +4,75 @@ template <typename T>
 void ans_times_col(const monolish::matrix::Dense<T> &A,
                    const monolish::vector<T> &mx,
                    monolish::matrix::Dense<T> &C) {
-    if (A.get_row() != mx.size()) {
-        std::runtime_error("A.row != y.size");
-    }
+  if (A.get_row() != mx.size()) {
+    std::runtime_error("A.row != y.size");
+  }
 
-    const T *x = mx.data();
-    int M = A.get_row();
-    int N = A.get_col();
+  const T *x = mx.data();
+  int M = A.get_row();
+  int N = A.get_col();
 
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
-            C.val[i * N + j] = A.val[i * N + j] * x[i];
-        }
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) {
+      C.val[i * N + j] = A.val[i * N + j] * x[i];
     }
+  }
 }
 
 template <typename MAT, typename T>
 bool test_send_times_col(const size_t M, const size_t N, double tol) {
-    size_t nnzrow = 27;
-    if (nnzrow < N) {
-        nnzrow = 27;
-    } else {
-        nnzrow = N - 1;
-    }
+  size_t nnzrow = 27;
+  if (nnzrow < N) {
+    nnzrow = 27;
+  } else {
+    nnzrow = N - 1;
+  }
 
-    monolish::matrix::COO<T> seedA =
-        monolish::util::random_structure_matrix<T>(M, N, nnzrow, 1.0);
+  monolish::matrix::COO<T> seedA =
+      monolish::util::random_structure_matrix<T>(M, N, nnzrow, 1.0);
 
-    MAT A(seedA);  // M*N matrix
-    MAT C(seedA);  // M*N matrix
-    monolish::vector<T> x(A.get_row(), 0.0, 1.0, test_random_engine());
+  MAT A(seedA); // M*N matrix
+  MAT C(seedA); // M*N matrix
+  monolish::vector<T> x(A.get_row(), 0.0, 1.0, test_random_engine());
 
-    monolish::matrix::Dense<T> AA(seedA);
-    monolish::matrix::Dense<T> CC(seedA);
-    ans_times_col(AA, x, CC);
-    monolish::matrix::COO<T> ansC(CC);
+  monolish::matrix::Dense<T> AA(seedA);
+  monolish::matrix::Dense<T> CC(seedA);
+  ans_times_col(AA, x, CC);
+  monolish::matrix::COO<T> ansC(CC);
 
-    monolish::util::send(A, x, C);
-    monolish::blas::times_col(A, x, C);
-    C.recv();
-    monolish::matrix::COO<T> resultC(C);
+  monolish::util::send(A, x, C);
+  monolish::blas::times_col(A, x, C);
+  C.recv();
+  monolish::matrix::COO<T> resultC(C);
 
-    return ans_check<T>(__func__, A.type(), resultC.val.data(), ansC.val.data(),
-                        ansC.get_nnz(), tol);
+  return ans_check<T>(__func__, A.type(), resultC.val.data(), ansC.val.data(),
+                      ansC.get_nnz(), tol);
 }
 
 template <typename MAT, typename T>
 bool test_times_col(const size_t M, const size_t N, double tol) {
-    size_t nnzrow = 27;
-    if (nnzrow < N) {
-        nnzrow = 27;
-    } else {
-        nnzrow = N - 1;
-    }
+  size_t nnzrow = 27;
+  if (nnzrow < N) {
+    nnzrow = 27;
+  } else {
+    nnzrow = N - 1;
+  }
 
-    monolish::matrix::COO<T> seedA =
-        monolish::util::random_structure_matrix<T>(M, N, nnzrow, 1.0);
+  monolish::matrix::COO<T> seedA =
+      monolish::util::random_structure_matrix<T>(M, N, nnzrow, 1.0);
 
-    MAT A(seedA);  // M*N matrix
-    MAT C(seedA);  // M*N matrix
-    monolish::vector<T> x(A.get_row(), 0.0, 1.0, test_random_engine());
+  MAT A(seedA); // M*N matrix
+  MAT C(seedA); // M*N matrix
+  monolish::vector<T> x(A.get_row(), 0.0, 1.0, test_random_engine());
 
-    monolish::matrix::Dense<T> AA(seedA);
-    monolish::matrix::Dense<T> CC(seedA);
-    ans_times_col(AA, x, CC);
-    monolish::matrix::COO<T> ansC(CC);
+  monolish::matrix::Dense<T> AA(seedA);
+  monolish::matrix::Dense<T> CC(seedA);
+  ans_times_col(AA, x, CC);
+  monolish::matrix::COO<T> ansC(CC);
 
-    monolish::blas::times_col(A, x, C);
-    monolish::matrix::COO<T> resultC(C);
+  monolish::blas::times_col(A, x, C);
+  monolish::matrix::COO<T> resultC(C);
 
-    return ans_check<T>(__func__, A.type(), resultC.val.data(), ansC.val.data(),
-                        ansC.get_nnz(), tol);
+  return ans_check<T>(__func__, A.type(), resultC.val.data(), ansC.val.data(),
+                      ansC.get_nnz(), tol);
 }
