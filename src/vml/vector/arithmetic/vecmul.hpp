a5fb7ae436e87dbe34d0e489bd92ed46ffe570eb
Daiki Adachi <adachi@ricos.co.jp>
2023-05-09 22:00:16 +0900

    [update] data+offset -> begin

--- src/vml/vector/arithmetic/vecmul.hpp
+++ src/vml/vector/arithmetic/vecmul.hpp
@@ -12,8 +12,8 @@ void svmul_core(const F1 &a, const F2 alpha, F3 &y) {
   assert(util::is_same_size(a, y));
   assert(util::is_same_device_mem_stat(a, y));
 
-  internal::vmul(y.size(), a.data() + a.get_offset(), alpha,
-                 y.data() + y.get_offset(), y.get_device_mem_stat());
+  internal::vmul(y.size(), a.begin(), alpha, y.begin(),
+                 y.get_device_mem_stat());
 
   logger.func_out();
 }
@@ -27,8 +27,8 @@ void vvmul_core(const F1 &a, const F2 &b, F3 &y) {
   assert(util::is_same_size(a, b, y));
   assert(util::is_same_device_mem_stat(a, b, y));
 
-  internal::vmul(y.size(), a.data() + a.get_offset(), b.data() + b.get_offset(),
-                 y.data() + y.get_offset(), y.get_device_mem_stat());
+  internal::vmul(y.size(), a.begin(), b.begin(), y.begin(),
+                 y.get_device_mem_stat());
 
   logger.func_out();
 }
--- src/vml/vector/math_1_3op/vecmin.hpp
+++ src/vml/vector/math_1_3op/vecmin.hpp
@@ -11,8 +11,8 @@ void vvmin_core(const F1 &a, const F2 &b, F3 &y) {
   assert(util::is_same_size(a, b, y));
   assert(util::is_same_device_mem_stat(a, b, y));
 
-  internal::vmin(y.size(), a.data() + a.get_offset(), b.data() + b.get_offset(),
-                 y.data() + y.get_offset(), y.get_device_mem_stat());
+  internal::vmin(y.size(), a.begin(), b.begin(), y.begin(),
+                 y.get_device_mem_stat());
 
   logger.func_out();
 }
@@ -26,8 +26,8 @@ void svmin_core(const F1 &a, const F2 alpha, F3 &y) {
   assert(util::is_same_size(a, y));
   assert(util::is_same_device_mem_stat(a, y));
 
-  internal::vmin(y.size(), a.data() + a.get_offset(), alpha,
-                 y.data() + y.get_offset(), y.get_device_mem_stat());
+  internal::vmin(y.size(), a.begin(), alpha, y.begin(),
+                 y.get_device_mem_stat());
 
   logger.func_out();
 }
@@ -36,8 +36,7 @@ template <typename F1, typename F2> F2 vmin_core(const F1 &y) {
   Logger &logger = Logger::get_instance();
   logger.func_in(monolish_func);
 
-  F2 ret = internal::vmin(y.size(), y.data() + y.get_offset(),
-                          y.get_device_mem_stat());
+  F2 ret = internal::vmin(y.size(), y.begin(), y.get_device_mem_stat());
 
   logger.func_out();
   return ret;
