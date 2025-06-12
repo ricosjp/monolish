fde6129592b37f4201f25614b3c616b13737ef00
Daiki Adachi <adachi@ricos.co.jp>
2025-06-12 14:59:53 +0900

    [update] add test for coo_to_crs

--- test/matrix/matrix_convert.cpp
+++ test/matrix/matrix_convert.cpp
@@ -50,6 +50,59 @@ bool test_convert(const size_t M, const size_t N) {
   return true;
 }
 
+template <typename MAT, typename T>
+bool test_convert2(const size_t M, const size_t N) {
+  // ans COO (source)
+
+  std::vector<int> row = {0, 0, 9, 9}, col = {0, 9, 0, 9};
+  std::vector<T> value = {1.0, 2.0, 3.0 ,4.0};
+  monolish::matrix::COO<T> ans_coo(10, 10, 4, row, col, value);
+
+  // convert COO -> MAT
+  MAT mat(ans_coo);
+
+  // convert MAT -> result COO (dest.)
+  monolish::matrix::COO<T> result_coo(mat);
+
+  // check source == dest.
+  if (result_coo.get_row() != ans_coo.get_row() ||
+      result_coo.get_col() != ans_coo.get_col() ||
+      result_coo.get_nnz() != ans_coo.get_nnz()) {
+
+    std::cout << "error, row, col, nnz are different(COO2" << mat.type() << ")"
+              << std::endl;
+    std::cout << result_coo.get_row() << " != " << ans_coo.get_row()
+              << std::endl;
+    std::cout << result_coo.get_col() << " != " << ans_coo.get_col()
+              << std::endl;
+    std::cout << result_coo.get_nnz() << " != " << ans_coo.get_nnz()
+              << std::endl;
+
+    return false;
+  }
+  for (size_t i = 0; i < ans_coo.get_nnz(); i++) {
+
+    if (result_coo.data()[i] != ans_coo.data()[i] ||
+        result_coo.row_index[i] != ans_coo.row_index[i] ||
+        result_coo.col_index[i] != ans_coo.col_index[i]) {
+
+      std::cout << i << "\t" << result_coo.row_index[i] << ","
+                << result_coo.col_index[i] << "," << result_coo.data()[i]
+                << std::flush;
+      std::cout << ", (ans: " << ans_coo.row_index[i] << ","
+                << ans_coo.col_index[i] << "," << ans_coo.data()[i] << ")"
+                << std::endl;
+      return false;
+    }
+  }
+
+  std::cout << __func__ << "(" << get_type<T>() << "," << mat.type() << ")"
+            << std::flush;
+  std::cout << ": pass" << std::endl;
+
+  return true;
+}
+
 int main(int argc, char **argv) {
 
   if (argc != 3) {
@@ -76,6 +129,20 @@ int main(int argc, char **argv) {
     return 1;
   }
   if (test_convert<monolish::matrix::CRS<float>, float>(M, N) == false) {
+    return 1;
+  }
+
+  if (test_convert2<monolish::matrix::Dense<double>, double>(M, N) == false) {
+    return 1;
+  }
+  if (test_convert2<monolish::matrix::CRS<double>, double>(M, N) == false) {
+    return 1;
+  }
+
+  if (test_convert2<monolish::matrix::Dense<float>, float>(M, N) == false) {
+    return 1;
+  }
+  if (test_convert2<monolish::matrix::CRS<float>, float>(M, N) == false) {
     return 1;
   }
 
